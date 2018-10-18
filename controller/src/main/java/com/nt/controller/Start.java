4c450c1d8c86aa34afd3e0dac66434dee295fb98
赵国栋 <dong8@LAPTOP-95K23HII>
2018-10-18 10:59:17 +0800

    扫描

--- controller/src/main/java/com/nt/controller/Start.java
+++ controller/src/main/java/com/nt/controller/Start.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.nt.*"})
 public class Start {
 
     private static Logger log = LoggerFactory.getLogger(Start.class);
