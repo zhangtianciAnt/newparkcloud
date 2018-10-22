f4046e0b15a861fc285d66e3e86e350abd581e1c
赵国栋 <dong8@LAPTOP-95K23HII>
2018-10-22 13:43:46 +0800

    ORGTREE 实体增加

--- dao/dao_Org/src/main/java/com/nt/dao_Org/OrgTree.java
+++ dao/dao_Org/src/main/java/com/nt/dao_Org/OrgTree.java
@@ -4,78 +4,30 @@ import com.nt.utils.BaseModel;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
-import java.util.List;
 
 @Document(collection = "orgtree")
+/*
+组织ID	ORGID
+节点ID	NODEID
+节点名称	TITLE
+节点类型	TYPE
+*/
 public class OrgTree extends BaseModel {
-    /**
-     * 节点id
-     */
-    private String nodeid;
-    /**
-     * 节点标题
-     */
-    private String title;
-    /**
-     * 节点类型：1：公司；2：部门
-     */
-    private String type;
-    /**
-     * 组织id
-     */
+    private static final long serialVersionUID = 1L;
+
     private String orgid;
-    /**
-     * 公司id
-     */
-    private String companyid;
-    /**
-     * 公司简称
-     */
-    private String companyshortname;
-    /**
-     * 公司英文名称
-     */
-    private String companyen;
-    /**
-     * 公司全称
-     */
-    private String companyname;
-    /**
-     * 公司地址
-     */
-    private String companyaddress;
-    /**
-     * 公司法人
-     */
-    private String companycorporation;
-    /**
-     * 公司执照
-     */
-    private String unifedsocialcreditcode;
-    /**
-     * 公司成立时间
-     */
-    private Date establish;
-    /**
-     * 部门id
-     */
-    private String departmentid;
-    /**
-     * 部门名称
-     */
-    private String departmentname;
-    /**
-     * 公司开票信息
-     */
-    private InvoiceInfo invoiceinfo;
-    /**
-     * 公司账户信息
-     */
-    private List<BankInfo> bankinfo;
-    /**
-     * 子公司信息
-     */
-    private List<OrgTree> orgs;
+    private String nodeid;
+    private String title;
+    private String type; //1：公司；2：部门
+    private Orgs orgs;
+
+    public String getOrgid() {
+        return orgid;
+    }
+
+    public void setOrgid(String orgid) {
+        this.orgid = orgid;
+    }
 
     public String getNodeid() {
         return nodeid;
@@ -101,115 +53,304 @@ public class OrgTree extends BaseModel {
         this.type = type;
     }
 
-    public String getOrgid() {
-        return orgid;
-    }
-
-    public void setOrgid(String orgid) {
-        this.orgid = orgid;
-    }
-
-    public String getCompanyid() {
-        return companyid;
-    }
-
-    public void setCompanyid(String companyid) {
-        this.companyid = companyid;
-    }
-
-    public String getCompanyshortname() {
-        return companyshortname;
-    }
-
-    public void setCompanyshortname(String companyshortname) {
-        this.companyshortname = companyshortname;
-    }
-
-    public String getCompanyen() {
-        return companyen;
-    }
-
-    public void setCompanyen(String companyen) {
-        this.companyen = companyen;
-    }
-
-    public String getCompanyname() {
-        return companyname;
-    }
-
-    public void setCompanyname(String companyname) {
-        this.companyname = companyname;
-    }
-
-    public String getCompanyaddress() {
-        return companyaddress;
-    }
-
-    public void setCompanyaddress(String companyaddress) {
-        this.companyaddress = companyaddress;
-    }
-
-    public String getCompanycorporation() {
-        return companycorporation;
-    }
-
-    public void setCompanycorporation(String companycorporation) {
-        this.companycorporation = companycorporation;
-    }
-
-    public String getUnifedsocialcreditcode() {
-        return unifedsocialcreditcode;
-    }
-
-    public void setUnifedsocialcreditcode(String unifedsocialcreditcode) {
-        this.unifedsocialcreditcode = unifedsocialcreditcode;
-    }
-
-    public Date getEstablish() {
-        return establish;
-    }
-
-    public void setEstablish(Date establish) {
-        this.establish = establish;
-    }
-
-    public InvoiceInfo getInvoiceinfo() {
-        return invoiceinfo;
-    }
-
-    public void setInvoiceinfo(InvoiceInfo invoiceinfo) {
-        this.invoiceinfo = invoiceinfo;
-    }
-
-    public List<BankInfo> getBankinfo() {
-        return bankinfo;
-    }
-
-    public void setBankinfo(List<BankInfo> bankinfo) {
-        this.bankinfo = bankinfo;
-    }
-
-    public List<OrgTree> getOrgs() {
+    public Orgs getOrgs() {
         return orgs;
     }
 
-    public void setOrgs(List<OrgTree> orgs) {
+    public void setOrgs(Orgs orgs) {
         this.orgs = orgs;
     }
 
-    public String getDepartmentid() {
-        return departmentid;
+    /*
+    节点ID	NODEID
+    节点名称	TITLE
+    节点类型	TYPE
+    公司ID	COMPANYID
+    公司简称	COMPANYSHORTNAME
+    公司英文大写	COMPANYEN
+    企业名称	COMPANYNAME
+    公司地址	COMPANYADDRESS
+    公司法人	COMPANYCORPORATION
+    公司执照	UNIFEDSOCIALCREDITCODE
+    公司成立时间	ESTABLISH
+    部门ID	DEPARTMENTID
+    部门名	DEPARTMENTNAME
+    */
+    public class Orgs extends BaseModel {
+        private static final long serialVersionUID = 1L;
+
+        private String nodeid;
+        private String title;
+        private String type;
+        private String departmentid;
+        private String departmentname;
+        private String companyid;
+        private String companyshortname;
+        private String companyen;
+        private String companyname;
+        private String companyaddress;
+        private String companycorporation;
+        private String unifedsocialcreditcode;
+        private Date establish;
+        private Invoiceinfo invoiceinfo;
+        private Bankinfo bankinfo;
+
+        public String getNodeid() {
+            return nodeid;
+        }
+
+        public void setNodeid(String nodeid) {
+            this.nodeid = nodeid;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDepartmentid() {
+            return departmentid;
+        }
+
+        public void setDepartmentid(String departmentid) {
+            this.departmentid = departmentid;
+        }
+
+        public String getDepartmentname() {
+            return departmentname;
+        }
+
+        public void setDepartmentname(String departmentname) {
+            this.departmentname = departmentname;
+        }
+
+        public String getCompanyid() {
+            return companyid;
+        }
+
+        public void setCompanyid(String companyid) {
+            this.companyid = companyid;
+        }
+
+        public String getCompanyshortname() {
+            return companyshortname;
+        }
+
+        public void setCompanyshortname(String companyshortname) {
+            this.companyshortname = companyshortname;
+        }
+
+        public String getCompanyen() {
+            return companyen;
+        }
+
+        public void setCompanyen(String companyen) {
+            this.companyen = companyen;
+        }
+
+        public String getCompanyname() {
+            return companyname;
+        }
+
+        public void setCompanyname(String companyname) {
+            this.companyname = companyname;
+        }
+
+        public String getCompanyaddress() {
+            return companyaddress;
+        }
+
+        public void setCompanyaddress(String companyaddress) {
+            this.companyaddress = companyaddress;
+        }
+
+        public String getCompanycorporation() {
+            return companycorporation;
+        }
+
+        public void setCompanycorporation(String companycorporation) {
+            this.companycorporation = companycorporation;
+        }
+
+        public String getUnifedsocialcreditcode() {
+            return unifedsocialcreditcode;
+        }
+
+        public void setUnifedsocialcreditcode(String unifedsocialcreditcode) {
+            this.unifedsocialcreditcode = unifedsocialcreditcode;
+        }
+
+        public Date getEstablish() {
+            return establish;
+        }
+
+        public void setEstablish(Date establish) {
+            this.establish = establish;
+        }
+
+        public Invoiceinfo getInvoiceinfo() {
+            return invoiceinfo;
+        }
+
+        public void setInvoiceinfo(Invoiceinfo invoiceinfo) {
+            this.invoiceinfo = invoiceinfo;
+        }
+
+        public Bankinfo getBankinfo() {
+            return bankinfo;
+        }
+
+        public void setBankinfo(Bankinfo bankinfo) {
+            this.bankinfo = bankinfo;
+        }
+
+        /*
+        公司名称	COMPANYNAME
+        纳税人识别号	DUTYNUMBER
+        地址	COMPANYADDRESS
+        电话	PHONE
+        银行名称	BANKNAME
+        开户行	BANKBRANCH
+        账号	BANKNUMBER
+        */
+        public class Invoiceinfo extends BaseModel {
+            private static final long serialVersionUID = 1L;
+
+            private String companyname;
+            private String dutynumber;
+            private String companyaddress;
+            private String phone;
+            private String bankname;
+            private String bankbranch;
+            private String banknumber;
+
+            public String getCompanyname() {
+                return companyname;
+            }
+
+            public void setCompanyname(String companyname) {
+                this.companyname = companyname;
+            }
+
+            public String getDutynumber() {
+                return dutynumber;
+            }
+
+            public void setDutynumber(String dutynumber) {
+                this.dutynumber = dutynumber;
+            }
+
+            public String getCompanyaddress() {
+                return companyaddress;
+            }
+
+            public void setCompanyaddress(String companyaddress) {
+                this.companyaddress = companyaddress;
+            }
+
+            public String getPhone() {
+                return phone;
+            }
+
+            public void setPhone(String phone) {
+                this.phone = phone;
+            }
+
+            public String getBankname() {
+                return bankname;
+            }
+
+            public void setBankname(String bankname) {
+                this.bankname = bankname;
+            }
+
+            public String getBankbranch() {
+                return bankbranch;
+            }
+
+            public void setBankbranch(String bankbranch) {
+                this.bankbranch = bankbranch;
+            }
+
+            public String getBanknumber() {
+                return banknumber;
+            }
+
+            public void setBanknumber(String banknumber) {
+                this.banknumber = banknumber;
+            }
+        }
+
+        /*
+        银行账户ID	BANKACCID
+        银行名称	BANKNAME
+        开户行	BANKBRANCH
+        银行账号	BANKNUMBER
+        公司ID	COMPANYID
+        */
+        public class Bankinfo extends BaseModel {
+            private static final long serialVersionUID = 1L;
+
+            private String bankaccid;
+            private String bankname;
+            private String bankbranch;
+            private String banknumber;
+            private String companyid;
+
+            public String getBankaccid() {
+                return bankaccid;
+            }
+
+            public void setBankaccid(String bankaccid) {
+                this.bankaccid = bankaccid;
+            }
+
+            public String getBankname() {
+                return bankname;
+            }
+
+            public void setBankname(String bankname) {
+                this.bankname = bankname;
+            }
+
+            public String getBankbranch() {
+                return bankbranch;
+            }
+
+            public void setBankbranch(String bankbranch) {
+                this.bankbranch = bankbranch;
+            }
+
+            public String getBanknumber() {
+                return banknumber;
+            }
+
+            public void setBanknumber(String banknumber) {
+                this.banknumber = banknumber;
+            }
+
+            public String getCompanyid() {
+                return companyid;
+            }
+
+            public void setCompanyid(String companyid) {
+                this.companyid = companyid;
+            }
+        }
     }
 
-    public void setDepartmentid(String departmentid) {
-        this.departmentid = departmentid;
-    }
 
-    public String getDepartmentname() {
-        return departmentname;
-    }
-
-    public void setDepartmentname(String departmentname) {
-        this.departmentname = departmentname;
-    }
 }
+
+
