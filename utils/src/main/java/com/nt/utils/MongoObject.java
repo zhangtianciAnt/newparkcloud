4d063e6649d5d65d4f38ef2c01da76b760145300
wenchao <wenchao@LAPTOP-U5867B76>
2018-10-22 16:28:50 +0800

    增加注释

--- utils/src/main/java/com/nt/utils/MongoObject.java
+++ utils/src/main/java/com/nt/utils/MongoObject.java
@@ -9,8 +9,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @类名称：MongoObject
+ * @描述：MongoDB数据库操作共通类
+ * @创建日期：2018/10/22
+ * @作者：赵国栋
+ */
 public class MongoObject {
-
+    /**
+     * @方法名：CustmizeUpdate
+     * @描述：更新数据
+     * @创建日期：2018/10/22
+     * @作者：赵国栋
+     * @参数：[obj：要更新的对象, UpWithoutNull：true空值不更新，false空值更新 ]
+     * @返回值：org.springframework.data.mongodb.core.query.Update
+     */
     public final static <T> Update CustmizeUpdate(T obj, Boolean UpWithoutNull) throws IllegalAccessException {
         Update update = new Update();
 
@@ -27,6 +40,14 @@ public class MongoObject {
         return update;
     }
 
+    /**
+     * @方法名：CustmizeQuery
+     * @描述：查询数据（自动判断是否带权限查询）
+     * @创建日期：2018/10/22
+     * @作者：赵国栋
+     * @参数：[obj：要查询的对象]
+     * @返回值：org.springframework.data.mongodb.core.query.Query
+     */
     public final static <T> Query CustmizeQuery(T obj) throws IllegalAccessException {
         Query query = new Query();
 
@@ -50,6 +71,14 @@ public class MongoObject {
         return query;
     }
 
+    /**
+     * @方法名：getAllField
+     * @描述：取得对象的所有字段
+     * @创建日期：2018/10/22
+     * @作者：赵国栋
+     * @参数：[obj]
+     * @返回值：java.lang.reflect.Field[]
+     */
     private final static <T> Field[] getAllField(T obj) {
         List<Field> fieldList = new ArrayList<>();
         Class tempClass = obj.getClass();
