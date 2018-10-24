e7aeaa35af08da519f6af39079525e5b4ab3401c
wenchao <wenchao@LAPTOP-U5867B76>
2018-10-24 16:13:04 +0800

    启动类增加跨域代码

--- controller/src/main/java/com/nt/controller/Start.java
+++ controller/src/main/java/com/nt/controller/Start.java
@@ -4,7 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.nt.*"})
@@ -14,5 +19,19 @@ public class Start {
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
         log.info("服务已启动！！！");
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(true);
+      config.addAllowedOrigin("*");
+      config.addAllowedHeader("*");
+      config.addAllowedMethod("*");
+      source.registerCorsConfiguration("/**", config);
+      FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+      bean.setOrder(0);
+      return bean;
     }
 }
