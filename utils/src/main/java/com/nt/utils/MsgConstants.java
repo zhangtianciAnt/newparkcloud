73bdeeb0f23f5d43a1e666e7ba450b5b458a3802
耿绍凯 <shaokai.geng@newtouch.cn>
2018-10-24 13:42:05 +0800

    message 共通文件更新

---
+++ utils/src/main/java/com/nt/utils/MsgConstants.java
@@ -0,0 +1,22 @@
+package com.nt.utils;
+
+/**
+ * 消息常量类
+ * 
+ * @author skaixx
+ */
+public class MsgConstants {
+
+    //region 参数相关message
+	// 参数类型错误
+	public final static String PARAM_ERR_01 = "PARAM_ERR_01";
+
+    // 参数为空
+    public final static String PARAM_ERR_02 = "PARAM_ERR_02";
+    //endregion
+
+    //region 程序异常message
+    // 操作异常，请稍后重试
+    public final static String EXCEPTION_ERR_O1 = "EXCEPTION_ERR_O1";
+    //endregion
+}
