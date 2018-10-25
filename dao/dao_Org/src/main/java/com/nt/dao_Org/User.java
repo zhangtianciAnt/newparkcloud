5a21febb679c2ac42cc8a636c49b117095399afa
赵国栋 <dong8@LAPTOP-95K23HII>
2018-10-25 10:02:32 +0800

    增加了lombok依赖

--- dao/dao_Org/src/main/java/com/nt/dao_Org/User.java
+++ dao/dao_Org/src/main/java/com/nt/dao_Org/User.java
@@ -1,47 +1,17 @@
 package com.nt.dao_Org;
 
 import com.nt.utils.BaseModel;
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.lang.reflect.InvocationTargetException;
 
 @Document(collection = "user")
+@Data
 public class User extends BaseModel {
     private String id;
     private String userName;
     private String passWord;
     private String uppassWord;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public String getUppassWord() {
-        return uppassWord;
-    }
-
-    public void setUppassWord(String uppassWord) {
-        this.uppassWord = uppassWord;
-    }
 
 }
