21a3736f04c91f23a80f147494e02263dea03fa2
耿绍凯 <shaokai.geng@newtouch.cn>
2018-11-29 13:48:43 +0800

    多余代码删除

--- services/service_Org/src/main/java/com/nt/service_Org/Impl/OrgTreeServiceImpl.java
+++ services/service_Org/src/main/java/com/nt/service_Org/Impl/OrgTreeServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
-
 import static com.nt.utils.MongoObject.CustmizeQuery;
 
 @Service
