1e49b5e2835f471994ba13cc6adee817bf005920
耿绍凯 <shaokai.geng@newtouch.cn>
2018-11-30 13:53:07 +0800

    新建租赁模块DAO工程

---
+++ dao/dao_Rent/src/main/java/com/nt/dao_Rent/RentCustomer.java
@@ -0,0 +1,16 @@
+package com.nt.dao_Rent;
+
+/**
+ * @ProjectName: newparkcloud
+ * @Package: com.nt.dao_Rent
+ * @ClassName: RentCustomer
+ * @Description: 类的描述
+ * @Author: SKAIXX
+ * @CreateDate: 2018/11/30
+ * @UpdateUser: SKAIXX
+ * @UpdateDate: 2018/11/30
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class RentCustomer {
+}
