c893bdbd78b909755541bc9b2d8bc22ec7ab41ab
xu.sun <xu.sun@newtouch.cn>
2018-12-04 09:08:24 +0800

    add 获取 消息接口

--- services/service_Org/src/main/java/com/nt/service_Org/ToDoNoticeService.java
+++ services/service_Org/src/main/java/com/nt/service_Org/ToDoNoticeService.java
@@ -1,8 +1,17 @@
 package com.nt.service_Org;
 
+
 import com.nt.dao_Org.ToDoNotice;
+import com.nt.dao_Org.ToDoNotice.Notices;
+
+import java.util.List;
 
 public interface ToDoNoticeService {
 
     void save(ToDoNotice toDoNotice) throws Exception;
+
+    //获取消息
+    List<ToDoNotice> get(ToDoNotice toDoNotice) throws Exception;
+
+    void updateNoticesStatus(ToDoNotice toDoNotice) throws Exception;
 }
