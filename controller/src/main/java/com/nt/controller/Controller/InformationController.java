49d3e9392b0b703daf2a9582e027f4df1b935104
xu.sun <xu.sun@newtouch.cn>
2018-12-06 15:40:03 +0800

    add 信息发布

---
+++ controller/src/main/java/com/nt/controller/Controller/InformationController.java
@@ -0,0 +1,61 @@
+package com.nt.controller.Controller;
+
+
+
+
+import com.nt.dao_Org.Information;
+import com.nt.service_Org.InformationService;
+import com.nt.utils.ApiResult;
+import com.nt.utils.MessageUtil;
+import com.nt.utils.MsgConstants;
+import com.nt.utils.dao.TokenModel;
+import com.nt.utils.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/information")
+public class InformationController {
+
+    @Autowired
+    private InformationService informationService;
+
+    @Autowired
+    private TokenService tokenService;
+
+
+    @RequestMapping(value = "/saveinformation", method = {RequestMethod.POST})
+    public ApiResult save(@RequestBody Information information, HttpServletRequest request) throws Exception {
+        if (information == null || StringUtils.isEmpty(information)) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.PARAM_ERR_02));
+        }
+        TokenModel tokenModel = tokenService.getToken(request);
+        information.preInsert(tokenModel);
+        informationService.save(information);
+        return ApiResult.success();
+    }
+
+    //获取消息
+    @RequestMapping(value = "/getinformation", method = {RequestMethod.GET})
+    public ApiResult get(HttpServletRequest request) throws Exception {
+        Information information = new Information();
+        TokenModel tokenModel = tokenService.getToken(request);
+        information.setTenantid(tokenModel.getTenantId());
+        information.setOwners(tokenModel.getOwnerList());
+        information.setIds(tokenModel.getIdList());
+        return ApiResult.success(informationService.get(information));
+    }
+
+
+
+
+
+
+}
