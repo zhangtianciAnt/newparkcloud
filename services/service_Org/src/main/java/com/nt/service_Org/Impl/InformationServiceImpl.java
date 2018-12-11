d52b4c7a1d5a25a1ff9216331e8d6941cda89e42
xu.sun <xu.sun@newtouch.cn>
2018-12-11 14:27:28 +0800

    【入驻服务】类，前端考虑无列表，发布时自动撤销上次发布信息。

--- services/service_Org/src/main/java/com/nt/service_Org/Impl/InformationServiceImpl.java
+++ services/service_Org/src/main/java/com/nt/service_Org/Impl/InformationServiceImpl.java
@@ -3,11 +3,15 @@ package com.nt.service_Org.Impl;
 import com.nt.dao_Org.Information;
 import com.nt.service_Org.InformationService;
 import com.nt.utils.AuthConstants;
+import com.nt.utils.dao.TokenModel;
+import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import javax.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
@@ -20,13 +24,42 @@ public class InformationServiceImpl implements InformationService {
     private MongoTemplate mongoTemplate;
 
     @Override
-    public void save(Information information) throws Exception {
+    public void save(Information information,TokenModel tokenModel) throws Exception {
+        if(information.getActivityinfo()!=null&&information.getActivityinfo().getSignupinfo()!=null)
+        {
+            information.getActivityinfo().preInsert(tokenModel);
             mongoTemplate.save(information);
+        }
+        else
+        {
+            if(information.getType().equals("8")||information.getType().equals("9")||information.getType().equals("10"))
+            {
+                Query query = new Query();
+                query.addCriteria(Criteria.where("type").is(information.getType()));
+                query.addCriteria(Criteria.where("releasestatus").is("1"));
+                List<Information> rst = mongoTemplate.find(query,Information.class);
+                if(rst.size() > 0){
+                    rst.get(0).setReleasestatus("2");
+                    mongoTemplate.save(rst.get(0));
+                    mongoTemplate.save(information);
+                }
+                else
+                {
+                    mongoTemplate.save(information);
+                }
+            }
+            else
+            {
+                mongoTemplate.save(information);
+            }
+
+        }
     }
 
     @Override
     public List<Information> get(Information information) throws Exception {
-        Query query = CustmizeQuery(information);
+        Query query = new Query();
+        query.with(new Sort(Sort.Direction.DESC,   "releasetime"));
         return mongoTemplate.find(query, Information.class);
     }
 }
