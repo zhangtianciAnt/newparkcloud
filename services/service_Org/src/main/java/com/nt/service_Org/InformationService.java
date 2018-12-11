aadf2c6d6e248883032587aa2a6321e4353249a6
xu.sun <xu.sun@newtouch.cn>
2018-12-11 14:26:10 +0800

    修改

--- services/service_Org/src/main/java/com/nt/service_Org/InformationService.java
+++ services/service_Org/src/main/java/com/nt/service_Org/InformationService.java
@@ -2,13 +2,16 @@ package com.nt.service_Org;
 
 
 import com.nt.dao_Org.Information;
+import com.nt.utils.dao.TokenModel;
+
+import javax.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
 public interface InformationService {
 
     //保存
-    void save(Information information) throws Exception;
+    void save(Information information, TokenModel tokenModel) throws Exception;
 
     //获取
     List<Information> get(Information information) throws Exception;
