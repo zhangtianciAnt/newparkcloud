8f66fc2f6e45a7a53f87d38429280852894264ce
xu.sun <xu.sun@newtouch.cn>
2018-12-14 17:03:36 +0800

    add 工单管理

---
+++ services/service_Org/src/main/java/com/nt/service_Org/Impl/WorkOrderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.nt.service_Org.Impl;
+import com.nt.dao_Org.WorkOrder;
+import com.nt.service_Org.WorkOrderService;
+import com.nt.utils.dao.TokenModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.nt.utils.MongoObject.CustmizeQuery;
+
+@Service
+public class WorkOrderServiceImpl implements WorkOrderService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * @方法名：save
+     * @描述：信息发布保存
+     * @创建日期：2018/12/13
+     * @作者：SUNXU
+     * @参数：[information, request]
+     * @返回值：
+     */
+    @Override
+    public void save(WorkOrder workorder, TokenModel tokenModel) throws Exception {
+        mongoTemplate.save(workorder);
+    }
+
+    /**
+     * @方法名：get
+     * @描述：获取信息发布列表
+     * @创建日期：2018/12/13
+     * @作者：SUNXU
+     * @参数：[information, request]
+     * @返回值：Information
+     */
+    @Override
+    public List<WorkOrder> get(WorkOrder workorder) throws Exception {
+        //共同带权限查询
+        Query query = CustmizeQuery(workorder);
+        return mongoTemplate.find(query, WorkOrder.class);
+    }
+}
