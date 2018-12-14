591582b8a0d5f53e0b156f8a1432977e432048c5
xu.sun <xu.sun@newtouch.cn>
2018-12-14 17:03:12 +0800

    add 工单管理

---
+++ services/service_Org/src/main/java/com/nt/service_Org/WorkOrderService.java
@@ -0,0 +1,16 @@
+package com.nt.service_Org;
+
+
+import com.nt.dao_Org.WorkOrder;
+import com.nt.utils.dao.TokenModel;
+import java.util.List;
+
+public interface WorkOrderService {
+
+    //保存
+    void save(WorkOrder workorder, TokenModel tokenModel) throws Exception;
+
+    //获取
+    List<WorkOrder> get(WorkOrder workorder) throws Exception;
+
+}
