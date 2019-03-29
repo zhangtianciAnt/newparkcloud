9df97d6fcf5d19e10dff0cd91f3b930c3c2b095e
赵国栋 <guodong.zhao@newtouch.cn>
2019-03-29 11:48:38 +0800

    添加视频功能

--- controller/src/main/java/com/nt/controller/Start.java
+++ controller/src/main/java/com/nt/controller/Start.java
@@ -1,9 +1,11 @@
 package com.nt.controller;
 
-import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.avcodec;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -19,31 +21,18 @@ import org.springframework.web.filter.CorsFilter;
 import javax.swing.*;
 import java.awt.*;
 
+import static com.nt.controller.DetectFace.Camera.recordCamera;
+
 @SpringBootApplication
 @ComponentScan(basePackages={"com.nt.*"})
 public class Start {
 
     private static Logger log = LoggerFactory.getLogger(Start.class);
-    public static void main(String[] args) throws FrameGrabber.Exception, InterruptedException {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Start.class, args);
         log.info("服务已启动！！！");
-
-        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-        grabber.start();   //开始获取摄像头数据
-        CanvasFrame canvas = new CanvasFrame("摄像头");//新建一个窗口
-        canvas.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        canvas.setAlwaysOnTop(true);
-        while (true) {
-            if (!canvas.isDisplayable()) {//窗口是否关闭
-                grabber.stop();//停止抓取
-                System.exit(-1);//退出
-            }
-
-            Frame frame = grabber.grab();
-
-            canvas.showImage(frame);//获取摄像头图像并放到窗口上显示， 这里的Frame frame=grabber.grab(); frame是一帧视频图像
-            Thread.sleep(50);//50毫秒刷新一次图像
-        }
+        System.setProperty("java.awt.headless", "false");
+        recordCamera("rtmp://59.80.34.104:1935/hls/test",25);
 
     }
 
@@ -60,4 +49,6 @@ public class Start {
       bean.setOrder(0);
       return bean;
     }
+
+
 }
