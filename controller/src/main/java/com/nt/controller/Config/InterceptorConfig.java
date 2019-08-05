9753bf6bdd3209eb2413b83ee02e5299f6c61187
xyq222085@163.com <xyq222085@163.com>
2019-08-05 16:14:16 +0800

    平台管理

--- controller/src/main/java/com/nt/controller/Config/InterceptorConfig.java
+++ controller/src/main/java/com/nt/controller/Config/InterceptorConfig.java
@@ -24,7 +24,8 @@ public class InterceptorConfig extends WebMvcConfigurerAdapter {
                 .excludePathPatterns("/SignIn/*")
                 .excludePathPatterns("/weChat/getWeChatUserInfo")
                 .excludePathPatterns("/verificationCode/ckVerificationCode")
-                .excludePathPatterns("/verificationCode/sendVerificationCode");
+                .excludePathPatterns("/verificationCode/sendVerificationCode")
+                .excludePathPatterns("/PMInformationDelivery/selectPMInformationDelivery");
     }
 
     @Bean
