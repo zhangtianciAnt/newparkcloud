9caff70994ef3ad071b0db9790473d9eb54f7d8e
赵国栋 <guodong.zhao@newtouch.cn>
2019-10-09 18:46:33 +0800

    审批

--- dao/dao_Org/src/main/java/com/nt/dao_Org/Dictionary.java
+++ dao/dao_Org/src/main/java/com/nt/dao_Org/Dictionary.java
@@ -5,24 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Table(name = "dictionary")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Dictionary extends BaseModel {
+public class Dictionary implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
+    @Column(name = "CODE")
     private String code;
 
-
+    @Column(name = "TYPE")
     private String type;
 
-
+    @Column(name = "VALUE")
     private String value;
 
-
+    @Column(name = "PCODE")
     private String pcode;
 
 }
--- services/service_Org/src/main/java/com/nt/service_Org/mapper/DictionaryMapper.java
+++ services/service_Org/src/main/java/com/nt/service_Org/mapper/DictionaryMapper.java
@@ -1,14 +1,13 @@
 package com.nt.service_Org.mapper;
 
 import com.nt.dao_Org.Dictionary;
-import com.nt.dao_Org.ToDoNotice;
 import com.nt.utils.MyMapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 
-public interface DictionaryMapper extends MyMapper<ToDoNotice> {
+public interface DictionaryMapper extends MyMapper<Dictionary> {
 
-    List<Dictionary> getForSelect(@Param("scode") String scode, @Param("ecode")String ecode) throws Exception;
+    List<Dictionary> getForSelect();
 }
