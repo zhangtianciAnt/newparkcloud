81d0f7ff9dab0b97e623dd647aeeeb326ab22680
费家亮 <jialiang.fei@newtouch.com>
2019-10-14 09:57:27 +0800

    Service 修改

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/AbNormalService.java
@@ -0,0 +1,8 @@
+package com.nt.service_pfans.PFANS2000;
+
+import com.nt.dao_Pfans.PFANS2000.AbNormal;
+import com.nt.utils.dao.TokenModel;
+
+public interface AbNormalService {
+    public void insert(AbNormal abNormal, TokenModel tokenModel)throws Exception;
+}
