aa3ff4dfd57f153ef002d0ce73abaa9b9a53b609
高兵兵 <bingbing.gao@newtouch.com>
2019-10-17 10:46:15 +0800

    2011添加

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/OvertimeMapper.java
@@ -0,0 +1,13 @@
+package com.nt.service_pfans.PFANS2000.mapper;
+
+import com.nt.dao_Pfans.PFANS2000.Overtime;
+import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface OvertimeMapper extends MyMapper<Overtime> {
+    List<Overtime> getOvertime();
+    int insert(@Param("overtime") Overtime overtime);
+    int updateByPrimaryKey(@Param("overtime") Overtime overtime);
+}
