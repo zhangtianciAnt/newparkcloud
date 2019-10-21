2503875b9ce3965596d25aeda9af9b6657c192ea
黄博 <bo.huang@newtouch.com>
2019-10-21 10:52:56 +0800

    UPDATE

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/GoalManagementService.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/GoalManagementService.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface GoalManagementService {
     public void insert(GoalManagement goalmanagement, TokenModel tokenModel)throws Exception;
     public List<GoalManagement> list(GoalManagement goalmanagement) throws Exception;
+    public void upd(GoalManagement management, TokenModel tokenModel)throws Exception;
     GoalManagement One(String goalmanagement_id) throws Exception;
 }
