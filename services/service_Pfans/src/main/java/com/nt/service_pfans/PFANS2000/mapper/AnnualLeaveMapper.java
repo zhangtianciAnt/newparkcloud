31b47e7237f7aa28c1814c143c56a7cc12270457
linmin <zhiming.zhang@newtouch.com>
2019-10-22 11:44:18 +0800

    pfans2013

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/AnnualLeaveMapper.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/AnnualLeaveMapper.java
@@ -3,15 +3,6 @@ package com.nt.service_pfans.PFANS2000.mapper;
 import com.nt.dao_Pfans.PFANS2000.AnnualLeave;
 import com.nt.utils.MyMapper;
 
-import java.util.List;
-
 public interface AnnualLeaveMapper extends MyMapper<AnnualLeave> {
 
-    /**
-     * 获取休假列表
-     *
-     * @param annualLeave
-     * @return
-     */
-    List<AnnualLeave> getDataList(AnnualLeave annualLeave);
 }
