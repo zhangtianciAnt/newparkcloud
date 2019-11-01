99f1e52e10efaf99565855a7684b38cd4b516d85
费家亮 <jialiang.fei@newtouch.com>
2019-11-01 15:21:24 +0800

    机票后台接口编写  （代码格式化）

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans2016Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans2016Controller.java
@@ -2,7 +2,10 @@ package com.nt.controller.Controller.PFANS;
 
 import com.nt.dao_Pfans.PFANS2000.AbNormal;
 import com.nt.service_pfans.PFANS2000.AbNormalService;
-import com.nt.utils.*;
+import com.nt.utils.ApiResult;
+import com.nt.utils.MessageUtil;
+import com.nt.utils.MsgConstants;
+import com.nt.utils.RequestUtils;
 import com.nt.utils.dao.TokenModel;
 import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,35 +26,35 @@ public class Pfans2016Controller {
     @Autowired
     private TokenService tokenService;
 
-    @RequestMapping(value="/list", method={RequestMethod.POST})
-    public ApiResult list(HttpServletRequest request) throws Exception{
-            TokenModel tokenModel = tokenService.getToken(request);
-            AbNormal abNormal = new AbNormal();
-            abNormal.setOwners(tokenModel.getOwnerList());
-            return ApiResult.success(abNormalService.list(abNormal));
+    @RequestMapping(value = "/list", method = {RequestMethod.POST})
+    public ApiResult list(HttpServletRequest request) throws Exception {
+        TokenModel tokenModel = tokenService.getToken(request);
+        AbNormal abNormal = new AbNormal();
+        abNormal.setOwners(tokenModel.getOwnerList());
+        return ApiResult.success(abNormalService.list(abNormal));
     }
 
-    @RequestMapping(value = "/insertInfo",method={RequestMethod.POST})
+    @RequestMapping(value = "/insertInfo", method = {RequestMethod.POST})
     public ApiResult create(@RequestBody AbNormal abNormal, HttpServletRequest request) throws Exception {
         if (abNormal == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         TokenModel tokenModel = tokenService.getToken(request);
-        abNormalService.insert(abNormal,tokenModel);
+        abNormalService.insert(abNormal, tokenModel);
         return ApiResult.success();
     }
 
-    @RequestMapping(value="/updateInfo",method = {RequestMethod.POST})
-    public ApiResult updateInformation(@RequestBody AbNormal abNormal, HttpServletRequest request) throws Exception{
+    @RequestMapping(value = "/updateInfo", method = {RequestMethod.POST})
+    public ApiResult updateInformation(@RequestBody AbNormal abNormal, HttpServletRequest request) throws Exception {
         if (abNormal == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         TokenModel tokenModel = tokenService.getToken(request);
-        abNormalService.upd(abNormal,tokenModel);
+        abNormalService.upd(abNormal, tokenModel);
         return ApiResult.success();
     }
 
-    @RequestMapping(value = "/oneInfo",method={RequestMethod.POST})
+    @RequestMapping(value = "/oneInfo", method = {RequestMethod.POST})
     public ApiResult one(@RequestBody AbNormal abNormal, HttpServletRequest request) throws Exception {
         if (abNormal == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
---
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans3001Controller.java
@@ -0,0 +1,65 @@
+package com.nt.controller.Controller.PFANS;
+
+import com.nt.dao_Pfans.PFANS3000.Tickets;
+import com.nt.service_pfans.PFANS3000.TicketsService;
+import com.nt.utils.ApiResult;
+import com.nt.utils.MessageUtil;
+import com.nt.utils.MsgConstants;
+import com.nt.utils.RequestUtils;
+import com.nt.utils.dao.TokenModel;
+import com.nt.utils.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tickets")
+public class Pfans3001Controller {
+
+    @Autowired
+    private TicketsService ticketsService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @RequestMapping(value = "/insertInfo", method = {RequestMethod.POST})
+    public ApiResult insertInfo(@RequestBody Tickets tickets, HttpServletRequest request) throws Exception {
+        if (tickets == null) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        TokenModel tokenModel = tokenService.getToken(request);
+        ticketsService.insert(tickets, tokenModel);
+        return ApiResult.success();
+    }
+
+    @RequestMapping(value = "/list", method = {RequestMethod.POST})
+    public ApiResult list(HttpServletRequest request) throws Exception {
+        TokenModel tokenModel = tokenService.getToken(request);
+        Tickets tickets = new Tickets();
+        tickets.setOwners(tokenModel.getOwnerList());
+        return ApiResult.success(ticketsService.list(tickets));
+    }
+
+    @RequestMapping(value = "updateInfo", method = {RequestMethod.POST})
+    public ApiResult updateInfo(@RequestBody Tickets tickets, HttpServletRequest request) throws Exception {
+        if (tickets == null) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        TokenModel tokenModel = tokenService.getToken(request);
+        ticketsService.update(tickets, tokenModel);
+        return ApiResult.success();
+    }
+
+    @RequestMapping(value = "oneInfo", method = {RequestMethod.POST})
+    public ApiResult one(@RequestBody Tickets tickets, HttpServletRequest request) throws Exception {
+        if (tickets == null) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        return ApiResult.success(ticketsService.One(tickets.getTickets_id()));
+    }
+
+}
