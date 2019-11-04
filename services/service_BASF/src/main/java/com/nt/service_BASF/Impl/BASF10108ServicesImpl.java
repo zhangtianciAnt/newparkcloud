a3c14e82426a504690a9f8150e5c8037e8c3e059
xinxin.li@newtouch.com <xinxin.li@newtouch.com>
2019-11-04 18:49:31 +0900

    BASF10108 INSERT

--- services/service_BASF/src/main/java/com/nt/service_BASF/Impl/BASF10108ServicesImpl.java
+++ services/service_BASF/src/main/java/com/nt/service_BASF/Impl/BASF10108ServicesImpl.java
@@ -4,6 +4,7 @@ package com.nt.service_BASF.Impl;
 import com.nt.dao_BASF.Alarmreceipt;
 import com.nt.service_BASF.BASF10108Services;
 import com.nt.service_BASF.mapper.AlarmreceiptMapper;
+import com.nt.utils.dao.TokenModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -24,5 +26,12 @@ public class BASF10108ServicesImpl implements BASF10108Services {
     @Override
     public List<Alarmreceipt> getList(Alarmreceipt alarmreceipt) throws Exception {
         return alarmreceiptMapper.select(alarmreceipt);
+    }
+
+    @Override
+    public void insert(Alarmreceipt alarmreceipt, TokenModel tokenModel) throws Exception {
+        alarmreceipt.preInsert(tokenModel);
+        alarmreceipt.setAlarmreceiptid(UUID.randomUUID().toString());
+        alarmreceiptMapper.insert(alarmreceipt);
     }
 }
