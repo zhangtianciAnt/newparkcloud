d14ca86c1e751bdbebae2469fc1cb25f7c13cbb1
sunxu <xu.sun@newtouch.cn>
2019-11-05 16:02:06 +0800

    add 用户组明细

---
+++ services/service_BASF/src/main/java/com/nt/service_BASF/mapper/UsergroupdetailedMapper.java
@@ -0,0 +1,9 @@
+package com.nt.service_BASF.mapper;
+
+import com.nt.dao_BASF.Usergroupdetailed;
+import com.nt.utils.MyMapper;
+
+public interface UsergroupdetailedMapper extends MyMapper<Usergroupdetailed> {
+
+
+}
