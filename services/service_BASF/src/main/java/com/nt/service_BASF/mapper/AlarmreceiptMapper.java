878b75a35f1799401ddc67847f856c3f868acda9
xinxin.li@newtouch.com <xinxin.li@newtouch.com>
2019-11-05 16:14:32 +0900

    UPDATE BASF10108

--- services/service_BASF/src/main/java/com/nt/service_BASF/mapper/AlarmreceiptMapper.java
+++ services/service_BASF/src/main/java/com/nt/service_BASF/mapper/AlarmreceiptMapper.java
@@ -1,7 +1,15 @@
 package com.nt.service_BASF.mapper;
 
 import com.nt.dao_BASF.Alarmreceipt;
+import com.nt.dao_BASF.VO.AlarmreceiptVo;
 import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface AlarmreceiptMapper extends MyMapper<Alarmreceipt> {
+
+    List<AlarmreceiptVo> selectAlarmreceiptVoList();
+
+    AlarmreceiptVo selectAlarmreceiptVo(@Param("alarmreceiptid") String alarmreceiptid);
 }
