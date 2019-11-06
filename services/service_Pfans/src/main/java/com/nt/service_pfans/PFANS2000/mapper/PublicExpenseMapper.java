bc8ce0acc6756175f507eb1917483be64afa2ca5
赵国栋 <guodong.zhao@newtouch.cn>
2019-11-06 16:48:30 +0800

    1012

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/PublicExpenseMapper.java
@@ -0,0 +1,8 @@
+package com.nt.service_pfans.PFANS2000.mapper;
+
+import com.nt.dao_Pfans.PFANS1000.PublicExpense;
+import com.nt.utils.MyMapper;
+
+public interface PublicExpenseMapper extends MyMapper<PublicExpense> {
+
+}
