206f7c4e91ae7a8149d9579dbac58788f52b3476
LiXinYu <lixinyu521>
2019-11-07 10:52:55 +0800

    aa

--- controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF10102Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF10102Controller.java
@@ -1,11 +1,9 @@
 package com.nt.controller.Controller.BASF.BASFLANController;
 
-
-import com.nt.dao_BASF.Usergroup;
-import com.nt.dao_BASF.Usergroupdetailed;
-import com.nt.dao_BASF.VO.UsergroupVo;
-import com.nt.service_BASF.UsergroupServices;
+import com.nt.dao_BASF.User;
+import com.nt.service_BASF.UserService;
 import com.nt.utils.ApiResult;
+import com.nt.utils.AuthConstants;
 import com.nt.utils.dao.TokenModel;
 import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,42 +19,44 @@ import javax.servlet.http.HttpServletRequest;
  * @Package: com.nt.controller.Controller.BASF.BASFLANController
  * @ClassName: BASF10102Controller
  * @Author: LXY
- * @Description: BASF用户组Controller
- * @Date: 2019/11/6 22.31
+ * @Description: BASF用户通讯录Controller
+ * @Date: 2019/11/6 22.40
  * @Version: 1.0
  */
 @RestController
-@RequestMapping("/BASF10103")
+@RequestMapping("/BASF10102")
 public class BASF10102Controller {
-
-    @Autowired
-    private UsergroupServices usergroupServices;
 
     @Autowired
     private TokenService tokenService;
 
-    @RequestMapping(value = "/list", method = {RequestMethod.POST})
-    public ApiResult list(@RequestBody Usergroup usergroup ,HttpServletRequest request) throws Exception {
-        return ApiResult.success(usergroupServices.list(usergroup));
-    }
+    @Autowired
+    private UserService userService;
 
-    @RequestMapping(value = "/getOneUserGroupDetailed", method = {RequestMethod.POST})
-    public ApiResult getOneUserGroupDetailed(@RequestBody Usergroupdetailed usergroupdetailed, HttpServletRequest request) throws Exception {
-
-        return ApiResult.success(usergroupServices.getDetailedList(usergroupdetailed));
+    @RequestMapping(value = "/get", method = {RequestMethod.POST})
+    public ApiResult get(@RequestBody User user) throws Exception {
+        return ApiResult.success(userService.get(user));
     }
 
     @RequestMapping(value = "/insert", method = {RequestMethod.POST})
-    public ApiResult insert(@RequestBody UsergroupVo usergroupVo, HttpServletRequest request) throws Exception {
+    public ApiResult insert(@RequestBody User user, HttpServletRequest request) throws Exception {
         TokenModel tokenModel = tokenService.getToken(request);
-        usergroupServices.insert(tokenModel,usergroupVo);
+        userService.insert(tokenModel, user);
         return ApiResult.success();
     }
 
     @RequestMapping(value = "/update", method = {RequestMethod.POST})
-    public ApiResult update(@RequestBody UsergroupVo usergroupVo, HttpServletRequest request) throws Exception {
+    public ApiResult update(@RequestBody User user, HttpServletRequest request) throws Exception {
         TokenModel tokenModel = tokenService.getToken(request);
-        usergroupServices.update(tokenModel,usergroupVo);
+        userService.update(tokenModel, user);
+        return ApiResult.success();
+    }
+
+    @RequestMapping(value = "/del", method = {RequestMethod.POST})
+    public ApiResult del(@RequestBody User user, HttpServletRequest request) throws Exception {
+        TokenModel tokenModel = tokenService.getToken(request);
+        user.setStatus(AuthConstants.DEL_FLAG_DELETE);
+        userService.del(tokenModel, user);
         return ApiResult.success();
     }
 }
