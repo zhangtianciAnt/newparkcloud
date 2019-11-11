3aceaa3209861c3f33f21c010a0ad3fd0cf0241f
高兵兵 <bingbing.gao@newtouch.com>
2019-11-11 09:40:32 +0800

    工资管理添加

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/WagesMapper.java
@@ -0,0 +1,12 @@
+package com.nt.service_pfans.PFANS2000.mapper;
+
+import com.nt.dao_Pfans.PFANS2000.FlexibleWork;
+import com.nt.utils.MyMapper;
+
+import java.util.List;
+
+
+public interface WagesMapper extends MyMapper<FlexibleWork>{
+
+
+}
