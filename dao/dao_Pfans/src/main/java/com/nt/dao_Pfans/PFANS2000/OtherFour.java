4316880fc29a574520a274c42c38cc5ec8b97276
黄博 <bo.huang@newtouch.com>
2019-11-20 10:14:09 +0800

    otherfour

---
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/OtherFour.java
@@ -0,0 +1,35 @@
+package com.nt.dao_Pfans.PFANS2000;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "OtherFour")
+public class OtherFour extends BaseModel {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name = "OTHERFOUR_ID")
+    private String otherfour_id;
+    @Column(name = "DEPARTMENT_ID")
+    private String department_id;
+    @Column(name = "GIVING_ID")
+    private String giving_id;
+    @Column(name = "SOCIALSECURITY")
+    private String socialsecurity;
+    @Column(name = "USER_ID")
+    private String user_id;
+    @Column(name = "TOTAL")
+    private String total;
+    @Column(name = "REMARKS")
+    private String remarks;
+    @Column(name = "ROWINDEX")
+    private Integer rowindex;
+}
