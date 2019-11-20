83f50f36a11bad964f34575813c12d9ba548e40f
刘德越 <9831848145@163.COM>
2019-11-20 10:14:59 +0800

    1013后台

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/mapper/EvectionMapper.java
@@ -0,0 +1,7 @@
+package com.nt.service_pfans.PFANS1000.mapper;
+
+import com.nt.dao_Pfans.PFANS1000.Evection;
+import com.nt.utils.MyMapper;
+
+public interface EvectionMapper extends MyMapper<Evection> {
+}
