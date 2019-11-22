667ac76a5273709297836bc830229a10a992e167
王帅 <shuai.wang1@newtouch.com>
2019-11-22 10:33:53 +0800

    王帅：后台修改

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/AppreciationService.java
@@ -0,0 +1,21 @@
+package com.nt.service_pfans.PFANS2000;
+
+import com.nt.dao_Pfans.PFANS2000.Appreciation;
+import com.nt.utils.dao.TokenModel;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public interface AppreciationService {
+
+    void insert(Appreciation appreciation, TokenModel tokenModel) throws Exception;
+
+
+    List<Appreciation> list(Appreciation appreciation) throws Exception;
+
+
+    void deletete(Appreciation appreciation, TokenModel tokenModel)throws Exception;
+
+    List<String> importUser(HttpServletRequest request, TokenModel tokenModel) throws Exception ;
+
+}
