0c223e1b0e2173002a6a8c37b6fa575d5b812c8b
王帅 <shuai.wang1@newtouch.com>
2019-11-29 18:09:58 +0800

    ws

---
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/OtherTwo2.java
@@ -0,0 +1,46 @@
+package com.nt.dao_Pfans.PFANS2000;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "othertwo2")
+public class OtherTwo2 extends BaseModel {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @Column(name = "OTHERTWO_ID")
+    private String othertwo_id;
+
+    @Column(name = "GIVING_ID")
+    private String giving_id;
+
+    @Column(name = "JOBNUMBER")
+    private String jobnumber;
+
+    @Column(name = "USER_ID")
+    private String user_id;
+
+    @Column(name = "MONEYS")
+    private String moneys;
+
+    @Column(name = "ROOTKNOT")
+    private String rootknot;
+    
+    @Column(name = "TYPE")
+    private String type;
+
+    @Column(name = "ROWINDEX")
+    private Integer rowindex;
+
+}
