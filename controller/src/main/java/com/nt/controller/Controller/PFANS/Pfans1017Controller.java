da51cf4ebe92b022239d72e23c06e195d190dbc6
敖婷婷 <tingting.ao@newtouch.com>
2019-12-04 17:36:45 +0800

    1017提交

---
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1017Controller.java
@@ -0,0 +1,67 @@
+package com.nt.controller.Controller.PFANS;
+
+import com.nt.dao_Pfans.PFANS1000.Psdcd;
+import com.nt.service_pfans.PFANS1000.PsdcdService;
+import com.nt.utils.ApiResult;
+import com.nt.utils.MessageUtil;
+import com.nt.utils.MsgConstants;
+import com.nt.utils.RequestUtils;
+import com.nt.utils.dao.TokenModel;
+import com.nt.utils.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/psdcd")
+public class Pfans1017Controller {
+
+//    psdcd_id
+    @Autowired
+    private PsdcdService psdcdService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @RequestMapping(value="/get",method = {RequestMethod.GET})
+    public ApiResult getPsdcd(HttpServletRequest request)throws  Exception{
+        TokenModel tokenModel = tokenService.getToken(request);
+        Psdcd psdcd = new Psdcd();
+        psdcd.setOwners(tokenModel.getOwnerList());
+        return ApiResult.success(psdcdService.getPsdcd(psdcd));
+
+    }
+
+    @RequestMapping(value = "/one",method={RequestMethod.POST})
+    public ApiResult one(@RequestBody Psdcd psdcd, HttpServletRequest request) throws Exception {
+        if (psdcd == null) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        TokenModel tokenModel = tokenService.getToken(request);
+        return ApiResult.success(psdcdService.One(psdcd.getPsdcd_id()));
+    }
+
+    @RequestMapping(value="/update",method = {RequestMethod.POST})
+    public ApiResult updateFlexibleWork(@RequestBody Psdcd psdcd, HttpServletRequest request) throws Exception{
+        if (psdcd == null) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        TokenModel tokenModel = tokenService.getToken(request);
+        psdcdService.updatePsdcd(psdcd,tokenModel);
+        return ApiResult.success();
+    }
+
+    @RequestMapping(value = "/create",method={RequestMethod.POST})
+    public ApiResult create(@RequestBody Psdcd psdcd, HttpServletRequest request) throws Exception {
+        if (psdcd == null) {
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        TokenModel tokenModel = tokenService.getToken(request);
+        psdcdService.insert(psdcd,tokenModel);
+        return ApiResult.success();
+    }
+}
---
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS1000/Psdcd.java
@@ -0,0 +1,174 @@
+package com.nt.dao_Pfans.PFANS1000;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "psdcd")
+public class Psdcd extends BaseModel {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *PSDCD ID申請ID
+     */
+    @Id
+    @Column(name = "PSDCD_ID")
+    private String psdcd_id;
+
+    /**
+     * 所属センター
+     */
+    @Column(name = "CENTER_ID")
+    private String center_id;
+
+    /**
+     * 所属グループ
+     */
+    @Column(name = "GROUP_ID")
+    private String group_id;
+
+    /**
+     * 所属チーム
+     */
+    @Column(name = "TEAM_ID")
+    private String team_id;
+
+
+    /**
+     * 申请人
+     */
+    @Column(name = "USER_ID")
+    private String user_id;
+
+    /**
+     * 申請種類
+     */
+    @Column(name = "TYPE")
+    private String type;
+
+    /**
+     * サブ種類
+     */
+    @Column(name = "SUBTYPE")
+    private String subtype;
+
+    /**
+     * 申請日付
+     */
+    @Column(name = "APPLICATION")
+    private String application;
+
+    /**
+     * 联系邮箱
+     */
+    @Column(name = "EMAIL")
+    private String email;
+
+    /**
+     * 内缐
+     */
+    @Column(name = "EXTENSION")
+    private String extension;
+
+    /**
+     * ID申請穜别
+     */
+    @Column(name = "IDTYPE")
+    private String idtype;
+
+    /**
+     * 番号
+     */
+    @Column(name = "NUMBER")
+    private String number;
+
+    /**
+     * ユーザ種類
+     */
+    @Column(name = "USERTYPE")
+    private String usertype;
+
+    /**
+     * ユーザ氏名
+     */
+    @Column(name = "USERNAME")
+    private String username;
+
+    /**
+     * 姓
+     */
+    @Column(name = "SURNAME")
+    private String surname;
+
+    /**
+     * 名
+     */
+    @Column(name = "MING")
+    private String ming;
+
+    /**
+     * 元のメールアカウント
+     */
+    @Column(name = "ACCOUNT")
+    private String account;
+
+    /**
+     * 社外送信
+     */
+    @Column(name = "TRANSMISSION")
+    private String transmission;
+
+    /**
+     * 期待時間
+     */
+    @Column(name = "WAITFORTIME")
+    private String waitfortime;
+
+    /**
+     * 预算单位
+     */
+    @Column(name = "BUDGETUNIT")
+    private String budgetunit;
+
+    /**
+     * サイボウズAccount
+     */
+    @Column(name = "CYBOZU")
+    private String cybozu;
+
+    /**
+     * A期待時間
+     */
+    @Column(name = "EXPECTTIME")
+    private String expecttime;
+
+    /**
+     * Domain Account
+     */
+    @Column(name = "DOMAINACCOUNT")
+    private String domainaccount;
+
+    /**
+     * D期待時間
+     */
+    @Column(name = "FORWARDTIME")
+    private String forwardtime;
+
+    /**
+     * 備考
+     */
+    @Column(name = "PREPAREFOR")
+    private String preparefor;
+
+
+
+}
