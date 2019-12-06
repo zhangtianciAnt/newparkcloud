89f68c06109c5dbc85b7183c01aefba4f7d629f3
王帅 <shuai.wang1@newtouch.com>
2019-12-06 16:19:35 +0800

    打卡记录修改

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/PunchcardRecordServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/PunchcardRecordServiceImpl.java
@@ -4,7 +4,9 @@ import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import com.nt.dao_Org.CustomerInfo;
 import com.nt.dao_Pfans.PFANS2000.PunchcardRecord;
+import com.nt.dao_Pfans.PFANS2000.PunchcardRecordDetail;
 import com.nt.service_pfans.PFANS2000.PunchcardRecordService;
+import com.nt.service_pfans.PFANS2000.mapper.PunchcardRecordDetailMapper;
 import com.nt.service_pfans.PFANS2000.mapper.PunchcardRecordMapper;
 import com.nt.utils.LogicalException;
 import com.nt.utils.dao.TokenModel;
@@ -33,18 +35,21 @@ public class PunchcardRecordServiceImpl implements PunchcardRecordService {
     private PunchcardRecordMapper punchcardrecordMapper;
 
     @Autowired
+    private PunchcardRecordDetailMapper punchcardrecorddetailmapper;
+
+    @Autowired
     private MongoTemplate mongoTemplate;
 
     @Override
     public List<PunchcardRecord> list(PunchcardRecord punchcardrecord) throws Exception {
-        return punchcardrecordMapper.select(punchcardrecord);
+        return  punchcardrecordMapper.getPunchCardRecord();
     }
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
     public List<String> importUser(HttpServletRequest request, TokenModel tokenModel) throws Exception {
         try {
-            List<PunchcardRecord> listVo = new ArrayList<PunchcardRecord>();
+            List<PunchcardRecordDetail> listVo = new ArrayList<PunchcardRecordDetail>();
             List<String> Result = new ArrayList<String>();
             MultipartFile file = ((MultipartHttpServletRequest) request).getFile("file");
             File f = null;
@@ -55,9 +60,7 @@ public class PunchcardRecordServiceImpl implements PunchcardRecordService {
             List<Object> model = new ArrayList<Object>();
             model.add("工号");
             model.add("姓名");
-            model.add("日期");
-            model.add("首次打卡");
-            model.add("末次打卡");
+            model.add("打卡时间");
             List<Object> key = list.get(0);
             for (int i = 0; i < key.size(); i++) {
                 if (!key.get(i).toString().trim().equals(model.get(i))) {
@@ -68,23 +71,14 @@ public class PunchcardRecordServiceImpl implements PunchcardRecordService {
             int accesscount = 0;
             int error = 0;
             for (int i = 1; i < list.size(); i++) {
-                PunchcardRecord punchcardrecord = new PunchcardRecord();
+                PunchcardRecordDetail punchcardrecorddetail = new PunchcardRecordDetail();
                 List<Object> value = list.get(k);
                 k++;
                 if (value != null && !value.isEmpty()) {
                     if (value.get(0).toString().equals("")) {
                         continue;
                     }
-                    SimpleDateFormat sf1 = new SimpleDateFormat("yyyy-MM-dd");
                     SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    String Time_start = value.get(3).toString();
-                    String Time_end = value.get(4).toString();
-                    int result1 = Time_start.compareTo(Time_end);
-                    if (result1 >= 0) {
-                        error = error + 1;
-                        Result.add("模板第" + (k-1) + "行的时间格式错误，开始时间不可以大于或等于结束时间，导入失败");
-                        continue;
-                    }
                     if (value.size() > 1) {
                         String date = value.get(2).toString();
                         String date1 = value.get(2).toString();
@@ -92,12 +86,12 @@ public class PunchcardRecordServiceImpl implements PunchcardRecordService {
                         date1 = date1.substring(8, 10);
                         if (Integer.parseInt(date1) > 31) {
                             error = error + 1;
-                            Result.add("模板第" + (k-1) + "行的日期格式错误，请输入正确的日子，导入失败");
+                            Result.add("模板第" + (k - 1) + "行的日期格式错误，请输入正确的日子，导入失败");
                             continue;
                         }
                         if (Integer.parseInt(date) > 12) {
                             error = error + 1;
-                            Result.add("模板第" + (k-1) + "行的日期格式错误，请输入正确的月份，导入失败");
+                            Result.add("模板第" + (k - 1) + "行的日期格式错误，请输入正确的月份，导入失败");
                             continue;
                         }
                     }
@@ -106,26 +100,25 @@ public class PunchcardRecordServiceImpl implements PunchcardRecordService {
                     query.addCriteria(Criteria.where("userinfo.jobnumber").is(jobnumber));
                     CustomerInfo customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
                     if (customerInfo != null) {
-                        punchcardrecord.setUser_id(customerInfo.getUserid());
-                        punchcardrecord.setJobnumber(value.get(0).toString());
+                        punchcardrecorddetail.setUser_id(customerInfo.getUserid());
+                        punchcardrecorddetail.setJobnumber(value.get(0).toString());
                     }
                     if (customerInfo == null) {
                         error = error + 1;
                         Result.add("模板第" + (k - 1) + "行的工号字段没有找到，请输入正确的工号，导入失败");
                         continue;
                     }
-                    punchcardrecord.setCenterid(customerInfo.getUserinfo().getCentername());
-                    punchcardrecord.setGroupid(customerInfo.getUserinfo().getGroupname());
-                    punchcardrecord.setTeamid(customerInfo.getUserinfo().getTeamname());
+                    punchcardrecorddetail.setCenter_id(customerInfo.getUserinfo().getCentername());
+                    punchcardrecorddetail.setGroup_id(customerInfo.getUserinfo().getGroupname());
+                    punchcardrecorddetail.setTeam_id(customerInfo.getUserinfo().getTeamname());
                     String Punchcardrecord_date = value.get(2).toString();
-                    punchcardrecord.setPunchcardrecord_date(sf1.parse(Punchcardrecord_date));
-                    punchcardrecord.setTime_start(sf.parse(Time_start));
-                    punchcardrecord.setTime_end(sf.parse(Time_end));
+                    punchcardrecorddetail.setPunchcardrecord_date(sf.parse(Punchcardrecord_date));
+
                 }
-                punchcardrecord.preInsert(tokenModel);
-                punchcardrecord.setPunchcardrecord_id(UUID.randomUUID().toString());
-                punchcardrecordMapper.insert(punchcardrecord);
-                listVo.add(punchcardrecord);
+                punchcardrecorddetail.preInsert(tokenModel);
+                punchcardrecorddetail.setPunchcardrecorddetail_id(UUID.randomUUID().toString());
+                punchcardrecorddetailmapper.insert(punchcardrecorddetail);
+                listVo.add(punchcardrecorddetail);
                 accesscount = accesscount + 1;
             }
             Result.add("失败数：" + error);
