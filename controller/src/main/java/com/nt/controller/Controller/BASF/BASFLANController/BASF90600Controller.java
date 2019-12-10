6b899c3732bf7ff7a579ef5cede734bdb3f1876b
耿绍凯 <shaokai.geng@newtouch.cn>
2019-12-10 16:57:54 +0800

    车辆定位大屏后台

---
+++ controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF90600Controller.java
@@ -0,0 +1,18 @@
+package com.nt.controller.Controller.BASF.BASFLANController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: BASF应急平台
+ * @Package: com.nt.controller.Controller.BASF.BASFLANController
+ * @ClassName: BASF90600Controller
+ * @Author: SKIAXX
+ * @Description: ERC大屏车辆定位模块接口
+ * @Date: 2019/12/4 13:40
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/BASF90600")
+public class BASF90600Controller {
+}
