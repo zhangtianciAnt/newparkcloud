9bce6d8d65b323f30c23ae5ff7f759f28cde6d8b
芦兆玮 <1098392478@qq.com>
2019-12-13 03:34:09 +0800

    工资

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/GivingServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/GivingServiceImpl.java
@@ -1829,7 +1829,8 @@ public class GivingServiceImpl implements GivingService {
         }
     }
 
-    public List<CustomerInfo> getInduction() {
+    //入职
+    public List<CustomerInfo> getInduction(String givingId) {
         Query query = new Query();
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar now = Calendar.getInstance();
@@ -1854,8 +1855,11 @@ public class GivingServiceImpl implements GivingService {
                     lastmon.get(Calendar.YEAR) + "" + getMouth(sf.format(now.getTime())), getMouth(sf.format(_now.getTime())) + "", getMouth(sf.format(now.getTime())) + "");
             if (inductions.size() > 0) {
                 inductions.forEach(induction -> {
-                    Optional<CustomerInfo> _customerInfo = customerInfo.stream().filter(a -> (induction.getUser_id()).equals(a.getUserid())).findAny();
+                    Optional<CustomerInfo> _customerInfo = customerInfo.stream().filter(a -> (induction.getUser_id()).equals(a.getUserid())).findFirst();
                     if (_customerInfo.isPresent()) {
+                        if(givingId!=null&&givingId!=""){
+                            induction.setGiving_id(givingId);
+                        }
                         try {
                             induction.setWorddate(sf.parse(_customerInfo.get().getUserinfo().getWorkday()));
                             if (_customerInfo.get().getUserinfo().getEnddate() != null && !_customerInfo.get().getUserinfo().getEnddate().equals("")) {
@@ -1864,8 +1868,8 @@ public class GivingServiceImpl implements GivingService {
                             if (_customerInfo.get().getUserinfo().getGridData() != null) {
                                 List<CustomerInfo.Personal> personals = _customerInfo.get().getUserinfo().getGridData().stream().sorted(Comparator.comparing(CustomerInfo.Personal::getDate).reversed())
                                         .collect(Collectors.toList());
-                                String lastMouth = "";
-                                String thisMouth = "";
+                                String lastMouth = "0";
+                                String thisMouth = "0";
                                 for (CustomerInfo.Personal personal : personals) {
                                     boolean _last = false;
                                     boolean _this = false;
@@ -1921,13 +1925,79 @@ public class GivingServiceImpl implements GivingService {
         return null;
     }
 
-    public void getRetire() {
+
+    //退职
+    public void getRetire(String givingId) {
         Query query = new Query();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar now = Calendar.getInstance();
-        Criteria criteria = Criteria.where("userinfo.enterday").gte(now.get(Calendar.YEAR) + "-" + now.get(Calendar.MONTH + 1) + "-01");
+        Calendar last = Calendar.getInstance();
+        last.add(Calendar.MONTH, -1);
+        now.add(Calendar.MONTH, -2);
+        now.set(Calendar.YEAR, now.get(Calendar.YEAR));
+        now.set(Calendar.MONTH, now.get(Calendar.MONTH));
+        int lastDay = now.getActualMaximum(Calendar.DAY_OF_MONTH);
+        Calendar cal = Calendar.getInstance();
+        Criteria criteria = Criteria.where("userinfo.resignation_date")
+                .gte(now.get(Calendar.YEAR) + "-" + getMouth(sf.format(now.getTime())) + "-" + lastDay)
+                .lte(cal.get(Calendar.YEAR) + "-" + getMouth(sf.format(cal.getTime())) + "-01");
         query.addCriteria(criteria);
         List<CustomerInfo> customerInfo = mongoTemplate.find(query, CustomerInfo.class);
+        if (customerInfo.size() > 0) {
+            List<String> list = new ArrayList<>();
+            customerInfo.forEach(customerInfo1 -> list.add(customerInfo1.getUserid()));
+            List<Retire> retires = retireMapper.selectRetire(list, last.get(Calendar.YEAR) + "" + getMouth(sf.format(last.getTime())), getMouth(sf.format(last.getTime())) + "");
+            if (retires.size() > 0) {
+                retires.forEach(retire -> {
+                    Optional<CustomerInfo> _customerInfo = customerInfo.stream().filter(a -> (retire.getUser_id()).equals(a.getUserid())).findFirst();
+                    if (_customerInfo.isPresent()) {
+                        if(givingId!=null&&givingId!=""){
+                            retire.setGiving_id(givingId);
+                        }
+                        String thisMouth = "0";
+                        try {
+                            if (_customerInfo.get().getUserinfo().getResignation_date() != null && !_customerInfo.get().getUserinfo().getResignation_date().equals("")) {
+                                retire.setRetiredate(sf.parse(_customerInfo.get().getUserinfo().getResignation_date()));
+                            }
+                            if (_customerInfo.get().getUserinfo().getGridData() != null) {
+                                List<CustomerInfo.Personal> personals = _customerInfo.get().getUserinfo().getGridData().stream().sorted(Comparator.comparing(CustomerInfo.Personal::getDate).reversed())
+                                        .collect(Collectors.toList());
 
+                                for (CustomerInfo.Personal personal : personals) {
+                                    if (sf.parse(personal.getDate()).getTime() <= sf.parse((last.get(Calendar.YEAR) + "-" + getMouth(sf.format(last.getTime())) + "-01")).getTime()) {
+                                        thisMouth = personal.getAfter();
+                                        return;
+                                    }
+                                }
+                            }
+                            DecimalFormat df = new DecimalFormat("#.00");
+                            //IF(今月出勤日数="全月",ROUND(基数.当月基本工资,2),IF(今月出勤日数<>"",ROUND(基数.当月基本工资/21.75*今月出勤日数,2),0))
+                            if (Integer.parseInt(retire.getAttendance()) == getWorkDays(last.get(Calendar.YEAR), Integer.parseInt(getMouth(sf.format(last.getTime()))))) {
+                                retire.setGive(df.format(Double.valueOf(thisMouth)));
+                            } else if (Integer.parseInt(retire.getAttendance()) != 0) {
+                                retire.setGive(df.format(Double.valueOf(df.format(Double.valueOf(thisMouth) / 21.75 * Integer.parseInt(retire.getAttendance())))));
+                            } else {
+                                retire.setGive("0");
+                            }
+                            //IF(今月出勤日数="全月",纳付率.食堂手当,ROUND(纳付率.食堂手当/21.75*今月出勤日数,2)
+                            if (Integer.parseInt(retire.getAttendance()) == getWorkDays(last.get(Calendar.YEAR), Integer.parseInt(getMouth(sf.format(last.getTime()))))) {
+                                retire.setLunch("105");
+                            } else {
+                                retire.setLunch(df.format(105 / 21.75 * Integer.parseInt(retire.getAttendance())));
+                            }
+                            //IF(今月出勤日数="全月",纳付率.交通手当,ROUND(纳付率.交通手当/21.75*今月出勤日数,2))
+                            if (Integer.parseInt(retire.getAttendance()) == getWorkDays(last.get(Calendar.YEAR), Integer.parseInt(getMouth(sf.format(last.getTime()))))) {
+                                retire.setLunch("84");
+                            } else {
+                                retire.setLunch(df.format(84 / 21.75 * Integer.parseInt(retire.getAttendance())));
+                            }
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
 
     }
 
@@ -1937,5 +2007,22 @@ public class GivingServiceImpl implements GivingService {
             return _mouth.substring(1);
         }
         return _mouth;
+    }
+
+
+
+    private static int getWorkDays(int theYear, int theMonth) {
+        int workDays = 0;
+        Calendar cal = Calendar.getInstance();
+        cal.set(theYear, theMonth - 1, 1);
+        int days = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        for (int i = 0; i < days; i++) {
+            int day = cal.get(Calendar.DAY_OF_WEEK);
+            if (!(day == Calendar.SUNDAY || day == Calendar.SATURDAY)) {
+                workDays++;
+            }
+            cal.add(Calendar.DATE, 1);
+        }
+        return workDays;
     }
 }
