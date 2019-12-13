2ddf3188f5d6c60106567f37ae14d7e55e98edb8
王帅 <shuai.wang1@newtouch.com>
2019-12-13 11:31:08 +0800

    日志填写修改

---
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS5000/Vo/PersonalProjectsVo.java
@@ -0,0 +1,20 @@
+package com.nt.dao_Pfans.PFANS5000.Vo;
+
+import com.nt.dao_Pfans.PFANS5000.PersonalProjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonalProjectsVo {
+
+    private List<PersonalProjects> personalprojects;
+
+
+
+}
--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/PersonalProjectsService.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/PersonalProjectsService.java
@@ -1,6 +1,7 @@
 package com.nt.service_pfans.PFANS5000;
 
 import com.nt.dao_Pfans.PFANS5000.PersonalProjects;
+import com.nt.dao_Pfans.PFANS5000.Vo.PersonalProjectsVo;
 import com.nt.utils.dao.TokenModel;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public interface PersonalProjectsService {
 
     public List<PersonalProjects> getProjectList(PersonalProjects personalprojects) throws Exception;
 
-    void insert(PersonalProjects personalprojects, TokenModel tokenModel)throws Exception;
+    void insert(PersonalProjectsVo personalprojectsvo, TokenModel tokenModel)throws Exception;
 
-    void delete( PersonalProjects personal, TokenModel tokenModel)throws Exception;
+    void delete(PersonalProjects personalprojects, TokenModel tokenModel)throws Exception;
 }
