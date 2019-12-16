cee0ce1419b67f38d3cf50f099665fe10b438744
耿绍凯 <shaokai.geng@newtouch.cn>
2019-12-16 14:31:37 +0800

    ERC大屏  车辆定位模块后台接口

--- controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF90600Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF90600Controller.java
@@ -1,6 +1,8 @@
 package com.nt.controller.Controller.BASF.BASFLANController;
 
+import com.nt.utils.ApiResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,4 +17,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/BASF90600")
 public class BASF90600Controller {
+
+    /**
+     * @Method getInsideList
+     * @Author SKAIXX
+     * @Version  1.0
+     * @Description 获取在场车辆信息一览
+     * @Return com.nt.utils.ApiResult
+     * @Date 2019/12/16 13:59
+     */
+    @RequestMapping(value = "/getInsideList", method = {RequestMethod.GET})
+    public ApiResult getInsideList() throws Exception {
+        // TODO 获取在场车辆信息一览
+        return ApiResult.success();
+    }
+
+    /**
+     * @Method getAccessStatistics
+     * @Author SKAIXX
+     * @Version  1.0
+     * @Description 车辆出入统计
+     * @param period 统计周期
+     * @Return com.nt.utils.ApiResult
+     * @Date 2019/12/16 14:18
+     */
+    @RequestMapping(value = "/getAccessStatistics", method = {RequestMethod.GET})
+    public ApiResult getAccessStatistics(String period) throws Exception {
+        // TODO 车辆出入统计
+        return ApiResult.success();
+    }
+
+    /**
+     * @Method getDailyVehicleInfo
+     * @Author SKAIXX
+     * @Version  1.0
+     * @Description 获取当日入场车辆信息
+     * @Return com.nt.utils.ApiResult
+     * @Date 2019/12/16 14:21
+     */
+    @RequestMapping(value = "/getDailyVehicleInfo", method = {RequestMethod.GET})
+    public ApiResult getDailyVehicleInfo() throws Exception {
+        // TODO 获取当日入场车辆信息
+        return ApiResult.success();
+    }
 }
