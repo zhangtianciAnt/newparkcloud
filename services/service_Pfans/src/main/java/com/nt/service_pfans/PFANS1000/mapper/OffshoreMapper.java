a3b1ce0adaca76c8250abf63a4495ee20a934426
敖婷婷 <tingting.ao@newtouch.com>
2019-12-24 14:55:50 +0800

    1011提交

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/mapper/OffshoreMapper.java
@@ -0,0 +1,7 @@
+package com.nt.service_pfans.PFANS1000.mapper;
+
+import com.nt.dao_Pfans.PFANS1000.Offshore;
+import com.nt.utils.MyMapper;
+
+public interface OffshoreMapper extends MyMapper<Offshore> {
+}
