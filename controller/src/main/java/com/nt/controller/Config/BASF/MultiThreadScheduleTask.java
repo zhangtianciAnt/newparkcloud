04279d2b530c256958f724ca76e3b2885f41b893
skaixx <shaokai.geng@newtouch.com>
2019-12-30 17:37:16 +0800

    ERC 车辆定位WebSocket推送

--- controller/src/main/java/com/nt/controller/Config/BASF/MultiThreadScheduleTask.java
+++ controller/src/main/java/com/nt/controller/Config/BASF/MultiThreadScheduleTask.java
@@ -1,9 +1,11 @@
 package com.nt.controller.Config.BASF;
 
 import com.alibaba.fastjson.JSONObject;
-import com.nt.controller.Controller.WebSocket.WebSocketVo;
 import com.nt.controller.Controller.WebSocket.WebSocket;
+import com.nt.controller.Controller.WebSocket.WebSocketVo;
+import com.nt.service_BASF.VehicleinformationServices;
 import com.nt.service_SQL.sqlMapper.BasfUserInfoMapper;
+import com.nt.utils.ApiResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
+import org.w3c.dom.Text;
 
 import java.time.LocalDateTime;
 
@@ -26,6 +29,16 @@ public class MultiThreadScheduleTask {
     @SuppressWarnings("all")
     BasfUserInfoMapper basfUserInfoMapper;
 
+    @Autowired
+    @SuppressWarnings("all")
+    VehicleinformationServices vehicleinformationServices;
+
+
+    // websocket消息推送
+    private WebSocket ws = new WebSocket();
+    // WebSocketVow
+    private WebSocketVo webSocketVo = new WebSocketVo();
+
     @Async
     @Scheduled(fixedDelay = 30000)
     public void selectUserCount() throws InterruptedException {
@@ -33,10 +46,39 @@ public class MultiThreadScheduleTask {
                 + "\r\n线程 : " + Thread.currentThread().getName());
         // 查询数据库
         int i = basfUserInfoMapper.selectUserCount();
-        // websocket消息推送
-        WebSocket ws = new WebSocket();
-        WebSocketVo messageVo = new WebSocketVo();
-        messageVo.setUsersCount(i);
-        ws.sendMessageToAll(new TextMessage(JSONObject.toJSONString(messageVo)));
+        webSocketVo.setUsersCount(i);
+        ws.sendMessageToAll(new TextMessage(JSONObject.toJSONString(webSocketVo)));
+    }
+
+    /**
+     * @return void
+     * @Method BASF90600_GetInsideList
+     * @Author SKAIXX
+     * @Description ERC大屏车辆定位模块获取在场车辆信息一览
+     * @Date 2019/12/30 15:47
+     * @Param
+     **/
+    @Async
+    @Scheduled(fixedDelay = 30000)
+    public void BASF90600_GetInsideList() throws Exception {
+        // 获取在场车辆信息一览
+        webSocketVo.setInsideVehicleinformationVoList(vehicleinformationServices.getInsideList());
+        ws.sendMessageToAll(new TextMessage(JSONObject.toJSONString(webSocketVo)));
+    }
+
+    /**
+     * @return void
+     * @Method BASF90600_getInsideVehicleType
+     * @Author SKAIXX
+     * @Description ERC大屏车辆定位模块在场车辆类别统计
+     * @Date 2019/12/30 16:27
+     * @Param
+     **/
+    @Async
+    @Scheduled(fixedDelay = 30000)
+    public void BASF90600_getInsideVehicleType() throws Exception {
+        // 在场车辆类别统计
+        webSocketVo.setInsideVehicleTypeVoList(vehicleinformationServices.getInsideVehicleType());
+        ws.sendMessageToAll(new TextMessage(JSONObject.toJSONString(webSocketVo)));
     }
 }
