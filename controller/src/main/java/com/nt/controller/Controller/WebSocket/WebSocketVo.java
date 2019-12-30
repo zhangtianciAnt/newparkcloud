21a3a9316515728af6cfa330a0ee3c6f33e48df7
skaixx <shaokai.geng@newtouch.com>
2019-12-30 15:30:09 +0800

    BASF90600 车辆定位模块 web SocketVo

--- controller/src/main/java/com/nt/controller/Controller/WebSocket/WebSocketVo.java
+++ controller/src/main/java/com/nt/controller/Controller/WebSocket/WebSocketVo.java
@@ -1,9 +1,15 @@
 package com.nt.controller.Controller.WebSocket;
 
+import com.nt.dao_BASF.VO.InsideVehicleTypeVo;
 import com.nt.dao_BASF.VO.InsideVehicleinformationVo;
+import com.nt.dao_BASF.VO.VehicleAccessStatisticsVo;
+import com.nt.dao_BASF.Vehicleinformation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ProjectName: BASF应急平台
@@ -21,6 +27,15 @@ public class WebSocketVo {
     // 员工人数
     private Integer usersCount;
 
-    // BASF90600 车辆定位模块 在场车辆信息Vo
-    private InsideVehicleinformationVo insideVehicleinformationVo;
+    // BASF90600 车辆定位模块 在场车辆信息一览Vo
+    private List<InsideVehicleinformationVo> insideVehicleinformationVoList = new ArrayList<>();
+
+    // BASF90600 车辆定位模块 车辆出入统计Vo
+    private List<VehicleAccessStatisticsVo> vehicleAccessStatisticsVoList = new ArrayList<>();
+
+    // BASF90600 车辆定位模块 当日车辆入场信息Vo
+    private List<Vehicleinformation> vehicleinformationList = new ArrayList<>();
+
+    // BASF90600 车辆定位模块 在场车辆类别统计Vo
+    private List<InsideVehicleTypeVo> insideVehicleTypeVoList = new ArrayList<>();
 }
