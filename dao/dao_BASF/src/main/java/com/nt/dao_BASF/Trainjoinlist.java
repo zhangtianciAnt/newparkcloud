a9999105b0b6229556bc78a468af2fa749014ddc
王哲 <zhe.wang@newtouch.cn>
2020-01-07 10:59:01 +0800

    培训参加人员dao层

---
+++ dao/dao_BASF/src/main/java/com/nt/dao_BASF/Trainjoinlist.java
@@ -0,0 +1,51 @@
+package com.nt.dao_BASF;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @ProjectName: BASF应急平台
+ * @Package: com.nt.dao_BASF
+ * @ClassName: Trainjoinlist
+ * @Author: 王哲
+ * @Description: 培训参加名单
+ * @Date: 2020/1/7 10:53
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "trainjoinlist")
+public class Trainjoinlist extends BaseModel {
+
+    /**
+     * 培训参加名单主键
+     */
+    @Id
+    private String trainjoinlistid;
+
+    /**
+     * 开班培训列表主键
+     */
+    private String startprogramid;
+
+    /**
+     * 人员id
+     */
+    private String personnelid;
+
+    /**
+     * 参加状态
+     */
+    private String jointype;
+
+    /**
+     * 成绩
+     */
+    private String performance;
+}
