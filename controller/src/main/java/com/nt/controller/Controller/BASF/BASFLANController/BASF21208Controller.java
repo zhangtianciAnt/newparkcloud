2a6a1216beb9b635b8b7c617e38688a6133250d1
王哲 <zhe.wang@newtouch.cn>
2020-01-07 10:52:04 +0800

    培训申请页面dao层、controller层

---
+++ controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF21208Controller.java
@@ -0,0 +1,14 @@
+package com.nt.controller.Controller.BASF.BASFLANController;
+
+/**
+ * @ProjectName: BASF应急平台
+ * @Package: com.nt.controller.Controller.BASF.BASFLANController
+ * @ClassName: BASF21208Controller
+ * @Author: 王哲
+ * @Description: 培训申请
+ * @Date: 2020/1/7 10:15
+ * @Version: 1.0
+ */
+public class BASF21208Controller {
+
+}
---
+++ dao/dao_BASF/src/main/java/com/nt/dao_BASF/Startprogram.java
@@ -0,0 +1,96 @@
+package com.nt.dao_BASF;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * @ProjectName: BASF应急平台
+ * @Package: com.nt.dao_BASF
+ * @ClassName: Startprogram
+ * @Author: Newtouch
+ * @Description: 申请考核表
+ * @Date: 2020/1/7 10:21
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "startprogram")
+public class Startprogram extends BaseModel {
+
+    //主键
+    @Id
+    private String startprogramid;
+
+    //培训清单主键
+    private String programlistid;
+
+    //培训名称
+    private String programname;
+
+    //通知开班人数
+    private String informpeople;
+
+    //理论开班时间
+    private Date starttheorydate;
+
+    //理论地点
+    private String theorysite;
+
+    //实操开班时间
+    private Date startoperationdate;
+
+    //实操地点
+    private String operationsite;
+
+    //报名截止日期
+    private java.sql.Date expirationdate;
+
+    //培训材料上交截止日期
+    private Date materialsexpirationdate;
+
+    //通知人员
+    private String informperson;
+
+    //code分类
+    private String programcode;
+
+    //负责人
+    private String programhard;
+
+    //内部/外部
+    private String insideoutside;
+
+    //强制/非强制
+    private String mandatory;
+
+    //线上/线下
+    private String isonline;
+
+    //培训时长
+    private int thelength;
+
+    //培训有效期（月）
+    private int validity;
+
+    //培训费用
+    private String money;
+
+    //初训/复训
+    private String isfirst;
+
+    //培训参加名单主键
+    private String trainjoinlistid;
+
+    //报名资料
+    private String applydata;
+
+    //开班状态
+    private String programtype;
+}
