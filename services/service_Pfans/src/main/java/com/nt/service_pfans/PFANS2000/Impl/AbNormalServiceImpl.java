3472f4082d33e3e962f0dccb4587b3f721abfbcf
高兵兵 <bingbing.gao@newtouch.com>
2020-01-15 01:52:33 +0800

    异常考勤更新考勤管理表

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/AbNormalServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/AbNormalServiceImpl.java
@@ -1,13 +1,17 @@
 package com.nt.service_pfans.PFANS2000.Impl;
 
 import com.nt.dao_Pfans.PFANS2000.AbNormal;
+import com.nt.dao_Pfans.PFANS2000.Attendance;
 import com.nt.service_pfans.PFANS2000.AbNormalService;
 import com.nt.service_pfans.PFANS2000.mapper.AbNormalMapper;
+import com.nt.service_pfans.PFANS2000.mapper.AttendanceMapper;
+import com.nt.utils.AuthConstants;
 import com.nt.utils.dao.TokenModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +21,8 @@ public class AbNormalServiceImpl implements AbNormalService {
 
     @Autowired
     private AbNormalMapper abNormalMapper;
+    @Autowired
+    private AttendanceMapper attendanceMapper;
 
     @Override
     public List<AbNormal> list(AbNormal abNormal) throws Exception {
@@ -34,6 +40,25 @@ public class AbNormalServiceImpl implements AbNormalService {
     public void upd(AbNormal abNormal, TokenModel tokenModel) throws Exception {
         abNormal.preUpdate(tokenModel);
         abNormalMapper.updateByPrimaryKey(abNormal);
+        if(abNormal.getStatus().equals(AuthConstants.APPROVED_FLAG_YES)){
+            Attendance attendance = new Attendance();
+            attendance.setUser_id(abNormal.getUser_id());
+            attendance.setDates(abNormal.getOccurrencedate());
+            List<Attendance> attendancelist = attendanceMapper.select(attendance);
+            DecimalFormat df = new DecimalFormat("######0.00");
+            if(attendancelist.size() > 0){
+                for (Attendance attend : attendancelist) {
+                    if(abNormal.getErrortype().equals("PR013005")){//年休
+                        attend.setAnnualrest(abNormal.getLengthtime());
+                    }
+                    else if(abNormal.getErrortype().equals("PR013008")){//事休/事假
+                        attend.setCompassionateleave(abNormal.getLengthtime());
+                    }
+                    attend.preUpdate(tokenModel);
+                    attendanceMapper.updateByPrimaryKey(attend);
+                }
+            }
+        }
     }
 
     @Override
