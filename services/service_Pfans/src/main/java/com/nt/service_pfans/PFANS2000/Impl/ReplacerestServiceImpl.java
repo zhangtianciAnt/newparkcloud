564ac68068db8c5e5df27d033491ef07dc772d15
刘德越 <9831848145@163.COM>
2020-01-17 17:14:43 +0800

    代休系统服务

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/ReplacerestServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/ReplacerestServiceImpl.java
@@ -4,12 +4,14 @@ import com.nt.dao_Pfans.PFANS2000.Replacerest;
 import com.nt.service_pfans.PFANS2000.ReplacerestService;
 import com.nt.service_pfans.PFANS2000.mapper.ReplacerestMapper;
 import com.nt.utils.dao.TokenModel;
+import org.apache.poi.ss.formula.functions.EDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 @Transactional(rollbackFor=Exception.class)
@@ -24,4 +26,22 @@ public class ReplacerestServiceImpl implements ReplacerestService {
         return replacerestMapper.select(replacerest);
     }
 
+    //系统服务（4月1日）
+    @Scheduled(cron="5 * * * * ?")
+    public void changestatus() throws Exception {
+        SimpleDateFormat sf1ymd = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdfxx = new SimpleDateFormat("EEE MMM dd 00:00:00 zzz yyyy", Locale.US);
+        Date dateStart = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dateStart);
+        cal.add(Calendar.DAY_OF_MONTH, -1);
+        String data = sf1ymd.format(sdfxx.parse(sdfxx.format(cal.getTime())));
+        List<Replacerest> replacerestlist = replacerestMapper.select(new Replacerest());
+            for (Replacerest replacerest : replacerestlist) {
+                replacerest.setRecognitionstate("1");
+                if(replacerest.equals(data)){
+                    replacerestMapper.updateByPrimaryKey(replacerest);
+                }
+            }
+    }
 }
