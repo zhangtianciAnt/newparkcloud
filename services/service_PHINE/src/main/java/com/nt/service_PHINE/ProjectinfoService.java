9b41f3ed5c557d82add46e2ac2b63e62132522aa
cash <mayuntao@cash>
2020-02-08 00:07:05 +0800

    项目创建模块，添加用户权限分配功能

--- services/service_PHINE/src/main/java/com/nt/service_PHINE/ProjectinfoService.java
+++ services/service_PHINE/src/main/java/com/nt/service_PHINE/ProjectinfoService.java
@@ -1,9 +1,9 @@
 package com.nt.service_PHINE;
 
-import com.nt.dao_PHINE.Project2device;
 import com.nt.dao_PHINE.Projectinfo;
 import com.nt.dao_PHINE.Vo.DeviceListVo;
 import com.nt.dao_PHINE.Vo.ProjectListVo;
+import com.nt.dao_PHINE.Vo.UserAuthListVo;
 import com.nt.utils.dao.TokenModel;
 
 import java.util.List;
@@ -28,11 +28,17 @@ public interface ProjectinfoService {
     // 保存项目分配的设备信息
     void saveResourcesInfo(TokenModel tokenModel, String projectid, String[] deviceidList);
 
+    // 保存平台管理员分配的用户权限信息
+    void saveUserAuthInfo(TokenModel tokenModel, String projectid, String[] useridList);
+
     // 删除用户权限及管理设备信息
-    void delUserAuth(String userid);
+    void delUserAuth(String projectid, String userid);
 
     // 查询projectid是否存在
     boolean selectProjectIdExist(String projectid);
+
+    // 获取用户权限信息
+    List<UserAuthListVo> getUserAuthList(String projectid);
 
     // 根据项目ID查询项目信息
     ProjectListVo getProjectInfo(String projectid);
--- services/service_PHINE/src/main/java/com/nt/service_PHINE/mapper/Project2userMapper.java
+++ services/service_PHINE/src/main/java/com/nt/service_PHINE/mapper/Project2userMapper.java
@@ -2,6 +2,8 @@ package com.nt.service_PHINE.mapper;
 
 import com.nt.dao_PHINE.Project2user;
 import com.nt.utils.MyMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface Project2userMapper extends MyMapper<Project2user> {
 }
