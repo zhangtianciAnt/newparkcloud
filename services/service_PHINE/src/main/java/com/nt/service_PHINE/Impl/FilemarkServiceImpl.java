37af5fbdf1937d98921dae7573202976222b6fc4
skaixx <shaokai.geng@newtouch.com>
2020-02-11 12:51:47 +0800

    文件标记后台初始化

---
+++ services/service_PHINE/src/main/java/com/nt/service_PHINE/Impl/FilemarkServiceImpl.java
@@ -0,0 +1,30 @@
+package com.nt.service_PHINE.Impl;
+
+import com.nt.dao_PHINE.Vo.FilemarkVo;
+import com.nt.service_PHINE.FilemarkService;
+import com.nt.utils.ApiResult;
+
+/**
+ * @ProjectName: newparkcloud
+ * @Package: com.nt.service_PHINE.Impl
+ * @ClassName: FilemarkServiceImpl
+ * @Description: java类作用描述
+ * @Author: SKAIXX
+ * @CreateDate: 2020/2/11
+ * @Version: 1.0
+ */
+public class FilemarkServiceImpl implements FilemarkService {
+
+    /**
+     * @return
+     * @Method saveFileMarkInfo
+     * @Author SKAIXX
+     * @Description TODO 方法描述
+     * @Date 2020/2/11 12:49
+     * @Param
+     **/
+    @Override
+    public ApiResult saveFileMarkInfo(FilemarkVo filemarkVo) {
+        return null;
+    }
+}
