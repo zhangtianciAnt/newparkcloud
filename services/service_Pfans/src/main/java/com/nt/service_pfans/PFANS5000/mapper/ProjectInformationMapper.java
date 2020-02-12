b2d0dd3e8ce37da1c75c0948c23fc98eba4309c4
刘德越 <9831848145@163.COM>
2020-02-12 13:41:59 +0800

    5009后台

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/mapper/ProjectInformationMapper.java
+++
@@ -1,7 +0,0 @@
-package com.nt.service_pfans.PFANS5000.mapper;
-
-import com.nt.dao_Pfans.PFANS5000.ProjectInformation;
-import com.nt.utils.MyMapper;
-
-public interface ProjectInformationMapper extends MyMapper<ProjectInformation> {
-}
