6cba1a11dff5cd782fa03bb91a6891e035db7f57
Mr.Wang <wxz0407225>
2020-02-13 14:48:24 +0800

    起案书实体

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/CompanyProjectsService.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/CompanyProjectsService.java
@@ -11,9 +11,11 @@ import java.util.List;
 public interface CompanyProjectsService {
     //新建
     void insert(CompanyProjectsVo companyProjectsVo, TokenModel tokenModel) throws Exception;
+
     public List<CompanyProjects> list(CompanyProjects talentPlan) throws Exception;
     //编辑
     void update(CompanyProjectsVo companyProjectsVo, TokenModel tokenModel) throws Exception;
+
     public List<CompanyProjects> getCompanyProjectList(CompanyProjects companyprojects,HttpServletRequest request) throws Exception;
     //按id查询
     CompanyProjectsVo selectById(String companyprojectsid) throws Exception;
