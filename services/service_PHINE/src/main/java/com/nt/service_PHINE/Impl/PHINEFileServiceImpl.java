d6a62aa49d72f5484d0ed1656a2a195f7e8acbe0
xyq222085@163.com <xyq222085@163.com>
2020-02-14 16:23:03 +0800

    update fileController

--- services/service_PHINE/src/main/java/com/nt/service_PHINE/Impl/PHINEFileServiceImpl.java
+++ services/service_PHINE/src/main/java/com/nt/service_PHINE/Impl/PHINEFileServiceImpl.java
@@ -57,8 +57,7 @@ public class PHINEFileServiceImpl implements PHINEFileService {
             operationRecordVo.setContent("上传" + result + "个文件");
             operationRecordVo.setDetailist(operationdetails);
             operationRecordVo.setProjectid(projectId);
-            operationRecordVo.preInsert(tokenModel);
-            operationrecordService.addOperationrecord(operationRecordVo);
+            operationrecordService.addOperationrecord(tokenModel.getToken(), operationRecordVo);
             return ApiResult.success(MsgConstants.INFO_01);
         } else {
             return ApiResult.fail("保存文件信息" + MsgConstants.ERROR_01);
