6f35f23743a9e4ca04aa239bf5ff1a38cd8abdd9
skaixx <shaokai.geng@newtouch.com>
2020-02-20 20:59:58 +0800

    BugList No.41 修改

--- services/service_PHINE/src/main/java/com/nt/service_PHINE/Impl/DeviceinfoServiceImpl.java
+++ services/service_PHINE/src/main/java/com/nt/service_PHINE/Impl/DeviceinfoServiceImpl.java
@@ -10,6 +10,7 @@ import com.nt.service_PHINE.OperationrecordService;
 import com.nt.service_PHINE.mapper.*;
 import com.nt.utils.ApiResult;
 import com.nt.utils.MsgConstants;
+import com.nt.utils.StringUtils;
 import com.nt.utils.dao.TokenModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -143,14 +144,30 @@ public class DeviceinfoServiceImpl implements DeviceinfoService {
     public ApiResult saveDeviceInfo(TokenModel tokenModel, DeviceinfoVo deviceinfoVo) {
         // region 设备信息
         Deviceinfo deviceinfo = new Deviceinfo();
-        String deviceid = UUID.randomUUID().toString();
+        String deviceid = deviceinfoVo.getId();
+        boolean isNew = false;
+        if (StringUtils.isEmpty(deviceid)) {
+            deviceid = UUID.randomUUID().toString();
+            isNew = true;
+        }
         deviceinfo.setId(deviceid);
         deviceinfo.setCabinetid(deviceinfoVo.getCabinetid());       // 机柜ID
         deviceinfo.setDeviceid(deviceinfoVo.getDeviceid());         // 设备编号
         deviceinfo.setDevicetype(deviceinfoVo.getDevicetype());     // 设备类型
         deviceinfo.setCompanyid(deviceinfoVo.getCompanyid());       // 所属公司ID
-        deviceinfo.preInsert(tokenModel);
-        deviceinfoMapper.insert(deviceinfo);
+        deviceinfo.setCabinetslotid(deviceinfoVo.getCabinetslotid());   // 机柜槽位信息
+        // 新建数据
+        if (isNew) {
+            deviceinfo.preInsert(tokenModel);
+            deviceinfoMapper.insert(deviceinfo);
+        } else {    // 更新数据
+            deviceinfo.preUpdate(tokenModel);
+            deviceinfoMapper.updateByPrimaryKey(deviceinfo);
+        }
+        // endregion
+
+        // region 删除板卡&芯片信息
+        deleteBoardChip(deviceinfoVo);
         // endregion
 
         // region 创建板卡&芯片信息
