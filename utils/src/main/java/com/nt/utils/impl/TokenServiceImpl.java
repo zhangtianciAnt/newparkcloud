3611902571cc30641d80a444c91bf73b373c47b1
赵国栋 <guodong.zhao@newtouch.cn>
2020-03-06 16:38:30 +0800

    修改

--- utils/src/main/java/com/nt/utils/impl/TokenServiceImpl.java
+++ utils/src/main/java/com/nt/utils/impl/TokenServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nt.utils.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
 import com.mongodb.client.result.DeleteResult;
@@ -7,7 +8,9 @@ import com.nt.utils.AuthConstants;
 import com.nt.utils.LogicalException;
 import com.nt.utils.MessageUtil;
 import com.nt.utils.MsgConstants;
+import com.nt.utils.dao.JsTokenModel;
 import com.nt.utils.dao.TokenModel;
+import com.nt.utils.services.JsTokenService;
 import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -24,6 +27,8 @@ public class TokenServiceImpl implements TokenService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Autowired
+    private JsTokenService jsTokenService;
     @Override
     public TokenModel setToken(TokenModel tokenModel) {
         tokenModel.setToken(SecureUtil.md5(tokenModel.getUserId()));
@@ -43,18 +48,18 @@ public class TokenServiceImpl implements TokenService {
     @Override
     public TokenModel getToken(HttpServletRequest request) throws Exception{
         String token = request.getHeader(AuthConstants.AUTH_TOKEN);
-        Query query = new Query(Criteria.where("token").is(token));
-        TokenModel rst = mongoTemplate.findOne(query, TokenModel.class);
-        if(rst == null){
-            throw new LogicalException(MessageUtil.getMessage(MsgConstants.ERROR_02,request.getHeader(AuthConstants.LOCALE)));
-        }
+        JsTokenModel tokenModel = jsTokenService.getTokenModel(token);
+        TokenModel rst = new TokenModel();
+        BeanUtil.copyProperties(tokenModel,rst);
         return rst;
     }
 
     @Override
     public TokenModel getToken(String token) {
         Query query = new Query(Criteria.where("token").is(token));
-        TokenModel rst = mongoTemplate.findOne(query, TokenModel.class);
+        JsTokenModel tokenModel = jsTokenService.getTokenModel(token);
+        TokenModel rst = new TokenModel();
+        BeanUtil.copyProperties(tokenModel,rst);
         return rst;
     }
 
