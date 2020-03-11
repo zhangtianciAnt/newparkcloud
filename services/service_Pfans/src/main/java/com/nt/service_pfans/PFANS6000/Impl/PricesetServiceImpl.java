33da45837e2d89ce74dc8b97ee5d65d0e2a493af
ztc <280883874@qq.com>
2020-03-11 10:08:55 +0800

    ztc

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS6000/Impl/PricesetServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS6000/Impl/PricesetServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -44,38 +45,44 @@ public class PricesetServiceImpl implements PricesetService {
      */
     @Override
     public void updatepriceset(List<Priceset> priceset, TokenModel tokenModel) throws Exception {
-//        for(int i = 0; i < priceset.size(); i++){
-//            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-//            Priceset price = priceset.get(i);
-//
-//            Priceset prices = new Priceset();
-//            prices.setPricesetid(priceset.get(i).getPricesetid());
-//            List<Priceset> pricesetList = pricesetMapper.select(prices);
-//
-//            String AssesstimeUpYear_s = (priceset.get(i).getAssesstime().substring(0, 4));
-//            String AssesstimeUpMonth_s = (priceset.get(i).getAssesstime().substring(5, 7));
-//            String AssesstimeUp_s = AssesstimeUpYear_s + AssesstimeUpMonth_s;
-//            int priceYearMounthUp_i = Integer.parseInt(AssesstimeUp_s);
-//
-//            for(int j = 0; j < pricesetList.size(); j++){
-//                if(pricesetList.get(j).getStatus().equals("0")){
-//                    String AssesstimeStYear_s = (pricesetList.get(j).getAssesstime().substring(0, 4));
-//                    String AssesstimeStMonth_s = (pricesetList.get(j).getAssesstime().substring(5, 7));
-//                    String AssesstimeSt_s = AssesstimeUpYear_s + AssesstimeUpMonth_s;
-//                    int priceYearMounthSt_i = Integer.parseInt(AssesstimeSt_s);
-//
-//                    if(priceYearMounthUp_i == priceYearMounthSt_i){
-//                        pricesetMapper.updateByPrimaryKeySelective(price);
-//                    }else{
-//                        pricesetList.get(j).setStatus("1");
-//                        price.setStatus("0");
-//                        pricesetMapper.insert(price);
-//                    }
-//                }
-//            }
+        for(int i = 0; i < priceset.size(); i++){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+            Priceset price = priceset.get(i);
 
-//            price.preUpdate(tokenModel);
-//            pricesetMapper.updateByPrimaryKeySelective(price);
-//        }
+            Priceset prices = new Priceset();
+            //用主键查询
+            prices.setPricesetid(priceset.get(i).getPricesetid());
+            List<Priceset> pricesetList = pricesetMapper.select(prices);
+
+            String AssesstimeUpYear_s = (priceset.get(i).getAssesstime().substring(0, 4));
+            String AssesstimeUpMonth_s = (priceset.get(i).getAssesstime().substring(5, 7));
+            String AssesstimeUp_s = AssesstimeUpYear_s + AssesstimeUpMonth_s;
+
+            for(int j = 0; j < pricesetList.size(); j++){
+                if(pricesetList.get(j).getStatus().equals("0")){
+                    String AssesstimeStYear_s = (pricesetList.get(j).getAssesstime().substring(0, 4));
+                    String AssesstimeStMonth_s = (pricesetList.get(j).getAssesstime().substring(5, 7));
+                    String AssesstimeSt_s = AssesstimeStYear_s + AssesstimeStMonth_s;
+
+
+                    //如果月份相同，直接修改
+                    if(AssesstimeUp_s.equals(AssesstimeSt_s)){
+                        price.preUpdate(tokenModel);
+                        pricesetMapper.updateByPrimaryKey(price);
+                    }else{
+                        //如果月份不同， ，status为0
+                        Priceset pricesetOld = pricesetList.get(j);
+                        pricesetOld.setStatus("1");
+                        pricesetOld.preUpdate(tokenModel);
+                        pricesetMapper.updateByPrimaryKeySelective(pricesetOld);
+
+                        price.preInsert(tokenModel);
+                        price.setPricesetid(UUID.randomUUID().toString());
+                        price.setStatus("0");
+                        pricesetMapper.insert(price);
+                    }
+                }
+            }
+        }
     }
 }
