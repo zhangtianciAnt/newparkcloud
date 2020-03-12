01a637ae9d2766c6178d716e10bb6c604ea36546
ztc <280883874@qq.com>
2020-03-12 18:16:47 +0800

    ztc

---
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS6000/mapper/DelegainformationMapper.java
@@ -0,0 +1,8 @@
+package com.nt.service_pfans.PFANS6000.mapper;
+
+import com.nt.dao_Pfans.PFANS6000.Delegainformation;
+import com.nt.utils.MyMapper;
+
+public interface DelegainformationMapper extends MyMapper<Delegainformation> {
+
+}
