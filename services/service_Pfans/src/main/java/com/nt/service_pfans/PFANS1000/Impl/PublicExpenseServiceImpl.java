e4cdabf5bd334e34a6da42feb30eaa8282bbc81e
影 <ying.zhang7@newtouch.cn>
2020-03-13 11:07:35 +0800

    -----张影 报销 csv出力报表生成

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/Impl/PublicExpenseServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/Impl/PublicExpenseServiceImpl.java
@@ -1,29 +1,33 @@
 package com.nt.service_pfans.PFANS1000.Impl;
 
+import com.nt.dao_Org.Dictionary;
 import com.nt.dao_Pfans.PFANS1000.*;
 import com.nt.dao_Pfans.PFANS1000.Vo.PublicExpenseVo;
 import com.nt.dao_Pfans.PFANS3000.Purchase;
+import com.nt.dao_Pfans.PFANS5000.StageInformation;
+import com.nt.service_Org.DictionaryService;
 import com.nt.service_pfans.PFANS1000.PublicExpenseService;
-import com.nt.service_pfans.PFANS1000.mapper.OtherDetailsMapper;
-import com.nt.service_pfans.PFANS1000.mapper.InvoiceMapper;
-import com.nt.service_pfans.PFANS1000.mapper.PublicExpenseMapper;
-import com.nt.service_pfans.PFANS1000.mapper.PurchaseDetailsMapper;
-import com.nt.service_pfans.PFANS1000.mapper.TrafficDetailsMapper;
+import com.nt.service_pfans.PFANS1000.mapper.*;
 import com.nt.utils.dao.TokenModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
+import java.lang.reflect.InvocationTargetException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @Transactional(rollbackFor = Exception.class)
 public class PublicExpenseServiceImpl implements PublicExpenseService {
-
 
     @Autowired
     private PublicExpenseMapper publicExpenseMapper;
@@ -40,6 +44,12 @@ public class PublicExpenseServiceImpl implements PublicExpenseService {
 
     @Autowired
     private OtherDetailsMapper otherDetailsMapper;
+
+    @Autowired
+    private TotalCostMapper totalCostMapper;
+
+    @Autowired
+    private DictionaryService dictionaryService;
 
     //列表查询
     @Override
@@ -103,6 +113,281 @@ public class PublicExpenseServiceImpl implements PublicExpenseService {
                 invoice.setRowindex(rowundex);
                 invoicemapper.insertSelective(invoice);
             }
+        }
+
+        saveTotalCostList(invoicelist, trafficDetailslist, purchaseDetailslist, otherDetailslist, publicExpenseVo, tokenModel, publicexpenseid);
+    }
+
+    private void saveTotalCostList(List<Invoice> invoicelist,List<TrafficDetails> trafficDetailslist,List<PurchaseDetails> purchaseDetailslist,
+                                   List<OtherDetails> otherDetailslist,PublicExpenseVo publicExpenseVo, TokenModel tokenModel,String publicexpenseid) throws Exception{
+        //发票编号
+        String invoiceNo = "";
+        Calendar cal = Calendar.getInstance();
+        String year = new SimpleDateFormat("yy",Locale.CHINESE).format(Calendar.getInstance().getTime());
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DATE);
+        // 发票日期，条件日期
+        Date date = new Date();
+        SimpleDateFormat myFormatter = new SimpleDateFormat("ddMMMyyyy", Locale.ENGLISH);
+        date = myFormatter.parse(myFormatter.format(date));
+        //通过字典查取税率
+        List<com.nt.dao_Org.Dictionary> dictionaryList = dictionaryService.getForSelect("PJ071");
+        Map<String, String> taxRateMap = new HashMap<>();
+        for ( Dictionary d : dictionaryList ) {
+            taxRateMap.put(d.getCode(), d.getValue1());
+        }
+        //采购
+        List<com.nt.dao_Org.Dictionary> dictionaryList1 = dictionaryService.getForSelect("PJ007");
+        Map<String, String> procurementDetailsMap = new HashMap<>();
+        for ( Dictionary d : dictionaryList ) {
+            procurementDetailsMap.put(d.getCode(), d.getValue1());
+        }
+        List<com.nt.dao_Org.Dictionary> dictionaryList2 = dictionaryService.getForSelect("PJ005");
+        Map<String, String> procurementProjectMap = new HashMap<>();
+        for ( Dictionary d : dictionaryList ) {
+            procurementProjectMap.put(d.getCode(), d.getValue1());
+        }
+        //其他
+        List<com.nt.dao_Org.Dictionary> dictionaryList3 = dictionaryService.getForSelect("PJ057");
+        Map<String, String> costItemMap = new HashMap<>();
+        for ( Dictionary d : dictionaryList ) {
+            costItemMap.put(d.getCode(), d.getValue1());
+        }
+
+        Map<String, Object> mergeResult = null;
+        Map<String, Float> specialMap = new HashMap<>();
+        for ( Invoice invoice : invoicelist ) {
+            if ( SPECIAL_KEY.equals(invoice.getInvoicetype()) ) {
+                // 专票，获取税率
+                float rate = getFloatValue(taxRateMap.getOrDefault(invoice.getTaxrate(), ""));
+                if ( rate <= 0 ) {
+                    throw new Exception("专票税率不能为0");
+                }
+                specialMap.put(invoice.getInvoicenumber(), rate);
+            }
+            // 合计发票金额（含税）
+            specialMap.put(TOTAL_TAX,
+                    specialMap.getOrDefault(TOTAL_TAX, 0f) + getFloatValue(invoice.getInvoiceamount()));
+        }
+        if ( specialMap.getOrDefault(TOTAL_TAX, 0f) <= 0 ) {
+            throw new Exception("发票合计金额不能为0");
+        }
+
+        List<Object> needMergeList = new ArrayList<>();
+        if(trafficDetailslist.size() > 0 &&
+                StringUtils.hasText(trafficDetailslist.get(0).getInvoicenumber())){
+            // 交通费
+            needMergeList.addAll(trafficDetailslist);
+//            for ( TrafficDetails details : trafficDetailslist ) {
+//                needMergeList.add(details);
+//            }
+        } else {
+            // 其他
+            needMergeList.addAll(purchaseDetailslist);
+            needMergeList.addAll(otherDetailslist);
+//            for ( PurchaseDetails details : purchaseDetailslist ) {
+//                needMergeList.add(details);
+//            }
+//            for ( OtherDetails details : otherDetailslist ) {
+//                needMergeList.add(details);
+//            }
+        }
+        mergeResult = mergeDetailList(needMergeList, specialMap);
+
+
+        List<TotalCost> csvList = new ArrayList<>();
+        List<TotalCost> taxList = (List<TotalCost>) mergeResult.getOrDefault(TAX_KEY, new ArrayList<>());
+        List<TotalCost> paddingList = (List<TotalCost>) mergeResult.getOrDefault(PADDING_KEY, new ArrayList<>());
+        String inputType = (String) mergeResult.get(INPUT_TYPE_KEY);
+        for ( Object o : mergeResult.values() ) {
+            if ( o instanceof  TrafficDetails || o instanceof PurchaseDetails || o instanceof OtherDetails ) {
+                String money = getProperty(o, inputType);
+                TotalCost cost = new TotalCost();
+                if ( FIELD_RMB.equals(inputType) ) {
+                    cost.setCurrency("CYN");
+                } else if ( FIELD_FOREIGNCURRENCY.equals(inputType) ) {
+                    cost.setCurrency("FOREIGN");
+                }
+                cost.setLineamount(money);
+                cost.setBudgetcoding(getProperty(o, "budgetcoding"));
+                cost.setSubjectnumber(getProperty(o, "subjectnumber"));
+                //todo 发票说明
+                csvList.add(cost);
+            }
+        }
+        csvList.addAll(taxList);
+        csvList.addAll(paddingList);
+        int rowindex = 0;
+        for (TotalCost insertInfo: csvList) {
+            rowindex = rowindex + 1;
+            insertInfo.preInsert(tokenModel);
+            insertInfo.setTotalcost_id(UUID.randomUUID().toString());
+            insertInfo.setPublicexpenseid(publicexpenseid);
+            insertInfo.setNumber(rowindex);
+            //日期格式，取当前日期， 输出CSV时需要格式化成28OCT2019
+            insertInfo.setInvoicedate(date);
+            insertInfo.setConditiondate(date);
+            insertInfo.setVendorcode(publicExpenseVo.getPublicexpense().getPayeecode());//供应商编号
+            insertInfo.setCurrency(publicExpenseVo.getPublicexpense().getCurrency());//币种
+            insertInfo.setInvoiceamount(specialMap.get(TOTAL_TAX).toString());//总金额
+            //todo 发票说明
+//            insertInfo.setRemark();
+            String no=String.format("%2d", rowindex).replace(" ", "0");
+            insertInfo.setInvoicenumber("DL4AP" + year + month + day + no);
+            totalCostMapper.insertSelective(insertInfo);
+        }
+    }
+
+    /**
+     * 专票类型KEY
+     */
+    private static final String SPECIAL_KEY = "PJ068001";
+    private static final String TOTAL_TAX = "__TOTAL_TAX__";
+    /**
+     * 税-》KEY
+     */
+    private static final String TAX_KEY = "__TAX_KEY__";
+    private static final String PADDING_KEY = "__PADDING_KEY__";
+    private static final String INPUT_TYPE_KEY = "__INPUT_TYPE_KEY__";
+    private static final DecimalFormat FNUM = new DecimalFormat("##0.00");
+
+    /**
+     * 对明细数据分组
+     * @param detailList
+     * @return resultMap
+     */
+    private Map<String, Object> mergeDetailList(List<Object> detailList, final Map<String, Float> specialMap) throws Exception {
+        Map<String, Object> resultMap = new HashMap<>();
+        if ( detailList.size() <= 0 ) {
+            throw new Exception("明细不能为空");
+        }
+        String inputType = getInputType(detailList.get(0));
+//        Map<String, Object> mergedMap = new HashMap<>();
+        for ( Object detail : detailList ) {
+            if ( !inputType.equals(getInputType(detail)) ) {
+                throw new Exception("一次申请，只能选择一种货币。");
+            }
+            // 发票No
+            String keyNo = getProperty(detail, FIELD_INVOICENUMBER);
+            String budgetcoding = getProperty(detail, "budgetcoding");
+            String subjectnumber = getProperty(detail, "subjectnumber");
+            String mergeKey;
+            if ( specialMap.containsKey(keyNo) ) {
+                mergeKey = keyNo + " ... " + budgetcoding + " ... " + subjectnumber;
+            } else {
+                mergeKey = budgetcoding + " ... " + subjectnumber;
+            }
+            // 行合并
+            float money = getPropertyFloat(detail, inputType);
+            Object mergeObject =  resultMap.get(mergeKey);
+            if ( mergeObject != null ) {
+                // 发现可以合并数据
+                float newMoney = getPropertyFloat(mergeObject, inputType) + money;
+                setProperty(mergeObject, inputType, newMoney+"");
+            } else {
+                resultMap.put(mergeKey, detail);
+            }
+        }
+
+        float totalTax = 0f;
+        List<Object> list = new ArrayList<>(resultMap.values());
+        for (Object detail: list) {
+            // 发票No
+            String keyNo = getProperty(detail, FIELD_INVOICENUMBER);
+            float money = getPropertyFloat(detail, inputType);
+            totalTax = totalTax + money;
+            // 如果是专票，处理税
+            if ( specialMap.containsKey(keyNo) ) {
+                List<TotalCost> taxList = (List<TotalCost>) resultMap.getOrDefault(TAX_KEY, new ArrayList<>());
+                resultMap.put(TAX_KEY, taxList);
+                float rate = specialMap.get(keyNo);
+                TotalCost taxCost = new TotalCost();
+
+                // 税拔
+                String lineCost = FNUM.format(money/(1+rate));
+                // 税金
+                String lineRate = FNUM.format((money/(1+rate))*rate);
+                if ( money>0 ) {
+                    // 税
+                    taxCost.setLineamount(lineRate);
+                    taxCost.setBudgetcoding(getProperty(detail, "budgetcoding"));
+                    taxCost.setSubjectnumber(getProperty(detail, "subjectnumber"));
+//                    String procurementDetails = "";
+//                    procurementDetails = getProperty(detail, "procurementdetails");
+//                    String procurementProject = "";
+//                    procurementProject = getProperty(detail, "procurementproject");
+//                    String costitem = "";
+//                    costitem = getProperty(detail, "costitem");
+//                    taxCost.setRemark(procurementDetails + procurementProject);
+                    //todo 发票说明
+                    taxList.add(taxCost);
+                    // 税拔
+                    setProperty(detail, inputType, lineCost);
+                    float diff = getFloatValue(lineRate) + getFloatValue(lineCost) - money;
+                    if ( diff!=0 ) {
+                        TotalCost padding = new TotalCost();
+                        padding.setLineamount(diff+"");
+                        padding.setBudgetcoding(getProperty(detail, "budgetcoding"));
+                        padding.setSubjectnumber(getProperty(detail, "subjectnumber"));
+                        //todo 发票说明
+                        List<TotalCost> paddingList = (List<TotalCost>) resultMap.getOrDefault(PADDING_KEY, new ArrayList<>());
+                        paddingList.add(padding);
+                        resultMap.put(PADDING_KEY, paddingList);
+                    }
+                }
+            }
+
+        }
+        if ( totalTax != specialMap.get(TOTAL_TAX) ) {
+            throw new Exception("发票合计金额与明细不匹配。");
+        }
+        resultMap.put(INPUT_TYPE_KEY, inputType);
+        return resultMap;
+    }
+
+    private static final String FIELD_RMB = "rmb";
+    private static final String FIELD_FOREIGNCURRENCY = "foreigncurrency";
+    private static final String FIELD_INVOICENUMBER = "invoicenumber";
+
+    private String getInputType(Object o) throws Exception {
+        float rmb = getPropertyFloat(o, FIELD_RMB);
+        float foreign = getPropertyFloat(o, FIELD_FOREIGNCURRENCY);
+        if ( rmb>0 && foreign>0 ) {
+            throw new Exception("人民币和外币不能同时输入。");
+        }
+        if ( rmb <0 || foreign<0 || (rmb+foreign) <0 ) {
+            throw new Exception("明细行金额不能为负数。");
+        }
+
+        return rmb > 0 ? FIELD_RMB : FIELD_FOREIGNCURRENCY;
+    }
+
+    private String getProperty(Object o, String key) {
+        try {
+            return org.apache.commons.beanutils.BeanUtils.getProperty(o, key);
+        } catch (Exception e) {
+            log.error("error get property for {}, key {}", o, key);
+            return "";
+        }
+    }
+
+    private float getPropertyFloat(Object o, String key) {
+        return getFloatValue(getProperty(o, key));
+    }
+
+    private void setProperty(Object o, String key, String val) {
+        try {
+            org.apache.commons.beanutils.BeanUtils.setProperty(o, key, val);
+        } catch (Exception e) {
+            log.error("error set bean {} property {} value {}", o.getClass(), key, val);
+        }
+    }
+
+    private float getFloatValue(String o) {
+        try {
+            return Float.parseFloat(o);
+        } catch (Exception e) {
+            return 0;
         }
     }
 
@@ -180,6 +465,11 @@ public class PublicExpenseServiceImpl implements PublicExpenseService {
                 invoicemapper.insertSelective(invoicel);
             }
         }
+
+        TotalCost totalCost=new TotalCost();
+        totalCost.setPublicexpenseid(spublicexpenseid);
+        totalCostMapper.delete(totalCost);
+        saveTotalCostList(invoicelist, trafficlist, purchaselist, otherlist, publicExpenseVo, tokenModel, spublicexpenseid);
     }
 
     //按id查询
