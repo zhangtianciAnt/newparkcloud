ccc2e118ecb10e4c34392e0d003b615f09932675
赵国栋 <guodong.zhao@newtouch.cn>
2020-03-14 10:26:11 +0800

    基数表增加字段

--- dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/Base.java
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/Base.java
@@ -85,16 +85,41 @@ public class Base extends BaseModel {
     private String registered;
 
     /**
-     * 2019年6月
+     * 上月月工资
      */
     @Column(name = "LASTMONTH")
     private String lastmonth;
 
     /**
-     * 2019年7月
+     * 上月基本工资
+     */
+    @Column(name = "LASTMONTHBASIC")
+    private String lastmonthbasic;
+
+    /**
+     * 上月职责工资
+     */
+    @Column(name = "LASTMONTHDUTY")
+    private String lastmonthduty;
+
+    /**
+     * 本月月工资
      */
     @Column(name = "THISMONTH")
     private String thismonth;
+
+    /**
+     * 当月基本工资
+     */
+    @Column(name = "THISMONTHBASIC")
+    private String thismonthbasic;
+
+    /**
+     * 当月职责工资
+     */
+    @Column(name = "THISMONTHDUTY")
+    private String thismonthduty;
+
 
     /**
      * 養老・失業・工傷基数
