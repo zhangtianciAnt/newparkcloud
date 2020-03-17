f17ff270329dbf42f7f32ed6c83a45984da6b67e
wxl <xinlei.wang@newtouch.com>
2020-03-17 14:01:49 +0800

    事业计划详细后台编码

--- dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/Vo/LunardetailVo.java
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/Vo/LunardetailVo.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,7 +14,20 @@ public class LunardetailVo {
     /**
      * 月度賞与考課一览
      */
-    private Lunarbonus lunarbonus;
+//    private Lunarbonus lunarbonus;
+
+    private String lunarbonus_id;
+
+    private Date evaluationday;
+
+    private String subjectmon;
+
+    private String subject;
+
+    private String evaluatenum;
+
+    private String user_id;
+
     /**
      * 考课对象id
      */
