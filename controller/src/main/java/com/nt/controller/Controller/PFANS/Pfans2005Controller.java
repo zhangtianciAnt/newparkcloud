4c0a86478f059623403def935c6e4f8ae27667f3
skaixx <shaokai.geng@newtouch.com>
2020-03-19 23:36:33 +0800

    接口修改：本月加班数据变更时，重新计算加班费合计

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans2005Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans2005Controller.java
@@ -214,8 +214,7 @@ public class Pfans2005Controller {
         if (givingVo == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
-        TokenModel tokenModel = tokenService.getToken(request);
-        return ApiResult.success(givingService.thisMonthOvertimeChange(givingVo, tokenModel));
+        return ApiResult.success(givingService.thisMonthOvertimeChange(givingVo));
     }
 
     /**
