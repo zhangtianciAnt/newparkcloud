cb918adb9fd3604ac1f5605d06785e76e2881e2b
王帅 <shuai.wang1@newtouch.com>
2020-03-20 11:34:34 +0800

    加班申请修改

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/PunchcardRecordMapper.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/PunchcardRecordMapper.java
@@ -4,9 +4,12 @@ import com.nt.dao_Pfans.PFANS2000.PunchcardRecord;
 import com.nt.utils.MyMapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 
 public interface PunchcardRecordMapper extends MyMapper<PunchcardRecord> {
+
+    List<PunchcardRecord> getDataList(@Param("user_id") String USER_ID);
 
 }
