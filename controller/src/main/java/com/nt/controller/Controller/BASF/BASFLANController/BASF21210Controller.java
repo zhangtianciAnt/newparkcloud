62a8387ff8769160fda7bf1364d3a2a62d60daaf
sunxu <xu.sun@newtouch.cn>
2020-03-20 17:04:41 +0800

    edit

--- controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF21210Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/BASF/BASFLANController/BASF21210Controller.java
@@ -1,5 +1,7 @@
 package com.nt.controller.Controller.BASF.BASFLANController;
 
+import com.nt.dao_BASF.EmailConfig;
+import com.nt.dao_BASF.SendEmail;
 import com.nt.service_BASF.TrainjoinlistServices;
 import com.nt.utils.ApiResult;
 import com.nt.utils.MessageUtil;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+import com.nt.service_BASF.EmailConfigServices;
+import com.nt.service_BASF.SendEmailServices;
 
 /**
  * @ProjectName: BASF应急平台
@@ -34,10 +40,46 @@ public class BASF21210Controller {
     @Autowired
     private TrainjoinlistServices trainjoinlistServices;
 
+    @Autowired
+    private EmailConfigServices emailConfigServices;
+    @Autowired
+    private SendEmailServices sendEmailServices;
+
+
     //获取参加过培训的人员信息
     @PostMapping("/list")
     public ApiResult upTrain(HttpServletRequest request) throws Exception {
         return ApiResult.success(trainjoinlistServices.joinPersonnel(trainjoinlistServices.joinPersonnelid()));
+    }
+
+    //发送邮件
+    @PostMapping("/sendemail21210")
+    public ApiResult sendemail21210(@RequestBody SendEmail sendemail,HttpServletRequest request) throws Exception {
+        TokenModel tokenModel = tokenService.getToken(request);
+        List<EmailConfig> emailconfig =  emailConfigServices.get();
+
+        sendemail.setUserName(emailconfig.get(0).getUsername());
+        sendemail.setPassword(emailconfig.get(0).getPassword());
+        sendemail.setHost(emailconfig.get(0).getHost());
+        sendemail.setPort(emailconfig.get(0).getPort());
+        sendemail.setFromAddress(emailconfig.get(0).getFromaddress());
+        sendemail.setContextType(emailconfig.get(0).getContexttype());
+
+        String EMAILCONTENT = "XXXX您好：<br>" +
+                "<table width=\"100%\" border=\"1\" cellspacing=\"0\" cellpadding=\"2\">"
+                + "<tr>" +
+                //"<td width=\"10%\">ID</td>" +
+                "<td>ID</td>" +
+                "<td>负责人</td>" +
+                "<td>数据量</td>" +
+                "<td>说明</td>" +
+                "<td>文件</td>" +
+                "</tr>" +
+                "</table>";
+
+        sendemail.setContext(EMAILCONTENT);
+
+        return ApiResult.success(sendEmailServices.sendmail(tokenModel,sendemail));
     }
 
 
