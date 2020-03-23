34404b7326a4ad7aba0d538bd50ec57cfbf52e4b
王帅 <shuai.wang1@newtouch.com>
2020-03-23 15:05:07 +0800

    check抛出

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans2016Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans2016Controller.java
@@ -35,7 +35,6 @@ public class Pfans2016Controller {
     @RequestMapping(value = "/insertInfo", method = {RequestMethod.POST})
     public ApiResult create(@RequestBody AbNormal abNormal, HttpServletRequest request) throws Exception {
         try{
-            
             if (abNormal == null) {
                 return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
             }
@@ -44,7 +43,7 @@ public class Pfans2016Controller {
             abNormal.setRecognitionstate(AuthConstants.RECOGNITION_FLAG_NO);
             abNormalService.insert(abNormal, tokenModel);
             return ApiResult.success();
-        }catch(LogicalException e){
+        }catch(Exception e){
             return ApiResult.fail(e.getMessage());
         }
     }
