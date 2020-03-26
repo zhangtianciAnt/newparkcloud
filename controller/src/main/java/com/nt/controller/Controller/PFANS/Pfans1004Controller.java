d04646cf4b63f59f726521d3e55d81159214821f
刘德越 <9831848145@163.COM>
2020-03-26 11:12:42 +0800

    决裁无偿设备后台

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1004Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1004Controller.java
@@ -1,7 +1,6 @@
 package com.nt.controller.Controller.PFANS;
 
 import com.nt.dao_Pfans.PFANS1000.Judgement;
-import com.nt.dao_Pfans.PFANS1000.Vo.JudgementVo;
 import com.nt.service_pfans.PFANS1000.JudgementService;
 import com.nt.utils.ApiResult;
 import com.nt.utils.MessageUtil;
@@ -54,22 +53,22 @@ public class Pfans1004Controller {
     }
 
     @RequestMapping(value="/update",method = {RequestMethod.POST})
-    public ApiResult updateJudgement(@RequestBody JudgementVo judgementVo, HttpServletRequest request) throws Exception{
-        if (judgementVo == null) {
+    public ApiResult updateJudgement(@RequestBody Judgement judgement, HttpServletRequest request) throws Exception{
+        if (judgement == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         TokenModel tokenModel = tokenService.getToken(request);
-        judgementService.updateJudgement(judgementVo,tokenModel);
+        judgementService.update(judgement,tokenModel);
         return ApiResult.success();
     }
 
     @RequestMapping(value = "/create",method={RequestMethod.POST})
-    public ApiResult create(@RequestBody JudgementVo judgementVo, HttpServletRequest request) throws Exception {
-        if (judgementVo == null) {
+    public ApiResult create(@RequestBody Judgement judgement, HttpServletRequest request) throws Exception {
+        if (judgement == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         TokenModel tokenModel = tokenService.getToken(request);
-        judgementService.insert(judgementVo,tokenModel);
+        judgementService.insert(judgement,tokenModel);
         return ApiResult.success();
     }
 
