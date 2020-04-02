0c7c5bc6add4514f647484015862b1b76d5f8643
赵国栋 <guodong.zhao@newtouch.cn>
2020-04-02 16:01:33 +0800

    年假增加权限

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/AnnualLeaveServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/AnnualLeaveServiceImpl.java
@@ -85,7 +85,7 @@ public class AnnualLeaveServiceImpl implements AnnualLeaveService {
 
     //系统服务--事业年度开始获取年休
     //@Scheduled(cron="10 * * * * ?")测试用
-    @Scheduled(cron="0 47 22 1 4 *")//正式时间每年4月1日零时执行
+    @Scheduled(cron="0 50 14 2 4 *")//正式时间每年4月1日零时执行
     public void insert() throws Exception {
         List<CustomerInfo> customerinfo = mongoTemplate.findAll(CustomerInfo.class);
         if (customerinfo != null) {
