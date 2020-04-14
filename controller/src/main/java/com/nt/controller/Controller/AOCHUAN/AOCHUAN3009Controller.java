6d551d41aa8c2747e463c47a5fb7c96af40b8e20
niuchuang <chuang.niu@newtouch.cn>
2020-04-14 21:39:47 +0800

    AOCHUAN3009:查询Done

--- controller/src/main/java/com/nt/controller/Controller/AOCHUAN/AOCHUAN3009Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/AOCHUAN/AOCHUAN3009Controller.java
@@ -1,7 +1,9 @@
 package com.nt.controller.Controller.AOCHUAN;
+import com.nt.dao_AOCHUAN.AOCHUAN3000.FollowUpRecord;
 import com.nt.dao_AOCHUAN.AOCHUAN3000.Projects;
 import com.nt.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +23,17 @@ public class AOCHUAN3009Controller {
      */
     @RequestMapping(value = "/getProjectList", method = {RequestMethod.POST})
     public ApiResult getProjectList(HttpServletRequest request) throws Exception {
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxx" );
         Projects projects = new Projects();
         return ApiResult.success(projectsSerivce.getProjectList(projects));
-
     }
+
+    /**
+     * 获取记录列表数据
+     */
+    @RequestMapping(value = "/getFollowUpRecordList", method = {RequestMethod.POST})
+    public ApiResult getFollowUpRecordList(@RequestBody FollowUpRecord followUpRecord, HttpServletRequest request) throws Exception {
+
+        return ApiResult.success(projectsSerivce.getFollowUpRecordList(followUpRecord));
+    }
+
 }
--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/ProjectsService.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/ProjectsService.java
@@ -1,11 +1,13 @@
 package com.nt.service_AOCHUAN.AOCHUAN3000;
 
 import com.nt.dao_AOCHUAN.AOCHUAN3000.Projects;
+import com.nt.dao_AOCHUAN.AOCHUAN3000.FollowUpRecord;
 
 import java.util.List;
 
 public interface ProjectsService {
-
     //获取projectscheduleList
     List<Projects> getProjectList(Projects projects) throws Exception;
+    //获取记录表数据
+    List<FollowUpRecord> getFollowUpRecordList(FollowUpRecord followUpRecord) throws Exception;
 }
