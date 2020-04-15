658c12488bef0a9990e0316499c7852270ad273b
niuchuang <chuang.niu@newtouch.cn>
2020-04-15 22:09:46 +0800

    AOCHUAN3009: insert update delete check

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/Impl/ProjectsServiceImpl.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/Impl/ProjectsServiceImpl.java
@@ -5,6 +5,7 @@ import com.nt.dao_AOCHUAN.AOCHUAN3000.Projects;
 import com.nt.service_AOCHUAN.AOCHUAN3000.ProjectsService;
 import com.nt.service_AOCHUAN.AOCHUAN3000.mapper.FollowUpRecordMapper;
 import com.nt.service_AOCHUAN.AOCHUAN3000.mapper.ProjectsMapper;
+import com.nt.utils.dao.TokenModel;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,8 @@ public class ProjectsServiceImpl implements ProjectsService {
 
     //获取projectscheduleList
     @Override
-    public List<Projects> getProjectList(Projects projects) throws Exception {
-        return projectsMapper.select(projects);
+    public List<Projects> getProjectList() throws Exception {
+        return projectsMapper.getProjectList();
     }
 
     //获取记录表数据
@@ -33,55 +34,105 @@ public class ProjectsServiceImpl implements ProjectsService {
 
         String productName = followUpRecord.getProduct_nm();
         String provider = followUpRecord.getProvider();
-        return projectsMapper.getFollowUpRecordList(productName,provider);
+        return followUpRecordMapper.getFollowUpRecordList(productName,provider,"0");
     }
 
     //新建
     @Override
-    public void insert(Object object) throws Exception {
-        //随机主键
-        String id = UUID.randomUUID().toString();
+    public void insert(Object object, TokenModel tokenModel) throws Exception {
 
         if (object instanceof Projects){
             //PROJECTS
             Projects projects = new Projects();
             BeanUtils.copyProperties(projects,object);
-            projects.setProjects_id(id);
+
+            projects.preInsert(tokenModel);
             projectsMapper.insert(projects);
 
         } else if(object instanceof FollowUpRecord){
             //FOLLOWUPRECORD
             FollowUpRecord followUpRecord = new FollowUpRecord();
             BeanUtils.copyProperties(followUpRecord,object);
-            followUpRecord.setFollowuprecord_id(id);
+
+            followUpRecord.preInsert(tokenModel);
             followUpRecordMapper.insert(followUpRecord);
         }
     }
 
     //更新
     @Override
-    public void update(Object object) throws Exception {
+    public void update(Object object, TokenModel tokenModel) throws Exception {
 
         if (object instanceof Projects){
 
             Projects projects = new Projects();
             BeanUtils.copyProperties(projects,object);
+            projects.preUpdate(tokenModel);
             projectsMapper.updateByPrimaryKeySelective(projects);
 
         } else if(object instanceof FollowUpRecord){
 
             FollowUpRecord followUpRecord = new FollowUpRecord();
             BeanUtils.copyProperties(followUpRecord,object);
+            followUpRecord.preUpdate(tokenModel);
             followUpRecordMapper.updateByPrimaryKeySelective(followUpRecord);
         }
     }
 
     //删除
     @Override
-    public void delete(String productName, String provider) throws Exception {
-        //TABLE:PROJECTS
-        projectsMapper.deleteByDoubleKey("projects",productName,provider);
-        //TABLE:FOLLOWUPRECORD
-        projectsMapper.deleteByDoubleKey("followuprecord",productName,provider);
+    public void delete(Object object, TokenModel tokenModel) throws Exception {
+
+        if (object instanceof Projects){
+
+            Projects projects = new Projects();
+            BeanUtils.copyProperties(projects,object);
+            projects.preUpdate(tokenModel);
+            projectsMapper.deleteFromProjectsByDoubleKey(projects.getModifyby(),projects.getProduct_nm(),projects.getProvider(),"1");
+
+        } else if(object instanceof FollowUpRecord){
+
+            FollowUpRecord followUpRecord = new FollowUpRecord();
+            BeanUtils.copyProperties(followUpRecord,object);
+            followUpRecord.preUpdate(tokenModel);
+            followUpRecordMapper.deleteFromFollowUpRecordByDoubleKey(followUpRecord.getModifyby(),followUpRecord.getProduct_nm(),followUpRecord.getProvider(),"1");
+        }
     }
+
+    //存在Check
+    @Override
+    public Boolean existCheck(Object object) throws Exception {
+
+        if (object instanceof Projects){
+            Projects projects = new Projects();
+            BeanUtils.copyProperties(projects,object);
+           List<Projects> resultLst =  projectsMapper.existCheck(projects.getProjects_id(),"0");
+           if (resultLst.isEmpty()){
+               return false;
+           }
+
+        } else if(object instanceof FollowUpRecord){
+            FollowUpRecord followUpRecord = new FollowUpRecord();
+            BeanUtils.copyProperties(followUpRecord,object);
+            List<FollowUpRecord> resultLst =  followUpRecordMapper.existCheck(followUpRecord.getFollowuprecord_id(),"0");
+            if (resultLst.isEmpty()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //唯一性Check
+    @Override
+    public Boolean uniqueCheck(Projects projects) throws Exception {
+        List<Projects> resultLst = projectsMapper.uniqueCheck(projects.getProjects_id(), projects.getProduct_nm(),projects.getProvider());
+
+        if (resultLst.isEmpty()){
+            return false;
+        }
+
+        return true;
+    }
+
 }
--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/mapper/ProjectsMapper.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/mapper/ProjectsMapper.java
@@ -1,6 +1,5 @@
 package com.nt.service_AOCHUAN.AOCHUAN3000.mapper;
 
-import com.nt.dao_AOCHUAN.AOCHUAN3000.FollowUpRecord;
 import com.nt.dao_AOCHUAN.AOCHUAN3000.Projects;
 import com.nt.utils.MyMapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,10 +7,13 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface ProjectsMapper extends MyMapper<Projects> {
-    //获取记录列表
-    public List<FollowUpRecord> getFollowUpRecordList(@Param("product_nm") String product_nm, @Param("provider") String provider);
-
-    //删除
-    void deleteByDoubleKey(@Param("table_nm") String table_nm,@Param("product_nm") String product_nm,@Param("provider") String provider);
+    //获取-项目表
+    public List<Projects> getProjectList();
+    //删除-项目表
+    void deleteFromProjectsByDoubleKey(@Param("modifyby") String modifyby,@Param("product_nm") String product_nm,@Param("provider") String provider,@Param("status") String status);
+    //存在Check
+    public List<Projects> existCheck(@Param("id") String id,@Param("status") String status);
+    //唯一性Check
+    public List<Projects> uniqueCheck(@Param("id") String id,@Param("product_nm") String product_nm,@Param("provider") String provider);
 }
 
