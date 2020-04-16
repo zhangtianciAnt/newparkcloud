a9db25f45a195fb0847233a303aa59f8d80fce74
芦兆玮 <1098392478@qq.com>
2020-04-16 17:48:30 +0800

    mapper

--- utils/src/main/java/com/nt/utils/ExcelOutPutUtil.java
+++ utils/src/main/java/com/nt/utils/ExcelOutPutUtil.java
@@ -1,12 +1,14 @@
 package com.nt.utils;
 
 import cn.hutool.core.io.resource.ClassPathResource;
+import com.aspose.cells.SaveFormat;
+import com.aspose.cells.Workbook;
 import com.nt.utils.jxlsUtil.JxlsBuilder;
 import org.apache.poi.ss.formula.functions.T;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,7 +42,55 @@ public class ExcelOutPutUtil {
             jxlsBuilder.putVar(entry.getKey(),entry.getValue());
         }
 
-        jxlsBuilder.build();
+
     }
+
+    public static void OutPut1(String fileName, String templetName, Map<String, Object> data,HttpServletResponse response) throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("jxls_templates");
+
+        String imgRoot = resource.getPath();
+
+//        response.setContentType("application/vnd.ms-excel;charset=utf-8");
+//        response.setHeader("Content-Disposition","attachment;filename="+fileName+".xlsx");
+      //  response.setContentType(java.net.URLEncoder.encode(fileName, "UTF-8"));
+      //  ServletOutputStream out = response.getOutputStream();
+        FileOutputStream out = new FileOutputStream("");
+        JxlsBuilder jxlsBuilder = JxlsBuilder
+                /* -- 加载模板方式 -- */
+                //使用路径加载模板，可以是相对路径，也可以绝对路径
+                .getBuilder(templetName)
+                /* -- 输出文件方式 -- */
+                //指定输出的文件流
+                .out(out)
+                //设置图片路径的根目录
+                .imageRoot(imgRoot)
+                //设置如果图片缺失不终止生成
+                .ignoreImageMiss(true);
+
+        for(Map.Entry<String, Object> entry : data.entrySet()){
+            jxlsBuilder.putVar(entry.getKey(),entry.getValue());
+        }
+
+        jxlsBuilder.build();
+
+
+
+        //File pdfFile = new File("C:\\Users\\Administrator\\Desktop\\jiaban.pdf");// 输出路径
+        Workbook wb = new Workbook("C:\\Users\\Administrator\\Desktop\\jiaban.xlsx");// 原始excel路径
+        //FileOutputStream fileOS = new FileOutputStream(pdfFile);
+        wb.save(response.getOutputStream(), SaveFormat.PDF);
+        //fileOS.close();
+    }
+
+    private static void copyStream(InputStream ips,OutputStream ops) throws Exception {
+        byte[] buf = new byte[1024];
+        int len = ips.read(buf);
+        while(len != -1) {
+            ops.write(buf,0,len);
+            len = ips.read(buf);
+        }
+    }
+
 
 }
