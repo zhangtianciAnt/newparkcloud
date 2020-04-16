2d07834edeabcec2edff740f0f97ae3c72e2540b
敖婷婷 <tingting.ao@newtouch.com>
2020-04-16 18:24:22 +0800

    zyb

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN4000/ProductsService.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN4000/ProductsService.java
@@ -18,7 +18,14 @@ public interface ProductsService {
 
     void delete(String id) throws Exception;
 
-
+    /**
+     * @param product
+     * @param tokenModel
+     * @return id
+     * @throws Exception
+     * @author zhaoyoubing
+     */
+    public String insertForSupplier(Products product, TokenModel tokenModel)throws  Exception;
 
 
 }
