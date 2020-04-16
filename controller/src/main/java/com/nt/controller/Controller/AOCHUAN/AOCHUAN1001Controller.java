1047dd6f287c903426bdd2300f2f66d9e71ff775
敖婷婷 <tingting.ao@newtouch.com>
2020-04-16 18:16:41 +0800

    zyb

--- controller/src/main/java/com/nt/controller/Controller/AOCHUAN/AOCHUAN1001Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/AOCHUAN/AOCHUAN1001Controller.java
@@ -1,8 +1,11 @@
 package com.nt.controller.Controller.AOCHUAN;
 import com.nt.dao_AOCHUAN.AOCHUAN1000.Linkman;
 import com.nt.dao_AOCHUAN.AOCHUAN1000.Supplierbaseinfor;
+import com.nt.dao_AOCHUAN.AOCHUAN4000.Products;
 import com.nt.service_AOCHUAN.AOCHUAN1000.LinkmanService;
 import com.nt.service_AOCHUAN.AOCHUAN1000.SupplierbaseinforService;
+import com.nt.service_AOCHUAN.AOCHUAN1000.SupplierproductrelationService;
+import com.nt.service_AOCHUAN.AOCHUAN4000.ProductsService;
 import com.nt.utils.*;
 import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,10 @@ public class AOCHUAN1001Controller {
     private SupplierbaseinforService supplierbaseinforService;
     @Autowired
     private LinkmanService linkmanService;
+    @Autowired
+    private SupplierproductrelationService supplierproductrelationService;
+    @Autowired
+    private ProductsService productsService;
     @Autowired
     private TokenService tokenService;
 
@@ -105,6 +112,14 @@ public class AOCHUAN1001Controller {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         linkmanService.delete(id);
+        return ApiResult.success();
+    }
+    @RequestMapping(value = "/insertProduct",method={RequestMethod.POST})
+    public ApiResult insertProduct(@RequestBody Products product, HttpServletRequest request) throws Exception {
+        if(product == null){
+            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
+        }
+        linkmanBaseinfoId = supplierbaseinforService.insert(supplierbaseinfor,tokenService.getToken(request));
         return ApiResult.success();
     }
 }
---
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN1000/mapper/SupplierproductrelationMapper.java
@@ -0,0 +1,13 @@
+package com.nt.service_AOCHUAN.AOCHUAN1000.mapper;
+
+import com.nt.dao_AOCHUAN.AOCHUAN1000.Supplierproductrelation;
+import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface SupplierproductrelationMapper extends MyMapper<Supplierproductrelation> {
+    @Select("select SUPPLIERPRODUCTRELATION_ID,SUPPLIERBASEINFOR_ID,PRODUCTS_ID from supplierproductrelation WHERE SUPPLIERBASEINFOR_ID = #{supplierbaseinforId} order by createon")
+    List<Supplierproductrelation> getBySupplierbaseinforId(@Param("supplierbaseinforId") String supplierbaseinforId);
+}
