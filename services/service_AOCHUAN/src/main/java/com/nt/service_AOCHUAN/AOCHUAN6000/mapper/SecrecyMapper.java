52e5999cf6ff0e1de1b6850fda8983d61bcd49f1
mr.wang <859041441@qq.com>
2020-04-20 13:23:51 +0800

    6007

---
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/mapper/SecrecyMapper.java
@@ -0,0 +1,10 @@
+package com.nt.service_AOCHUAN.AOCHUAN6000.mapper;
+
+import com.nt.dao_AOCHUAN.AOCHUAN6000.Secrecy;
+import com.nt.utils.MyMapper;
+
+
+
+public interface SecrecyMapper extends MyMapper<Secrecy> {
+
+}
