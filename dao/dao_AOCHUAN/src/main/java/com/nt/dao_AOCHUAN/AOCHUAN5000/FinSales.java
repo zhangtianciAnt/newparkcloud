42072b6d48cb44e04addaa5e6635391c54222fca
niuchuang <chuang.niu@newtouch.cn>
2020-04-20 21:42:39 +0800

    AOCHUAN6006

---
+++ dao/dao_AOCHUAN/src/main/java/com/nt/dao_AOCHUAN/AOCHUAN5000/FinSales.java
@@ -0,0 +1,87 @@
+package com.nt.dao_AOCHUAN.AOCHUAN5000;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "fin_sales")
+public class FinSales extends BaseModel {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    //主键
+    @Id
+    @Column(name = "SALES_ID")
+    private String sales_id;
+    //合同号
+    @Column(name = "CONTRACTNUMBER")
+    private String contractnumber;
+    //产品中文
+    @Column(name = "PRODUCTUS")
+    private String productus;
+    //规格
+    @Column(name = "SPECIFICATIONS")
+    private String specifications;
+    //客户名称
+    @Column(name = "CUSTOMER")
+    private String customer;
+    //客户联系人
+    @Column(name = "ACCOUNT")
+    private String account;
+    //国家
+    @Column(name = "COUNTRY")
+    private String country;
+    //销售单价
+    @Column(name = "UNITPRICE")
+    private String unitprice;
+    //币种
+    @Column(name = "CURRENCY")
+    private String currency;
+    //数量
+    @Column(name = "AMOUNT")
+    private String amount;
+    //销售金额
+    @Column(name = "SALESAMOUNT")
+    private String salesamount;
+    //付款方式
+    @Column(name = "PAYMENT")
+    private String payment;
+    //贸易术语
+    @Column(name = "TRADETERMS")
+    private String tradeterms;
+    //收款账户
+    @Column(name = "COLLECTIONACCOUNT")
+    private String collectionaccount;
+    //销售合同回签
+    @Column(name = "SIGNBACK")
+    private String signback;
+    //销售担当
+    @Column(name = "SALERESPONSIBILITY")
+    private String saleresponsibility;
+    //回款日期
+    @Column(name = "ARRIVALTIME")
+    private String arrivaltime;
+    //订单需求
+    @Column(name = "ORDER")
+    private String order;
+    //回款状态
+    @Column(name = "ARRIVAL_STATUS")
+    private String arrival_status;
+    //凭证状态
+    @Column(name = "CREDENTIAL_STATUS")
+    private String credential_status;
+    //佣金金额
+    @Column(name = "COMMISSION_AMOUNT")
+    private String commission_amount;
+}
+
---
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN5000/Impl/FinSalesServiceImpl.java
@@ -0,0 +1,55 @@
+package com.nt.service_AOCHUAN.AOCHUAN5000.Impl;
+
+import com.nt.dao_AOCHUAN.AOCHUAN5000.FinSales;
+import com.nt.service_AOCHUAN.AOCHUAN5000.FinSalesService;
+import com.nt.service_AOCHUAN.AOCHUAN5000.mapper.FinSalesMapper;
+import com.nt.utils.dao.TokenModel;
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FinSalesServiceImpl implements FinSalesService {
+
+    @Autowired
+    private FinSalesMapper finSalesMapper;
+
+    //获取财务-销售数据
+    @Override
+    public List<FinSales> getFinSalesList(FinSales finSales) throws Exception {
+
+        return finSalesMapper.getFinSalesListByArrivalStatus(finSales.getArrival_status(), finSales.getStatus());
+    }
+
+    //更新
+    @Override
+    public void update(FinSales finSales, TokenModel tokenModel) throws Exception {
+
+        finSales.preUpdate(tokenModel);
+        finSalesMapper.updateByPrimaryKeySelective(finSales);
+    }
+
+    //存在Check
+    @Override
+    public Boolean existCheck(FinSales finSales) throws Exception {
+
+            List<FinSales> resultLst =  finSalesMapper.existCheck(finSales.getSales_id(),"0");
+            if (resultLst.isEmpty()){
+                return false;
+            }
+        return true;
+    }
+
+    //唯一性Check
+    @Override
+    public Boolean uniqueCheck(FinSales finSales) throws Exception {
+        List<FinSales> resultLst = finSalesMapper.uniqueCheck(finSales.getSales_id(), finSales.getContractnumber());
+
+        if (resultLst.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
