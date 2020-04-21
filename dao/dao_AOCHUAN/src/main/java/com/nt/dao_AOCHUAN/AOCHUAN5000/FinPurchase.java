b53419dd93c5378064db05c30b10ac02a1bb0509
niuchuang <chuang.niu@newtouch.cn>
2020-04-21 16:48:38 +0800

    AOCHUAN5002

---
+++ dao/dao_AOCHUAN/src/main/java/com/nt/dao_AOCHUAN/AOCHUAN5000/FinPurchase.java
@@ -0,0 +1,83 @@
+package com.nt.dao_AOCHUAN.AOCHUAN5000;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "fin_purchase")
+public class FinPurchase extends BaseModel {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    //主键
+    @Id
+    @Column(name = "PURCHASE_ID")
+    private String purchase_id;
+    //合同号
+    @Column(name = "CONTRACTNUMBER")
+    private String contractnumber;
+    //产品中文名
+    @Column(name = "PRODUCTEN")
+    private String producten;
+    //供应商名称
+    @Column(name = "SUPPLIER")
+    private String supplier;
+    //采购单价
+    @Column(name = "UNITPRICE1")
+    private String unitprice1;
+    //币种
+    @Column(name = "CURRENCY1")
+    private String currency1;
+    //采购金额
+    @Column(name = "PURCHASEAMOUNT")
+    private String purchaseamount;
+    //采购合同回签
+    @Column(name = "SIGNBACK1")
+    private String signback1;
+    //海关编码
+    @Column(name = "HSCODE")
+    private String hscode;
+    //退税率
+    @Column(name = "DRAWBACK")
+    private String drawback;
+    //危险品
+    @Column(name = "DANGEROUS")
+    private String dangerous;
+    //计划发货时间（提醒）
+    @Column(name = "DELIVERYTIME")
+    private String deliverytime;
+    //预计付款时间（提醒）
+    @Column(name = "PAYMENTTIME")
+    private String paymenttime;
+    //唛头
+    @Column(name = "MARKS")
+    private String marks;
+    //发货状态
+    @Column(name = "DELIVERYSTATUS")
+    private String deliverystatus;
+    //发票号码
+    @Column(name = "INVOICENUMBER")
+    private String invoicenumber;
+    //付款状态
+    @Column(name = "PAYMENTSTATUS")
+    private String paymentstatus;
+    //采购担当
+    @Column(name = "PRODUCTRESPONSIBILITY")
+    private String productresponsibility;
+    //凭证状态
+    @Column(name = "CREDENTIAL_STATUS")
+    private String credential_status;
+    //附件
+    @Column(name = "ATTACHMENT")
+    private  String attachment;
+}
---
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN5000/FinPurchaseSerivce.java
@@ -0,0 +1,21 @@
+package com.nt.service_AOCHUAN.AOCHUAN5000;
+
+import com.nt.dao_AOCHUAN.AOCHUAN5000.FinPurchase;
+import com.nt.utils.dao.TokenModel;
+
+import java.util.List;
+
+public interface FinPurchaseSerivce {
+
+    //获取财务-采购数据
+    List<FinPurchase> getFinPurchaseList(FinPurchase finPurchase) throws  Exception;
+
+    //更新
+    void update(FinPurchase finSales, TokenModel tokenModel) throws Exception;
+
+    //存在Check
+    Boolean existCheck(FinPurchase finPurchase) throws Exception;
+
+    //唯一性Check
+    Boolean uniqueCheck(FinPurchase finPurchase) throws Exception;
+}
