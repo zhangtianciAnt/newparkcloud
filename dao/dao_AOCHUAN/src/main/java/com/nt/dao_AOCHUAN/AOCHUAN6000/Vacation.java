29c0fb14430d83160d8e59001b971feb551592c7
王鑫磊 <xinlei.wang@newtouch.com>
2020-04-22 17:45:28 +0800

    休假管理

---
+++ dao/dao_AOCHUAN/src/main/java/com/nt/dao_AOCHUAN/AOCHUAN6000/Vacation.java
@@ -0,0 +1,71 @@
+package com.nt.dao_AOCHUAN.AOCHUAN6000;
+
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "vacation")
+public class Vacation extends BaseModel {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    //主键
+    @Id
+    @Column(name = "VACATION_ID")
+    private String vacation_id;
+
+
+    @Column(name = "DEPARTMENT")
+    private String department;
+
+
+    @Column(name = "APPLICANTER")
+    private String applicanter;
+
+
+    @Column(name = "APPLICATIONTIM")
+    private Date applicationtim;
+
+
+    @Column(name = "APPLICATIONTYPE")
+    private String applicationtype;
+
+
+    @Column(name = "STARTDATE")
+    private Date startdate;
+
+
+    @Column(name = "ENDDATE")
+    private Date enddate;
+
+
+    @Column(name = "STARTTIM")
+    private String starttim;
+
+
+    @Column(name = "ENDTIM")
+    private String endtim;
+
+
+    @Column(name = "SUMTIM")
+    private String sumtim;
+
+    @Column(name = "ANNEX")
+    private String annex;
+
+    @Column(name = "REMARKS")
+    private String remarks;
+
+
+}
---
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/VacationService.java
@@ -0,0 +1,24 @@
+package com.nt.service_AOCHUAN.AOCHUAN6000;
+
+
+import com.nt.dao_AOCHUAN.AOCHUAN4000.Products;
+import com.nt.dao_AOCHUAN.AOCHUAN6000.Vacation;
+import com.nt.utils.dao.TokenModel;
+
+import java.util.List;
+
+public interface VacationService {
+
+    List<Vacation> get(Vacation vacation) throws Exception;
+
+    public void insert(Vacation vacation, TokenModel tokenModel)throws  Exception;
+
+    public Vacation One(String ids)throws  Exception;
+
+    public void update(Vacation vacation, TokenModel tokenModel)throws  Exception;
+
+    void delete(String id) throws Exception;
+
+
+
+}
