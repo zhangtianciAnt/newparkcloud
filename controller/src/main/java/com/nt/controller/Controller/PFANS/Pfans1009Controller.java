cb5c7f746132c84b53c5fc0aae27324ea5ef7780
赵国栋 <guodong.zhao@newtouch.cn>
2020-04-22 19:56:41 +0800

    moban

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1009Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1009Controller.java
@@ -1,22 +1,30 @@
 package com.nt.controller.Controller.PFANS;
 
 import com.nt.dao_Assets.Assets;
+import com.nt.dao_Org.CustomerInfo;
 import com.nt.dao_Pfans.PFANS1000.Fixedassets;
+import com.nt.dao_Workflow.Vo.StartWorkflowVo;
+import com.nt.dao_Workflow.Vo.WorkflowLogDetailVo;
 import com.nt.service_Assets.AssetsService;
+import com.nt.service_WorkFlow.WorkflowServices;
 import com.nt.service_pfans.PFANS1000.FixedassetsService;
-import com.nt.utils.ApiResult;
-import com.nt.utils.MessageUtil;
-import com.nt.utils.MsgConstants;
-import com.nt.utils.RequestUtils;
+import com.nt.utils.*;
 import com.nt.utils.dao.TokenModel;
 import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/fixedassets")
@@ -26,7 +34,14 @@ public class Pfans1009Controller {
     private FixedassetsService fixedassetsService;
 
     @Autowired
+    private WorkflowServices workflowServices;
+
+    @Autowired
     private TokenService tokenService;
+
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Autowired
     private AssetsService assetsService;
@@ -37,7 +52,6 @@ public class Pfans1009Controller {
         Fixedassets fixedassets = new Fixedassets();
         fixedassets.setOwners(tokenModel.getOwnerList());
         return ApiResult.success(fixedassetsService.getFixedassets(fixedassets));
-
     }
 
     @RequestMapping(value = "/one",method={RequestMethod.POST})
@@ -77,7 +91,80 @@ public class Pfans1009Controller {
         TokenModel tokenModel = tokenService.getToken(request);
         assets.setOwners(tokenModel.getOwnerList());
         return ApiResult.success(assetsService.getAssetsnameList(assets, request));
+    }
 
+    @RequestMapping(value = "/downLoad1", method = {RequestMethod.POST})
+    public void downLoad1(@RequestBody List<String> fixedassetsList, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        TokenModel tokenModel = tokenService.getToken(request);
+        for (int i = 0; i < fixedassetsList.size(); i++) {
+            Fixedassets fxs = fixedassetsService.One(fixedassetsList.get(i));
+            String pp[] = fxs.getRepair().split(" ~ ");
+            if (fxs.getRepairkits().equals("PJ010001")) {
+                fxs.setRepairkits("有");
+            } else {
+                fxs.setRepairkits("无");
+            }
+            Query query = new Query();
+            query.addCriteria(Criteria.where("userid").is(fxs.getUser_id()));
+            CustomerInfo customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+            if (customerInfo != null) {
+                fxs.setUser_id(customerInfo.getUserinfo().getCustomername());
+            }
+            String wfList1 = "";
+            String wfList2 = "";
+            String wfList3 = "";
+            String wfList4 = "";
+            String wfList5 = "";
+            StartWorkflowVo startWorkflowVo = new StartWorkflowVo();
+            startWorkflowVo.setDataId(fxs.getFixedassets_id());
+            List<WorkflowLogDetailVo> wfList = workflowServices.ViewWorkflow2(startWorkflowVo, tokenModel.getLocale());
+            if (wfList.size() > 0) {
+                query = new Query();
+                query.addCriteria(Criteria.where("userid").is(wfList.get(0).getUserId()));
+                customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+                if (customerInfo != null) {
+                    wfList1 = customerInfo.getUserinfo().getCustomername();
+                }
+                query = new Query();
+                query.addCriteria(Criteria.where("userid").is(wfList.get(1).getUserId()));
+                customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+                if (customerInfo != null) {
+                    wfList2 = customerInfo.getUserinfo().getCustomername();
+                }
+                query = new Query();
+                query.addCriteria(Criteria.where("userid").is(wfList.get(2).getUserId()));
+                customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+                if (customerInfo != null) {
+                    wfList3 = customerInfo.getUserinfo().getCustomername();
+                }
+                query = new Query();
+                query.addCriteria(Criteria.where("userid").is(wfList.get(3).getUserId()));
+                customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+                if (customerInfo != null) {
+                    wfList4 = customerInfo.getUserinfo().getCustomername();
+                }
+                query = new Query();
+                query.addCriteria(Criteria.where("userid").is(wfList.get(4).getUserId()));
+                customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+                if (customerInfo != null) {
+                    wfList5 = customerInfo.getUserinfo().getCustomername();
+                }
+            }
+            Map<String, Object> data = new HashMap<>();
+            data.put("fxs", fxs);
+            data.put("wfList1", wfList1);
+            data.put("wfList2", wfList2);
+            data.put("wfList3", wfList3);
+            data.put("wfList4", wfList4);
+            data.put("wfList5", wfList5);
+            data.put("wfList", wfList);
+            if (pp.length > 0) {
+                data.put("statime", pp);
+            } else {
+                data.put("statime", "");
+            }
+            ExcelOutPutUtil.OutPutPdf("固定資産貸出修理持出決裁願", "gudingzichan_jiechuxiulichichu.xls", data, response);
+        }
     }
 }
 
