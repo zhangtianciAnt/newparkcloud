0cc1e43f01e26e477cb0a6ebfe703db5b6f50e43
niuchuang <chuang.niu@newtouch.cn>
2020-04-22 21:24:11 +0800

    search id

---
+++ dao/dao_AOCHUAN/src/main/java/com/nt/dao_AOCHUAN/AOCHUAN3000/Vo/FinReport.java
@@ -0,0 +1,17 @@
+package com.nt.dao_AOCHUAN.AOCHUAN3000.Vo;
+
+import com.nt.dao_AOCHUAN.AOCHUAN3000.TransportGood;
+import com.nt.utils.dao.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FinReport{
+
+    //走货
+    private TransportGood transportGood;
+
+}
