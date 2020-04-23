72685e833ae9bfadd3dd303e38bdf82c308275e1
赵国栋 <guodong.zhao@newtouch.cn>
2020-04-23 20:52:10 +0800

    moban

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1008Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1008Controller.java
@@ -1,8 +1,10 @@
 package com.nt.controller.Controller.PFANS;
 
 import com.nt.dao_Org.CustomerInfo;
+import com.nt.dao_Org.Dictionary;
 import com.nt.dao_Pfans.PFANS1000.Softwaretransfer;
 import com.nt.dao_Pfans.PFANS1000.Vo.SoftwaretransferVo;
+import com.nt.service_Org.DictionaryService;
 import com.nt.service_pfans.PFANS1000.SoftwaretransferService;
 import com.nt.utils.*;
 import com.nt.utils.dao.TokenModel;
@@ -30,6 +32,8 @@ public class Pfans1008Controller {
     private SoftwaretransferService softwaretransferService;
     @Autowired
     private MongoTemplate mongoTemplate;
+    @Autowired
+    private DictionaryService dictionaryService;
     @Autowired
     private TokenService tokenService;
 
@@ -74,17 +78,77 @@ public class Pfans1008Controller {
         TokenModel tokenModel = tokenService.getToken(request);
 //        for (int i = 0; i < softwaretransferList.size(); i++) {
         SoftwaretransferVo soft = softwaretransferService.selectById(softwaretransferId);
-        String groupName1 = "";
-        String groupName2 = "";
+
         Query query = new Query();
         query.addCriteria(Criteria.where("userid").is(soft.getSoftwaretransfer().getUser_id()));
         CustomerInfo customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
         if (customerInfo != null) {
             soft.getSoftwaretransfer().setUser_id(customerInfo.getUserinfo().getCustomername());
         }
+        List<Dictionary> curList = dictionaryService.getForSelect("JY002");
+        for (Dictionary item : curList) {
+            if (item.getCode().equals(soft.getSoftwaretransfer().getFerrybudgetunit())) {
+                soft.getSoftwaretransfer().setFerrybudgetunit(item.getValue1());
+            }
+        }
+        List<Dictionary> curList1 = dictionaryService.getForSelect("JY002");
+        for (Dictionary item : curList1) {
+            if (item.getCode().equals(soft.getSoftwaretransfer().getTubebudgetunit())) {
+                soft.getSoftwaretransfer().setTubebudgetunit(item.getValue1());
+            }
+        }
+
+
+        //region 查询TL
+        String StrTl = "";
+        query = new Query();
+        String strFerryteam_id = soft.getSoftwaretransfer().getFerryteam_id();
+        query.addCriteria(Criteria.where("userinfo.teamid").is(strFerryteam_id).and("userinfo.post").is("PG021005"));
+        customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+        if (customerInfo != null) {
+            StrTl = customerInfo.getUserinfo().getCustomername();
+        }
+        //endregion
+
+        //region 查询GM
+        String StrGM = "";
+        query = new Query();
+        String strFerrygroup_id = soft.getSoftwaretransfer().getFerrygroup_id();
+        query.addCriteria(Criteria.where("userinfo.groupid").is(strFerrygroup_id).and("userinfo.post").is("PG021002"));
+        customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+        if (customerInfo != null) {
+            StrGM = customerInfo.getUserinfo().getCustomername();
+        }
+        //endregion
+
+
+        //region 查询TL
+        String CtrTL = "";
+        query = new Query();
+        String strTubeteam_id = soft.getSoftwaretransfer().getTubeteam_id();
+        query.addCriteria(Criteria.where("userinfo.teamid").is(strTubeteam_id).and("userinfo.post").is("PG021005"));
+        customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+        if (customerInfo != null) {
+            CtrTL = customerInfo.getUserinfo().getCustomername();
+        }
+        //endregion
+
+        //region 查询GM
+        String CtrGM = "";
+        query = new Query();
+        String strTubegroup_id = soft.getSoftwaretransfer().getTubegroup_id();
+        query.addCriteria(Criteria.where("userinfo.groupid").is(strTubegroup_id).and("userinfo.post").is("PG021002"));
+        customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
+        if (customerInfo != null) {
+            CtrGM = customerInfo.getUserinfo().getCustomername();
+        }
+        //endregion
             Map<String, Object> data = new HashMap<>();
             data.put("soft", soft);
-            data.put("notn", soft.getNotification());
+        data.put("StrTl", StrTl);
+        data.put("StrGM", StrGM);
+        data.put("CtrTL", CtrTL);
+        data.put("CtrGM", CtrGM);
             ExcelOutPutUtil.OutPutPdf("固定资产·软件移转申请", "gudingzichanfq.xls", data, response);
         }
 //    }
