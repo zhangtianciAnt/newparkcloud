09aac197c04a28f2760cbf60e58842fda0aff02f
niuchuang <chuang.niu@newtouch.cn>
2020-04-26 20:05:02 +0800

    upd

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN1000/Impl/SupplierbaseinforServiceImpl.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN1000/Impl/SupplierbaseinforServiceImpl.java
@@ -45,4 +45,14 @@ public class SupplierbaseinforServiceImpl implements SupplierbaseinforService {
     public void delete(String id) throws Exception {
         supplierbaseinforMapper.deleteByPrimaryKey(id);
     }
+
+    /**
+     * 获取不在项目表中的供应商
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<Supplierbaseinfor> getSuppliersExceptUnique() throws Exception {
+        return supplierbaseinforMapper.getSuppliersExceptUnique();
+    }
 }
