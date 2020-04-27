64c29afbf833bae684b75a574c36d71cdd723d5c
芦兆玮 <1098392478@qq.com>
2020-04-27 13:44:49 +0800

    3001

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/Impl/QuotationsServiceImpl.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN3000/Impl/QuotationsServiceImpl.java
@@ -2,7 +2,7 @@ package com.nt.service_AOCHUAN.AOCHUAN3000.Impl;
 
 import com.nt.dao_AOCHUAN.AOCHUAN3000.Enquiry;
 import com.nt.dao_AOCHUAN.AOCHUAN3000.Quotations;
-import com.nt.dao_AOCHUAN.AOCHUAN3000.Vo.QuotationsAndEnquiry;
+import com.nt.dao_AOCHUAN.AOCHUAN3000.Vo.QuoAndEnq;
 import com.nt.dao_Auth.Vo.MembersVo;
 import com.nt.dao_Org.ToDoNotice;
 import com.nt.service_AOCHUAN.AOCHUAN3000.QuotationsService;
@@ -70,7 +70,7 @@ public class QuotationsServiceImpl implements QuotationsService {
     }
 
     @Override
-    public List<QuotationsAndEnquiry> getForSupplier(String id) throws Exception {
+    public List<QuoAndEnq> getForSupplier(String id) throws Exception {
         return quotationsMapper.getForSupplier(id);
     }
 
