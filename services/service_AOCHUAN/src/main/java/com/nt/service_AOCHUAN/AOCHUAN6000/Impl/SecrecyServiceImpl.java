05778e300cbed6e376fc317b2d49bae0d61e6d0d
mr.wang <859041441@qq.com>
2020-04-28 10:59:03 +0800

    6007

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/Impl/SecrecyServiceImpl.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/Impl/SecrecyServiceImpl.java
@@ -1,15 +1,24 @@
 package com.nt.service_AOCHUAN.AOCHUAN6000.Impl;
 
+
 import com.nt.dao_AOCHUAN.AOCHUAN6000.Secrecy;
+import com.nt.dao_Auth.Vo.MembersVo;
+import com.nt.dao_Org.ToDoNotice;
 import com.nt.service_AOCHUAN.AOCHUAN6000.SecrecyService;
+
+import com.nt.service_AOCHUAN.AOCHUAN6000.SelectSecrecy;
 import com.nt.service_AOCHUAN.AOCHUAN6000.mapper.SecrecyMapper;
+import com.nt.service_Org.ToDoNoticeService;
 import com.nt.utils.dao.TokenModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.UUID;
+
+import java.util.*;
 
 @Service
 @Transactional(rollbackFor=Exception.class)
@@ -17,6 +26,9 @@ public class SecrecyServiceImpl implements SecrecyService {
 
     @Autowired
     private SecrecyMapper secrecyMapper;
+
+    @Autowired
+    private ToDoNoticeService toDoNoticeService;
 
 
 
@@ -52,4 +64,37 @@ public class SecrecyServiceImpl implements SecrecyService {
         secrecy.setStatus("1");
         secrecyMapper.updateByPrimaryKey(secrecy);
     }
+
+
+    //系统服务（4月1日）
+    @Scheduled(cron="59 * * * * ?")
+    public void changestatus() throws Exception {
+        List<Secrecy> secrelist = secrecyMapper.selectsecrecy();
+        for( Secrecy secrecy: secrelist){
+        ToDoNotice(secrecy);
+        secrecy.setType("1");
+            secrecyMapper.updateByPrimaryKey(secrecy);
+        }
+
+    }
+
+
+    //生成代办
+    @Async
+    public void ToDoNotice(Secrecy secrecy) throws Exception {
+        // 创建代办
+
+        ToDoNotice toDoNotice = new ToDoNotice();
+        toDoNotice.setTitle("你有已归档文件");
+        toDoNotice.setInitiator(secrecy.getResponsible()); /*发起人*/
+        toDoNotice.setContent("归档文件编号【" + secrecy.getNo() + "】");
+        toDoNotice.setDataid(secrecy.getSecrecy_id());
+        toDoNotice.setUrl("/AOCHUAN6007FormView");
+        //toDoNotice.preInsert(tokenModel);
+        toDoNotice.setOwner(secrecy.getResponsible());
+        toDoNoticeService.save(toDoNotice);
+
+    }
+
+
 }
