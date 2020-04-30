5b6f4d4e0fd53f144cd304ef9f39008d1076d1e0
wangshuai <1203765575@qq.com>
2020-04-30 16:05:48 +0800

    公共费用决裁已关联得精算

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/mapper/PublicExpenseMapper.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/mapper/PublicExpenseMapper.java
@@ -2,9 +2,11 @@ package com.nt.service_pfans.PFANS1000.mapper;
 
 import com.nt.dao_Pfans.PFANS1000.PublicExpense;
 import com.nt.utils.MyMapper;
-
+import org.apache.ibatis.annotations.Param;
 import java.util.Date;
+import java.util.List;
 
 public interface PublicExpenseMapper extends MyMapper<PublicExpense> {
     Integer getInvoiceNo(String reimbursementDate);
+    List<PublicExpense> getpublicelist(@Param("code") String publicexpenseid);
 }
