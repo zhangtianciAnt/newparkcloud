4d312c59617073d5d5f7bc9efa5924b22be84904
wangshuai <1203765575@qq.com>
2020-05-09 20:55:50 +0800

    修改sql

--- dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS6000/Vo/bpSum3Vo.java
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS6000/Vo/bpSum3Vo.java
@@ -14,10 +14,19 @@ public class bpSum3Vo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String GROUP_ID;
-
     private String SUPPLIERNAME;
+    private String yaer;
+    private String APRIL;
+    private String MAY;
+    private String JUNE;
+    private String JULY;
+    private String AUGUST;
+    private String SEPTEMBER;
+    private String OCTOBER;
+    private String NOVEMBER;
+    private String DECEMBER;
+    private String JANUARY;
+    private String FEBRUARY;
+    private String MARCH;
 
-    private String date;
-
-    private String counts;
 }
