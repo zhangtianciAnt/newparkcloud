8e41d577118bef480f37e248b8ee14511c7bd357
wangshuai <1203765575@qq.com>
2020-05-21 13:35:56 +0800

    No.50-内采时，增加委托元项目

--- dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS5000/CompanyProjects.java
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS5000/CompanyProjects.java
@@ -315,7 +315,13 @@ public class CompanyProjects extends BaseModel {
      */
     @Column(name = "OUENTRUST")
     private String ouentrust;
-
+//    add-ws-No.50-内采时，增加委托元项目
+    /**
+     * 是否允许内采
+     */
+    @Column(name = "NEICE")
+    private String neice;
+//    add-ws-No.50-内采时，增加委托元项目
     /**
      * 实际完成时间
      */
