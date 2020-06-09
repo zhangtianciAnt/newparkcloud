fea5a8f4a23488d4b3f7ef7ac6eee9390449a7c3
高兵兵 <bingbing.gao@newtouch.com>
2020-06-09 11:24:01 +0800

    工资计算:上月工资取到修改

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/WagesMapper.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/WagesMapper.java
@@ -11,6 +11,8 @@ public interface WagesMapper extends MyMapper<Wages> {
 
     List<Wages> selectWage();
 
+    List<Wages> lastWages(@Param("year") int year, @Param("month") int month);
+
     List<String> lastMonthWage(@Param("year") int year, @Param("month") int month);
 
 //    List<Wages> getWageList(Wages wages);
