5e24f86d645bd6c60cf6814cef245805c4e77ed0
shubo <1078680188@qq.com>
2020-06-15 19:12:05 +0800

    发布

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/AttendancesService.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/AttendancesService.java
@@ -3,26 +3,31 @@ package com.nt.service_AOCHUAN.AOCHUAN6000;
 import com.nt.dao_AOCHUAN.AOCHUAN6000.Attendance;
 import com.nt.utils.dao.EWxBaseResponse;
 import com.nt.utils.dao.TokenModel;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface AttendancesService {
 
     List<Attendance> get(Attendance attendance) throws Exception;
-//    异常数据
+
+    //    异常数据
     List<Attendance> getYICHANG(Attendance attendance) throws Exception;
 
     List<Attendance> getNow(Attendance attendance) throws Exception;
 
     List<Attendance> getNowMons(String id, String attendancetim) throws Exception;
 
-    List<Attendance> getNowMonYC(String id,String attendancetim) throws Exception;
+    List<Attendance> getNowMonYC(String id, String attendancetim) throws Exception;
 
-    List<String> importUser(HttpServletRequest request, TokenModel tokenModel) throws Exception ;
+    List<String> importUser(HttpServletRequest request, TokenModel tokenModel) throws Exception;
 
     List<Attendance> getByUserId(String userId) throws Exception;
 
     //获取企业微信打卡
-    List<Attendance> getCheckInData (EWxBaseResponse data) throws Exception;
+    List<Attendance> getCheckInData(EWxBaseResponse data) throws Exception;
+
+    //自动获取企业微信打卡
+    List<Attendance> getAutoCheckInData(EWxBaseResponse data) throws Exception;
 
 }
