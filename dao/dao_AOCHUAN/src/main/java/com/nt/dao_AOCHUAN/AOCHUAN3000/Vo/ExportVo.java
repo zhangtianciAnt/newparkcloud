6c4a23e23f2f79bab44c812b1078259f154a0cdc
DH <1078680188@qq.com>
2020-06-23 11:15:14 +0800

    导出

---
+++ dao/dao_AOCHUAN/src/main/java/com/nt/dao_AOCHUAN/AOCHUAN3000/Vo/ExportVo.java
@@ -0,0 +1,34 @@
+package com.nt.dao_AOCHUAN.AOCHUAN3000.Vo;
+
+import com.nt.dao_AOCHUAN.AOCHUAN3000.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExportVo {
+
+    /**
+     * 走货
+     */
+    private TransportGood transportgood;
+
+    /**
+     * 走货-销售明细子表
+     */
+    private Saledetails saledetails;
+
+    /**
+     * 走货-用款申请记录子表
+     */
+    private Applicationrecord applicationrecord;
+
+//    /**
+//     * 记录表
+//     */
+//    private List<Reg_Record> reg_records;
+}
