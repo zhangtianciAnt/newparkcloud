f54d801b21bc0cdf80eaf17b2c5b05dcf54ba642
zhaowei.lu <1098392478@qq.ocm>
2020-06-29 10:09:14 +0800

    Merge branch 'AOCHUAN-DEV' of E:\代码\newparkcloud with conflicts.

--- controller/src/main/java/com/nt/controller/Controller/AOCHUAN/AOCHUAN3002Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/AOCHUAN/AOCHUAN3002Controller.java
@@ -1,8 +1,12 @@
 package com.nt.controller.Controller.AOCHUAN;
 
+import com.nt.dao_AOCHUAN.AOCHUAN3000.Applicationrecord;
+import com.nt.dao_AOCHUAN.AOCHUAN3000.Enquiry;
+import com.nt.dao_AOCHUAN.AOCHUAN3000.Quotations;
 import com.nt.dao_AOCHUAN.AOCHUAN3000.TransportGood;
 import com.nt.dao_AOCHUAN.AOCHUAN5000.FinPurchase;
 import com.nt.dao_AOCHUAN.AOCHUAN5000.FinSales;
+import com.nt.service_AOCHUAN.AOCHUAN3000.QuotationsService;
 import com.nt.service_AOCHUAN.AOCHUAN3000.TransportGoodService;
 import com.nt.utils.*;
 import com.nt.utils.dao.TokenModel;
@@ -12,7 +16,14 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/transportgood")
@@ -23,7 +34,7 @@ public class AOCHUAN3002Controller {
     @Autowired
     private TokenService tokenService;
 
-    @RequestMapping(value = "/get", method = {RequestMethod.GET})
+    @RequestMapping(value = "/get",method={RequestMethod.GET})
     public ApiResult get(HttpServletRequest request) throws Exception {
         TokenModel tokenModel = tokenService.getToken(request);
         TransportGood transportGood = new TransportGood();
@@ -31,51 +42,51 @@ public class AOCHUAN3002Controller {
         return ApiResult.success(transportGoodService.get(transportGood));
     }
 
-    @RequestMapping(value = "/getone", method = {RequestMethod.GET})
+    @RequestMapping(value = "/getone",method={RequestMethod.GET})
     public ApiResult getOne(@RequestParam String id, HttpServletRequest request) throws Exception {
-        if (!StringUtils.isNotBlank(id)) {
+        if(!StringUtils.isNotBlank(id)){
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         return ApiResult.success(transportGoodService.getOne(id));
     }
 
-    @RequestMapping(value = "/getForSupplier", method = {RequestMethod.GET})
+    @RequestMapping(value = "/getForSupplier",method={RequestMethod.GET})
     public ApiResult getForSupplier(@RequestParam String id, HttpServletRequest request) throws Exception {
-        if (!StringUtils.isNotBlank(id)) {
+        if(!StringUtils.isNotBlank(id)){
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         return ApiResult.success(transportGoodService.getForSupplier(id));
     }
 
-    @RequestMapping(value = "/getForCustomer", method = {RequestMethod.GET})
+    @RequestMapping(value = "/getForCustomer",method={RequestMethod.GET})
     public ApiResult getForCustomer(@RequestParam String id, HttpServletRequest request) throws Exception {
-        if (!StringUtils.isNotBlank(id)) {
+        if(!StringUtils.isNotBlank(id)){
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         return ApiResult.success(transportGoodService.getForCustomer(id));
     }
 
-    @RequestMapping(value = "/update", method = {RequestMethod.POST})
+    @RequestMapping(value = "/update",method={RequestMethod.POST})
     public ApiResult update(@RequestBody TransportGood transportGood, HttpServletRequest request) throws Exception {
-        if (transportGood == null) {
+        if(transportGood == null){
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
-        transportGoodService.update(transportGood, tokenService.getToken(request));
+        transportGoodService.update(transportGood,tokenService.getToken(request));
         return ApiResult.success();
     }
 
-    @RequestMapping(value = "/insert", method = {RequestMethod.POST})
+    @RequestMapping(value = "/insert",method={RequestMethod.POST})
     public ApiResult insert(@RequestBody TransportGood transportGood, HttpServletRequest request) throws Exception {
-        if (transportGood == null) {
+        if(transportGood == null){
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
-        transportGoodService.insert(transportGood, tokenService.getToken(request));
+        transportGoodService.insert(transportGood,tokenService.getToken(request));
         return ApiResult.success();
     }
 
-    @RequestMapping(value = "/delete", method = {RequestMethod.GET})
+    @RequestMapping(value = "/delete",method={RequestMethod.GET})
     public ApiResult delete(@RequestParam String id, HttpServletRequest request) throws Exception {
-        if (!StringUtils.isNotBlank(id)) {
+        if(!StringUtils.isNotBlank(id)){
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
         transportGoodService.delete(id);
@@ -100,7 +111,7 @@ public class AOCHUAN3002Controller {
 //        return ApiResult.success();
 //    }
 
-    @RequestMapping(value = "/paymentcg", method = {RequestMethod.POST})
+    @RequestMapping(value = "/paymentcg",method={RequestMethod.POST})
     public ApiResult paymentCG(@RequestBody List<FinSales> finSales, HttpServletRequest request) throws Exception {
         if (finSales == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
@@ -109,7 +120,7 @@ public class AOCHUAN3002Controller {
         return ApiResult.success();
     }
 
-    @RequestMapping(value = "/paymentxs", method = {RequestMethod.POST})
+    @RequestMapping(value = "/paymentxs",method={RequestMethod.POST})
     public ApiResult paymentXS(@RequestBody List<FinPurchase> finPurchases, HttpServletRequest request) throws Exception {
         if (finPurchases == null) {
             return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
@@ -118,14 +129,34 @@ public class AOCHUAN3002Controller {
         return ApiResult.success();
     }
 
-    @RequestMapping(value = "/getexport", method = {RequestMethod.POST})
-    public ApiResult getexport(@RequestBody List<TransportGood> exportVo, HttpServletResponse response) throws Exception {
-
-        for (int i = 0; i < exportVo.size(); i++) {
-            String id = exportVo.get(i).getTransportgood_id();
-            transportGoodService.setExport(id, response);
+    @RequestMapping(value = "/getexport",method={RequestMethod.GET})
+    public void getexport(@RequestParam String id, HttpServletResponse response) throws Exception {
+        if(!StringUtils.isNotBlank(id)){
+//            return ApiResult.fail(MessageUtil.getMessage(MsgConstants.ERROR_03, RequestUtils.CurrentLocale(request)));
         }
-        return ApiResult.success();
+
+//        transportGoodService.setExport(id , response);
+
+        /* 设置文件ContentType类型，这样设置，会自动判断下载文件类型 */
+     //   response.setContentType(java.net.URLEncoder.encode("aaa.xlsx", "UTF-8"));
+        /* 设置文件头：最后一个参数是设置下载文件名 */
+        //response.setHeader("Content-Disposition", "attachment;filename=bbab.xlsx");
+        response.setHeader("content-Type","application/ms-excel");
+        response.setHeader("Content-Disposition","attachment;filename=" + URLEncoder.encode("aaa.xlsx", "UTF-8"));
+        response.setCharacterEncoding("UTF-8");
+        try (
+                InputStream ins = new FileInputStream("C:\\Users\\llll\\Desktop\\aaa.xlsx");
+                OutputStream os = response.getOutputStream()
+        ) {
+            byte[] b = new byte[1024];
+            int len;
+            while ((len = ins.read(b)) > 0) {
+                os.write(b, 0, len);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+//        return ApiResult.success();
     }
 
 
