86460196bdb45b33139c8e77107e5d40c4482ae5
wangshuai <1203765575@qq.com>
2020-07-07 15:39:01 +0800

    公共费用精算修改禅道169任务

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1012Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1012Controller.java
@@ -232,7 +232,7 @@ public class Pfans1012Controller {
             loanapplication.setLoanapplication_id(pubvo.getPublicexpense().getLoan());
             List<LoanApplication> list = loanapplicationMapper.select(loanapplication);
             if (list.size() > 0) {
-                if(list.get(0).getUser_name().equals("PJ015002")){
+                if(list.get(0).getPaymentmethod().equals("PJ015002")){
                     query.addCriteria(Criteria.where("userid").is(list.get(0).getUser_name()));
                     customerInfo = mongoTemplate.findOne(query, CustomerInfo.class);
                     if (customerInfo != null) {
