dbdcfd8081e2768c38fccc6c792828960987f2b4
zhaowei.lu <1098392478@qq.ocm>
2020-07-13 11:36:15 +0800

    peizhi

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/Impl/AttendancesServiceImpl.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN6000/Impl/AttendancesServiceImpl.java
@@ -519,43 +519,87 @@ public class AttendancesServiceImpl implements AttendancesService {
                 for (int k = 0; k < data.getCheckindata().size(); k++) {
                     if (val.equals(data.getCheckindata().get(k).getUserid())) {
                         if (!"上班打卡".equals(data.getCheckindata().get(k).getCheckin_type())) {
-                            long endepoch = data.getCheckindata().get(k).getCheckin_time();
-                            Date dateendepoch = new Date(endepoch * 1000);
-                            String stroff = df.format(dateendepoch);
-                            //下班打卡时间
-                            String off = stroff.substring(11);
-                            String wo = stroff.substring(0, 11);
-                            String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
-                            Calendar cal = Calendar.getInstance();
-                            cal.setTime(dateendepoch);
-                            int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
-                            if (week_index < 0) {
-                                week_index = 0;
+                            if ("未打卡".equals(data.getCheckindata().get(k).getException_type())) {
+                                long endepoch = data.getCheckindata().get(k).getCheckin_time();
+                                Date dateendepoch = new Date(endepoch * 1000);
+                                String stroff = df.format(dateendepoch);
+                                //下班打卡时间
+                                String off = "";
+                                String wo = stroff.substring(0, 11);
+                                String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+                                Calendar cal = Calendar.getInstance();
+                                cal.setTime(dateendepoch);
+                                int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
+                                if (week_index < 0) {
+                                    week_index = 0;
+                                }
+                                String xingqi = weeks[week_index];
+                                wo = wo + "" + xingqi;
+                                attendance.setAttendancetim(wo);
+                                attendance.setOffhours(off);
+                            }else {
+                                long endepoch = data.getCheckindata().get(k).getCheckin_time();
+                                Date dateendepoch = new Date(endepoch * 1000);
+                                String stroff = df.format(dateendepoch);
+                                //下班打卡时间
+                                String off = stroff.substring(11);
+                                String wo = stroff.substring(0, 11);
+                                String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+                                Calendar cal = Calendar.getInstance();
+                                cal.setTime(dateendepoch);
+                                int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
+                                if (week_index < 0) {
+                                    week_index = 0;
+                                }
+                                String xingqi = weeks[week_index];
+                                wo = wo + "" + xingqi;
+                                attendance.setAttendancetim(wo);
+                                attendance.setOffhours(off);
                             }
-                            String xingqi = weeks[week_index];
-                            wo = wo + "" + xingqi;
-                            attendance.setAttendancetim(wo);
-                            attendance.setOffhours(off);
+
                         }
                         if (!"下班打卡".equals(data.getCheckindata().get(k).getCheckin_type())) {
-                            long strepoch = data.getCheckindata().get(k).getCheckin_time();
-                            Date datestrepoch = new Date(strepoch * 1000);
-                            String strworking = df.format(datestrepoch);
-                            String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
-                            Calendar cal = Calendar.getInstance();
-                            cal.setTime(datestrepoch);
-                            int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
-                            if (week_index < 0) {
-                                week_index = 0;
+                            if ("未打卡".equals(data.getCheckindata().get(k).getException_type())) {
+                                long strepoch = data.getCheckindata().get(k).getCheckin_time();
+                                Date datestrepoch = new Date(strepoch * 1000);
+                                String strworking = df.format(datestrepoch);
+                                String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+                                Calendar cal = Calendar.getInstance();
+                                cal.setTime(datestrepoch);
+                                int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
+                                if (week_index < 0) {
+                                    week_index = 0;
+                                }
+                                String xingqi = weeks[week_index];
+                                //上班打卡时间
+                                String working = "";
+                                String wo = strworking.substring(0, 11);
+                                wo = wo + "" + xingqi;
+                                attendance.setAttendancetim(wo);
+                                attendance.setWorkinghours(working);
+                            }else{
+                                long strepoch = data.getCheckindata().get(k).getCheckin_time();
+                                Date datestrepoch = new Date(strepoch * 1000);
+                                String strworking = df.format(datestrepoch);
+                                String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+                                Calendar cal = Calendar.getInstance();
+                                cal.setTime(datestrepoch);
+                                int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
+                                if (week_index < 0) {
+                                    week_index = 0;
+                                }
+                                String xingqi = weeks[week_index];
+                                //上班打卡时间
+                                String working = strworking.substring(11);
+                                String wo = strworking.substring(0, 11);
+                                wo = wo + "" + xingqi;
+                                attendance.setAttendancetim(wo);
+                                attendance.setWorkinghours(working);
                             }
-                            String xingqi = weeks[week_index];
-                            //上班打卡时间
-                            String working = strworking.substring(11);
-                            String wo = strworking.substring(0, 11);
-                            wo = wo + "" + xingqi;
-                            attendance.setAttendancetim(wo);
-                            attendance.setWorkinghours(working);
+
+
                         }
+
                         //mongodb
                         Query query = new Query();
                         query.addCriteria(Criteria.where("userinfo.ewechatid").is(data.getCheckindata().get(k).getUserid()));
