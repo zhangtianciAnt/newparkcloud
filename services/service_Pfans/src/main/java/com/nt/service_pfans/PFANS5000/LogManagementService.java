8d0e43a73719e99453a8cbdd8e7bb956af216edc
fjl <fjl121206>
2020-07-16 15:59:06 +0800

    添加PL权限的人查看日志一览

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/LogManagementService.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS5000/LogManagementService.java
@@ -21,6 +21,10 @@ public interface LogManagementService {
 
     public List<LogManagement> getDataList(LogManagement logmanagemenr) throws Exception;
 
+    //add_fjl_0716_添加PL权限的人查看日志一览  start
+    public List<LogManagement> getDataListPL(TokenModel tokenModel) throws Exception;
+    //add_fjl_0716_添加PL权限的人查看日志一览  end
+
     public List<LogManagement> getLogDataList(LogManagement logmanagemenr,String startDate,String endDate) throws Exception;
 
     public List<LogManagement> getCheckList(LogManagement logmanagemenr) throws Exception;
