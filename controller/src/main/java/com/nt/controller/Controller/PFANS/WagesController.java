140b04e29ea4922770044d2ab2842b19db584ad2
高兵兵 <bingbing.gao@newtouch.com>
2020-07-24 09:43:36 +0800

    工资计算用模板下载

--- controller/src/main/java/com/nt/controller/Controller/PFANS/WagesController.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/WagesController.java
@@ -3,13 +3,17 @@ package com.nt.controller.Controller.PFANS;
 import com.nt.dao_Pfans.PFANS2000.Wages;
 import com.nt.service_pfans.PFANS2000.WagesService;
 import com.nt.utils.ApiResult;
+import com.nt.utils.ExcelOutPutUtil;
 import com.nt.utils.dao.TokenModel;
 import com.nt.utils.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/wages")
@@ -50,5 +54,36 @@ public class WagesController {
     public ApiResult getWagesByResign(HttpServletRequest request, @RequestParam String user_id) throws Exception {
         TokenModel tokenModel = tokenService.getToken(request);
         return new ApiResult(wagesService.getWagesByResign(user_id,tokenModel));
+    }
+
+    //gbb 0724 模板下载 start
+    @RequestMapping(value = "/download", method = {RequestMethod.GET})
+    public void download(String type, HttpServletResponse response) throws Exception {
+        Map<String, Object> data = new HashMap<>();
+        String templateName = null;
+        String fileName = null;
+        if ( "0".equals(type) ) {
+            templateName = "othertwo.xlsx";
+            fileName = "其他2导入模板";
+        }
+        if ( "1".equals(type) ) {
+            templateName = "otherfour.xlsx";
+            fileName = "其他4导入模板";
+        }
+        if ( "2".equals(type) ) {
+            templateName = "otherfive.xlsx";
+            fileName = "其他5导入模板";
+        }
+        if ( "3".equals(type) ) {
+            templateName = "lunarbasic.xlsx";
+            fileName = "月度赏与导入模板";
+        }
+        if ( "4".equals(type) ) {
+            templateName = "additional.xlsx";
+            fileName = "附加控除导入模板";
+        }
+        if (templateName != null ) {
+            ExcelOutPutUtil.OutPut(fileName,templateName,data,response);
+        }
     }
 }
