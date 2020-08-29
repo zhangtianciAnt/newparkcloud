3c365da022d829c5fd2155a1af9f959063024dc8
wangshuai <1203765575@qq.com>
2020-08-29 10:53:00 +0800

    禅道任务469

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1012Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1012Controller.java
@@ -610,19 +610,19 @@ public class Pfans1012Controller {
         LoanApplication loanapplication = new LoanApplication();
         loanapplication.setPublicradio("1");
         List<LoanApplication> LoanApplicationlist = loanapplicationMapper.select(loanapplication);
-        for(LoanApplication loan : LoanApplicationlist){
-            int scale = 2;//设置位数
-            int roundingMode = 4;//表示四舍五入，可以选择其他舍值方式，例如去尾，等等.
-            BigDecimal bd = new BigDecimal(loan.getBalance());
-            bd = bd.setScale(scale, roundingMode);
-            BigDecimal bd1 = new BigDecimal(loan.getMoneys());
-            bd1 = bd1.setScale(scale, roundingMode);
-            if(bd.compareTo(bd1) == -1){
-                loanapplicationlist.add(loan);
-            }
-        }
+//        for(LoanApplication loan : LoanApplicationlist){
+//            int scale = 2;//设置位数
+//            int roundingMode = 4;//表示四舍五入，可以选择其他舍值方式，例如去尾，等等.
+//            BigDecimal bd = new BigDecimal(loan.getBalance());
+//            bd = bd.setScale(scale, roundingMode);
+//            BigDecimal bd1 = new BigDecimal(loan.getMoneys());
+//            bd1 = bd1.setScale(scale, roundingMode);
+//            if(bd.compareTo(bd1) == -1){
+//                loanapplicationlist.add(loan);
+//            }
+//        }
 
-        return ApiResult.success(loanapplicationlist);
+        return ApiResult.success(LoanApplicationlist);
     }
 
     // add-ws-8/20-禅道469
