6a075a29e8da5099d26ba0fd58b9721464d95763
赵国栋 <guodong.zhao@newtouch.cn>
2020-09-07 13:31:19 +0800

    数据加密解密

--- controller/src/main/java/com/nt/controller/Config/GlobalAop.java
+++ controller/src/main/java/com/nt/controller/Config/GlobalAop.java
@@ -47,8 +47,8 @@ public class GlobalAop {
     public void doAfterReturning(Object ret) throws Throwable {
 
 //        encoder
-//        AES aes = new AES();
-//        ((ApiResult) ret).setData(aes.encrypt(JSONObject.toJSONString(((ApiResult) ret).getData())));
+        AES aes = new AES();
+        ((ApiResult) ret).setData(aes.encrypt(JSONObject.toJSONString(((ApiResult) ret).getData())));
         //处理完请求，返回内容
 //        if( ret !=null && ((ApiResult)ret).getData() != null){
 //            log.info("返回值 : " + JSONUtil.parse(((ApiResult) ret).getData()).toStringPretty());
