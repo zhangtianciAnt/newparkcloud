93d077b7ef0ea65f79d6152f65caaf0c3693b9ef
wangshuai <1203765575@qq.com>
2020-10-13 13:55:46 +0800

    禅道任务429问题修改

--- services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN4000/mapper/ProductsMapper.java
+++ services/service_AOCHUAN/src/main/java/com/nt/service_AOCHUAN/AOCHUAN4000/mapper/ProductsMapper.java
@@ -2,6 +2,8 @@ package com.nt.service_AOCHUAN.AOCHUAN4000.mapper;
 
 import com.nt.dao_AOCHUAN.AOCHUAN4000.Products;
 import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,4 +14,6 @@ public interface ProductsMapper extends MyMapper<Products> {
     public List<Products>  getProdutsExceptUnique();
     //获取不在注册表中的数据
     public List<Products>  getProdutsExceptUniqueInReg();
+    @Select("select * from products where date_format(products.CREATEON, '%Y%m') = #{createon} order by createon desc")
+    public List<Products>  selectlist(@Param("createon") String createon);
 }
