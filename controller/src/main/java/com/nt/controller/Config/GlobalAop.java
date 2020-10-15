f1f006736197ebf4a292e51f3c7d2e46badf0637
高兵兵 <bingbing.gao@newtouch.com>
2020-10-15 19:23:17 +0800

    数据传输加密代码注释（角色权限编辑不好用）

--- controller/src/main/java/com/nt/controller/Config/GlobalAop.java
+++ controller/src/main/java/com/nt/controller/Config/GlobalAop.java
@@ -47,9 +47,9 @@ public class GlobalAop {
     @AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturning(Object ret) throws Throwable {
 //        encoder
-        AES aes = new AES();
-        ((ApiResult) ret).setData(aes.encrypt(JSONObject.toJSONString(((ApiResult) ret).getData(), SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullStringAsEmpty)));
+//        AES aes = new AES();
+//        ((ApiResult) ret).setData(aes.encrypt(JSONObject.toJSONString(((ApiResult) ret).getData(), SerializerFeature.WriteMapNullValue,
+//                SerializerFeature.WriteNullStringAsEmpty)));
         //处理完请求，返回内容
 //        if( ret !=null && ((ApiResult)ret).getData() != null){
 //            log.info("返回值 : " + JSONUtil.parse(((ApiResult) ret).getData()).toStringPretty());
