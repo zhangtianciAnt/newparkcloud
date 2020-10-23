94eb3d36860a7e17f550546b750d847c359e82fc
高兵兵 <bingbing.gao@newtouch.com>
2020-10-23 11:33:43 +0800

    传输加密对【null】的处理

--- controller/src/main/java/com/nt/controller/Config/GlobalAop.java
+++ controller/src/main/java/com/nt/controller/Config/GlobalAop.java
@@ -48,8 +48,7 @@ public class GlobalAop {
     public void doAfterReturning(Object ret) throws Throwable {
 //        encoder
         AES aes = new AES();
-        ((ApiResult) ret).setData(aes.encrypt(JSONObject.toJSONString(((ApiResult) ret).getData(), SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullStringAsEmpty)));
+        ((ApiResult) ret).setData(aes.encrypt(JSONObject.toJSONString(((ApiResult) ret).getData(), SerializerFeature.WriteMapNullValue)));
         //处理完请求，返回内容
 //        if( ret !=null && ((ApiResult)ret).getData() != null){
 //            log.info("返回值 : " + JSONUtil.parse(((ApiResult) ret).getData()).toStringPretty());
