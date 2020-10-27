d1be3832b285a2b523171f948d6e3e8eb7fed275
cash <mayuntao@cash>
2020-10-27 13:23:09 +0800

    添加既有档案导入功能

---
+++ services/service_BASF/src/main/java/com/nt/service_BASF/mapper/ExisttrainarchivesMapper.java
@@ -0,0 +1,17 @@
+package com.nt.service_BASF.mapper;
+
+import com.nt.dao_BASF.Existtrainarchives;
+import com.nt.dao_BASF.VO.ExisttrainarchivesVo;
+import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ExisttrainarchivesMapper extends MyMapper<Existtrainarchives> {
+
+    List<ExisttrainarchivesVo> getAllList() throws Exception;
+
+    ExisttrainarchivesVo getExisttrainarchivesInfoById(@Param("id") String id) throws Exception;
+
+    List<ExisttrainarchivesVo> existtrainarchivesList() throws Exception;
+}
