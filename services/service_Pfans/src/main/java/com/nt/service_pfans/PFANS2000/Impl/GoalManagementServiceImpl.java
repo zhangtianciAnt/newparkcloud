de30f6488bc1107fce784fafab607724d14b340f
changchunmei <chunmei.chang@newtouch.com>
2020-11-11 10:17:34 +0800

    客户提出目标管理中离职人员不应该显示，修改完毕

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/GoalManagementServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/GoalManagementServiceImpl.java
@@ -1,16 +1,23 @@
 package com.nt.service_pfans.PFANS2000.Impl;
 
+import com.nt.dao_Org.CustomerInfo;
 import com.nt.dao_Pfans.PFANS2000.GoalManagement;
+import com.nt.service_Org.UserService;
 import com.nt.service_pfans.PFANS2000.GoalManagementService;
 import com.nt.service_pfans.PFANS2000.mapper.GoalManagementMapper;
 import com.nt.utils.dao.TokenModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(rollbackFor=Exception.class)
@@ -19,9 +26,29 @@ public class GoalManagementServiceImpl implements GoalManagementService {
     @Autowired
     private GoalManagementMapper goalmanagementMapper;
 
+    @Autowired
+    private UserService userService;
+
     @Override
     public List<GoalManagement> list(GoalManagement goalManagement ) throws Exception {
-        return goalmanagementMapper.select(goalManagement);
+        SimpleDateFormat sfym = new SimpleDateFormat("yyyy-MM");
+        Calendar calend = Calendar.getInstance();
+        calend.setTime(new Date());
+        String yearmm  = sfym.format(calend.getTime());
+        String years = yearmm.substring(0,4);
+        String month = yearmm.substring(5,7);
+        if(Integer.valueOf(month)<3)
+        {
+            years = String.valueOf(Integer.valueOf(years)-1);
+        }
+        goalManagement.setYears(years);
+        List<GoalManagement> goalManagementList = goalmanagementMapper.select(goalManagement);
+        List<CustomerInfo> customerInfoList = userService.getCustomerInfoResign(years);
+        for(CustomerInfo c:customerInfoList)
+        {
+            goalManagementList = goalManagementList.stream().filter(item -> (!item.getUser_id().equals(c.getUserid()))).collect(Collectors.toList());
+        }
+        return goalManagementList;
     }
     @Override
     public GoalManagement One(String goalmanagement_id) throws Exception {
