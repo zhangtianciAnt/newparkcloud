efe777313fd815abc9b6484b6dbe47894272a610
张天赐 <tianci.zhao@newtouch.cn>
2020-12-29 14:15:26 +0800

    personalcost表

--- dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/PersonalCost.java
+++ dao/dao_Pfans/src/main/java/com/nt/dao_Pfans/PFANS2000/PersonalCost.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Date;
 
 @Data
 @AllArgsConstructor
@@ -30,210 +31,9 @@ public class PersonalCost extends BaseModel {
     @Column(name = "YEARSANTID")
     private String yearsantid;
 
-    /**
-     * 姓名
-     */
-    @Column(name = "USERID")
-    private String userid;
 
     /**
-     * 等级
-     */
-    @Column(name = "PRANK")
-    private String prank;
-
-    /**
-     * 在职状态
-     */
-    @Column(name = "WORKDAY")
-    private String workday;
-
-
-    /**
-     * 社保基数-养老
-     */
-    @Column(name = "YANGBASERESULT")
-    private String yangbaseresult;
-
-    /**
-     * 社保基数-医疗
-     */
-    @Column(name = "YIBASERESULT")
-    private String yibaseresult;
-
-    /**
-     * 社保基数-公积金
-     */
-    @Column(name = "ZHUBASERESULT")
-    private String zhubaseresult;
-
-
-    /**
-     * 4月人件费预计
-     */
-    @Column(name = "APRILCOSTY")
-    private String aprilcosty;
-
-    /**
-     * 5月人件费预计
-     */
-    @Column(name = "MAYCOSTY")
-    private String maycosty;
-
-    /**
-     * 6月人件费预计
-     */
-    @Column(name = "JUNECOSTY")
-    private String junecosty;
-
-    /**
-     * 7月人件费预计
-     */
-    @Column(name = "JULYCOSTY")
-    private String julycosty;
-
-    /**
-     * 8月人件费预计
-     */
-    @Column(name = "AUGUSTCOSTY")
-    private String augustcosty;
-
-    /**
-     * 9月人件费预计
-     */
-    @Column(name = "SEPTEMBERCOSTY")
-    private String septembercosty;
-
-    /**
-     * 10月人件费预计
-     */
-    @Column(name = "OCTOBERCOSTY")
-    private String octobercosty;
-
-    /**
-     * 11月人件费预计
-     */
-    @Column(name = "NOVEMBERCOSTY")
-    private String novembercosty;
-
-    /**
-     * 12月人件费预计
-     */
-    @Column(name = "DECEMBERCOSTY")
-    private String decembercosty;
-
-    /**
-     * 1月人件费预计
-     */
-    @Column(name = "JANUARYCOSTY")
-    private String januarycosty;
-
-    /**
-     * 2月人件费预计
-     */
-    @Column(name = "FEBRUARYCOSTY")
-    private String februarycosty;
-
-    /**
-     * 3月人件费预计
-     */
-    @Column(name = "MARCHCOSTY")
-    private String marchcosty;
-
-
-    /**
-     * 4月人件费实际
-     */
-    @Column(name = "APRILCOSTS")
-    private String aprilcosts;
-
-    /**
-     * 5月人件费实际
-     */
-    @Column(name = "MAYCOSTS")
-    private String maycosts;
-
-    /**
-     * 6月人件费实际
-     */
-    @Column(name = "JUNECOSTS")
-    private String junecosts;
-
-    /**
-     * 7月人件费实际
-     */
-    @Column(name = "JULYCOSTS")
-    private String julycosts;
-
-    /**
-     * 8月人件费实际
-     */
-    @Column(name = "AUGUSTCOSTS")
-    private String augustcosts;
-
-    /**
-     * 9月人件费实际
-     */
-    @Column(name = "SEPTEMBERCOSTS")
-    private String septembercosts;
-
-    /**
-     * 10月人件费实际
-     */
-    @Column(name = "OCTOBERCOSTS")
-    private String octobercosts;
-
-    /**
-     * 11月人件费实际
-     */
-    @Column(name = "NOVEMBERCOSTS")
-    private String novembercosts;
-
-    /**
-     * 12月人件费实际
-     */
-    @Column(name = "DECEMBERCOSTS")
-    private String decembercosts;
-
-    /**
-     * 1月人件费实际
-     */
-    @Column(name = "JANUARYCOSTS")
-    private String januarycosts;
-
-    /**
-     * 2月人件费实际
-     */
-    @Column(name = "FEBRUARYCOSTS")
-    private String februarycosts;
-
-    /**
-     * 3月人件费实际
-     */
-    @Column(name = "MARCHCOSTS")
-    private String marchcosts;
-
-
-    /**
-     * 4~6月人件费
-     */
-    @Column(name = "APTOJUCOST")
-    private String aptojucost;
-
-    /**
-     * 7~3月人件费 1月1日自动生成
-     */
-    @Column(name = "JUTOMACOST")
-    private String jutomacost;
-
-    /**
-     * 备注
-     */
-    @Column(name = "REMARKS")
-    private String remarks;
-
-    /**
-     *Centerid
+     *CENTERID
      */
     @Column(name = "CENTERID ")
     private String centerid ;
@@ -243,5 +43,41 @@ public class PersonalCost extends BaseModel {
      */
     @Column(name = "GROUPID ")
     private String groupid ;
+
+    /**
+     * 姓名id
+     */
+    @Column(name = "USERID")
+    private String userid;
+
+    /**
+     * 姓名name
+     */
+    @Column(name = "USERNAME")
+    private String username;
+
+    /**
+     * 部门简称
+     */
+    @Column(name = "DEPARTSHORT")
+    private String departshort;
+
+    /**
+     * 配付与否
+     */
+    @Column(name = "ALLOTMENT")
+    private String allotment;
+
+    /**
+     * 新人入社预定月
+     */
+    @Column(name = "NEWPERSONALDATE")
+    private Date newpersonaldate;
+
+    /**
+     * 升格前Rn
+     */
+    @Column(name = "EXRANK")
+    private String exrank;
 
 }
