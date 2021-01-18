5f09238343c1d509faddc7bc43184e2c74e8e588
wangshuai <1203765575@qq.com>
2021-01-18 10:50:21 +0800

    委托决裁关联方针合同修改

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1045Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1045Controller.java
@@ -42,12 +42,16 @@ public class Pfans1045Controller {
         SimpleDateFormat sf1 = new SimpleDateFormat("yyyy");
         List<PolicyContract> policycontractlist2 = new ArrayList<>();
         String cycle =  policycontract.getInformation().substring(5, 7);
+        String year =  policycontract.getInformation().substring(0, 4);
         PolicyContract policy = new PolicyContract();
         policy.setOutsourcingcompany(policycontract.getOutsourcingcompany());
         List<PolicyContract> policycontractlist = policycontractmapper.select(policy);
         policycontractlist = policycontractlist.stream().filter(item -> (item.getStatus().equals("4"))).collect(Collectors.toList());
+        //add-ws-01/18-年度问题数据匹配
+
+        //add-ws-01/18-年度问题数据匹配
         for (PolicyContract PolicyCon : policycontractlist) {
-            if(PolicyCon.getYearss().equals(sf1.format(new Date()))){
+            if(PolicyCon.getYearss().equals(year)){
                 if (cycle.equals("03") || cycle.equals("04") || cycle.equals("05")) {
                     if (PolicyCon.getCycle().equals("3") || PolicyCon.getCycle().equals("1")) {
                         policycontractlist2.add(PolicyCon);
