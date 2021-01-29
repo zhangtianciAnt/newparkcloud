db9da663263513a2fc27f120de151eb18cdbcccc
wangshuai <1203765575@qq.com>
2021-01-29 15:14:42 +0800

    NT_PFANS_20210129_BUG_008

--- controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1045Controller.java
+++ controller/src/main/java/com/nt/controller/Controller/PFANS/Pfans1045Controller.java
@@ -154,50 +154,50 @@ public class Pfans1045Controller {
                 if(PolicyContract.getYearss().equals(year)) {
                     if (PolicyContract.getCycle().equals("0")) {
                         if (cycle.equals("0") || cycle.equals("1") || cycle.equals("2") || cycle.equals("3") || cycle.equals("4") || cycle.equals("5") || cycle.equals("6")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
                     } else if (PolicyContract.getCycle().equals("1")) {
                         if (cycle.equals("3") || cycle.equals("4") || cycle.equals("1")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
 
                     } else if (PolicyContract.getCycle().equals("2")) {
                         if (cycle.equals("5") || cycle.equals("6") || cycle.equals("2")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
 
                     } else if (PolicyContract.getCycle().equals("3")) {
                         if (cycle.equals("3") || cycle.equals("1")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
 
                     } else if (PolicyContract.getCycle().equals("4")) {
                         if (cycle.equals("4") || cycle.equals("1")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
 
                     } else if (PolicyContract.getCycle().equals("5")) {
                         if (cycle.equals("5") || cycle.equals("2")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
 
                     } else if (PolicyContract.getCycle().equals("6")) {
                         if (cycle.equals("6") || cycle.equals("2")) {
-                            policycontractlist2 = policylist;
-                        } else {
                             policycontractlist2 = policycontractlist3;
+                        } else {
+                            policycontractlist2 = policylist;
                         }
                     }
                 }
