bd1fe89eb82f1eb42f8d7ca0bd8f3f1db12f5968
高兵兵 <bingbing.gao@newtouch.com>
2021-02-23 13:18:53 +0800

    PSDCD_PFANS_20200921_XQ_007 资产盘点时状态变化check

--- services/service_Assets/src/main/java/com/nt/service_Assets/mapper/InventoryplanMapper.java
+++ services/service_Assets/src/main/java/com/nt/service_Assets/mapper/InventoryplanMapper.java
@@ -2,6 +2,11 @@ package com.nt.service_Assets.mapper;
 
 import com.nt.dao_Assets.Inventoryplan;
 import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface InventoryplanMapper extends MyMapper<Inventoryplan> {
+    @Select("select inventoryplan_id  from inventoryplan where status  in ('0','4') ")
+    List<Inventoryplan> selectList();
 }
