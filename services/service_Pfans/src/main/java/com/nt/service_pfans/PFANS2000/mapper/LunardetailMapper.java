75af8c50d29f036d19095cefb6bfff80410b1f24
秦浩然 <haoran.qin@newtouch.com>
2021-03-16 18:32:40 +0800

    NT_PFANS_20210315_BUG_146 NT_PFANS_20210315_BUG_147 NT_PFANS_20210315_BUG_148 NT_PFANS_20210315_BUG_149

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/LunardetailMapper.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/LunardetailMapper.java
@@ -14,10 +14,10 @@ public interface LunardetailMapper extends MyMapper<Lunardetail>{
     List<Lunardetail> updateProcesT(@Param("Process") String Process, @Param("Tmid") String Tmid);
     List<Lunardetail> updateProcesG(@Param("Process") String Process, @Param("Gpid") String Gpid);
     List<Lunardetail> updateProcesC(@Param("Process") String Process, @Param("Crid") String Crid);
-    List<Lunardetail> updateProcess1(@Param("Process") String Process);
+    List<Lunardetail> updateProcess1(@Param("Process") String Process, @Param("Evaluationday") String Evaluationday, @Param("Subjectmon") String Subjectmon);
     List<Lunardetail> updateProcess2(@Param("Process") String Process, @Param("Evaluationday") String Evaluationday, @Param("Subjectmon") String Subjectmon);
-    List<Lunardetail> selectTeam(@Param("lunardetailList") List<String> lunardetailList, @Param("Self") String Self);
-    List<Lunardetail> selectGroup(@Param("lunardetailList") List<String> lunardetailList, @Param("Self") String Self);
-    List<Lunardetail> selectCenter(@Param("lunardetailList") List<String> lunardetailList, @Param("Self") String Self);
+    List<Lunardetail> selectTeam(@Param("lunardetailList") List<String> lunardetailList, @Param("Self") String Self, @Param("Evaluationday") String Evaluationday, @Param("Subjectmon") String Subjectmon);
+    List<Lunardetail> selectGroup(@Param("lunardetailList") List<String> lunardetailList, @Param("Self") String Self, @Param("Evaluationday") String Evaluationday, @Param("Subjectmon") String Subjectmon);
+    List<Lunardetail> selectCenter(@Param("lunardetailList") List<String> lunardetailList, @Param("Self") String Self, @Param("Evaluationday") String Evaluationday, @Param("Subjectmon") String Subjectmon);
     List<Lunardetail> selectResult(@Param("Evanum") String Evanum, @Param("Evaday") String Evaday, @Param("Submon") String Submon, @Param("Self") String Self);
 }
