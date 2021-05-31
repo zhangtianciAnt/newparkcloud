67b67ad72a14899614979707f2c2372065c37d21
秦浩然 <haoran.qin@newtouch.com>
2021-05-31 17:50:35 +0800

    试用员工当月转正的当月试用天数

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/GivingServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/GivingServiceImpl.java
@@ -892,7 +892,8 @@ public class GivingServiceImpl implements GivingService {
                     if (calEnterDay.getTime().getTime() > calLastOne.getTime().getTime()) {
                         tempStart = calEnterDay.getTime();
                     }
-                    calSuitDate.add(Calendar.DATE, -1);//转正日当天不算使用
+//                    add 试用员工当月转正的当月试用天数 fr
+                    //calSuitDate.add(Calendar.DATE, -1);//转正日当天不算使用
                     lastMonthSuitDays = getTrialWorkDaysExceptWeekend(tempStart, calLast.getTime());
                     thisMonthSuitDays = getTrialWorkDaysExceptWeekend(calNowOne.getTime(), calSuitDate.getTime());
                 }
