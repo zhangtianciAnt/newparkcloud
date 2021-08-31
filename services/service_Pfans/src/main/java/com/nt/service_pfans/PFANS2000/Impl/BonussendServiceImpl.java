1887345140f37ec4d3e052721ee5740bf3aa8952
秦浩然 <haoran.qin@newtouch.com>
2021-08-31 13:58:41 +0800

    修改去重条件

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/BonussendServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/Impl/BonussendServiceImpl.java
@@ -218,7 +218,7 @@ public class BonussendServiceImpl implements BonussendService {
             }
             //region add_qhr_20210830 添加导入数据的去重，保证每年每人有一条数据
             listVo = listVo.stream().collect(Collectors.collectingAndThen(
-                    Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Bonussend::getJobnumber))), ArrayList::new));
+                    Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Bonussend::getUsername))), ArrayList::new));
             for (Bonussend bonussend : listVo) {
                 Bonussend bonus = new Bonussend();
                 bonus.setUsername(bonussend.getUsername());
