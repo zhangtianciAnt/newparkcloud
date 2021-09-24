4c3eda1c0d52e84eef23dd6fe85eaffc7c18dabe
高兵兵 <bingbing.gao@newtouch.com>
2021-09-24 16:28:21 +0800

    删除bpcompanycost

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS6000/mapper/BpCompanyCostMapper.java
+++
@@ -1,7 +0,0 @@
-package com.nt.service_pfans.PFANS6000.mapper;
-
-import com.nt.dao_Pfans.PFANS6000.BpCompanyCost;
-import com.nt.utils.MyMapper;
-
-public interface BpCompanyCostMapper extends MyMapper<BpCompanyCost> {
-}
