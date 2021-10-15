c4f0a508ef9abe4b7240f32721fb59f1e7caef56
sunchengchuan <chengchuan.sun@newtouch.com>
2021-10-15 13:14:24 +0800

    事业计划PL导出

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/BusinessplanService.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/BusinessplanService.java
@@ -4,13 +4,12 @@ package com.nt.service_pfans.PFANS1000;
 import com.nt.dao_Org.OrgTree;
 import com.nt.dao_Pfans.PFANS1000.Businessplan;
 import com.nt.dao_Pfans.PFANS1000.PersonPlanTable;
-import com.nt.dao_Pfans.PFANS1000.Vo.BusinessGroupA1Vo;
-import com.nt.dao_Pfans.PFANS1000.Vo.BusinessGroupA2Vo;
-import com.nt.dao_Pfans.PFANS1000.Vo.BusinessplanVo;
-import com.nt.dao_Pfans.PFANS1000.Vo.OrgTreeVo;
+import com.nt.dao_Pfans.PFANS1000.Vo.*;
 import com.nt.utils.dao.TokenModel;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
@@ -37,4 +36,8 @@ public interface BusinessplanService {
     //region scc add 9/28 根据审批状态，人员计划，受托theme,委托theme编辑按钮状态 from
     boolean whetherEditor(String years,String centerid) throws Exception;
     //endregion scc add 9/28 根据审批状态，人员计划，受托theme,委托theme编辑按钮状态 to
+
+    //region scc add 事业计划PL导出 from
+    void export(List<ReportBusinessVo> reportBusinessVos, HttpServletRequest request, HttpServletResponse resp) throws Exception;
+    //region scc add 事业计划PL导出 to
 }
