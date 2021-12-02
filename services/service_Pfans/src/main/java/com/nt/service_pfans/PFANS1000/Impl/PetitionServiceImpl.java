a976aecdb0deed3339bdc2a43296921a30f4034e
molei <lei.mo@newtouch.com>
2021-12-02 14:29:04 +0800

    受托契约  请求书分页

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/Impl/PetitionServiceImpl.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS1000/Impl/PetitionServiceImpl.java
@@ -1,9 +1,11 @@
 package com.nt.service_pfans.PFANS1000.Impl;
 
+import com.nt.dao_Pfans.PFANS1000.Contractapplication;
 import com.nt.dao_Pfans.PFANS1000.Napalm;
 import com.nt.dao_Pfans.PFANS1000.Petition;
 import com.nt.dao_Pfans.PFANS5000.CompanyProjects;
 import com.nt.service_pfans.PFANS1000.PetitionService;
+import com.nt.service_pfans.PFANS1000.mapper.ContractapplicationMapper;
 import com.nt.service_pfans.PFANS1000.mapper.PetitionMapper;
 import com.nt.service_pfans.PFANS5000.mapper.CompanyProjectsMapper;
 import com.nt.utils.dao.TokenModel;
@@ -11,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.text.SimpleDateFormat;
 @Service
@@ -26,6 +26,9 @@ public class PetitionServiceImpl implements PetitionService {
     @Autowired
     CompanyProjectsMapper  companyProjectsMapper;
 
+    @Autowired
+    private ContractapplicationMapper contractapplicationMapper;
+
     @Override
     public List<Petition> get(Petition petition) throws Exception {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -35,6 +38,45 @@ public class PetitionServiceImpl implements PetitionService {
         }
         return petitionlist;
     }
+
+    //  add  ml  211201  分页  from
+    @Override
+    public List<Petition> getPage(Petition petition) throws Exception {
+        Contractapplication contractapplication = new Contractapplication();
+        contractapplication.setType("1");
+        List<Contractapplication> coList = contractapplicationMapper.select(contractapplication);
+        List<Contractapplication> pjCodeList = contractapplicationMapper.getPjCode();
+        List<Map<String, String>> checkdata = new ArrayList<>();
+        for (Contractapplication con : coList) {
+            List<Contractapplication> newpjCodeList = pjCodeList.stream().filter(str -> (str.getContractnumber().equals(con.getContractnumber()))).collect(Collectors.toList());
+            if (newpjCodeList.size() > 0) {
+                String strProjectnumber = newpjCodeList.get(0).getProjectnumber();
+                con.setProjectnumber(strProjectnumber.substring(0, strProjectnumber.length() - 1));
+            }
+            if ("1".equals(con.getState()) || "有效".equals(con.getState())) {
+                Map<String, String> map = new HashMap<>();
+                map.put("contractnumber", con.getContractnumber());
+                checkdata.add(map);
+            }
+        }
+
+        List<Petition> petitionlist = petitionMapper.select(petition);
+        if (petitionlist.size() > 0) {
+            petitionlist = petitionlist.stream().sorted(Comparator.comparing(Petition::getCreateon).reversed()).collect(Collectors.toList());
+        }
+
+        List<Petition> petitionList = new ArrayList<>();
+        for (Map<String, String> data : checkdata) {
+            for (Petition pet : petitionlist) {
+                if (data.get("contractnumber").equals(pet.getContractnumber())) {
+                    petitionList.add(pet);
+                }
+            }
+        }
+
+        return petitionList;
+    }
+    //  add  ml  211201  分页  to
 
     @Override
     public Petition one(String petition_id) throws Exception {
