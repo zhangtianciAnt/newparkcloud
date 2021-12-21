f779c825238ed943d90c611fd7674b2170e00e7b
张天赐 <tianci.zhao@newtouch.cn>
2021-12-21 17:54:06 +0800

    离职日变更，删除原数据关联的退职者调书以及待办

--- services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/StaffexitprocedureMapper.java
+++ services/service_Pfans/src/main/java/com/nt/service_pfans/PFANS2000/mapper/StaffexitprocedureMapper.java
@@ -2,6 +2,16 @@ package com.nt.service_pfans.PFANS2000.mapper;
 
 import com.nt.dao_Pfans.PFANS2000.Staffexitprocedure;
 import com.nt.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface StaffexitprocedureMapper extends MyMapper<Staffexitprocedure> {
+
+
+    @Select("UPDATE staffexitproce SET `STATUS` = '1' where STAFFEXITPROCEDURE_ID = {#staff_id}")
+    void upStaffproe(@Param("staff_id") String staff_id);
+
+    @Select("UPDATE todonotice SET `STATUS` = '1' WHERE DATAID IN " +
+            "( SELECT STAFFEXITPROCE_ID FROM `staffexitproce` WHERE STAFFEXITPROCEDURE_ID = {#staff_id})")
+    void upTodoNo(@Param("staff_id") String staff_id);
 }
